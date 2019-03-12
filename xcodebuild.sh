3416ab551aedb1905579c8bc4594e69e84e00f89
fenix <betzy_skamm@hotmail.com>
2019-03-12 15:35:15 -0700

    added config to build libsodium

--- xcodebuild.sh
+++ xcodebuild.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+cd ./3rdParty/libsodium/
+chmod +x autogen.sh
+sudo ./autogen.sh
+sh ./dist-build/osx.sh
+
+cd ../../
+
 xcodebuild -workspace "./Xcode/devilutionX.xcworkspace" -scheme "devilutionX" build
