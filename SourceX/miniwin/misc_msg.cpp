a7e2cd2900ce59d79a1098a0157008b7919d9688
erfg12 <newgmobile@gmail.com>
2019-07-05 17:44:36 -0400

    plus now opens menu, minus opens mini map

--- SourceX/miniwin/misc_msg.cpp
+++ SourceX/miniwin/misc_msg.cpp
@@ -239,21 +239,6 @@ WINBOOL PeekMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilter
 		lpMsg->message = e.type == SDL_KEYUP;
 		lpMsg->lParam = 0;
 		break;
-	case SDL_JOYBUTTONUP:
-		switch(e.jbutton.button)
-		{
-			case 8:
-					lpMsg->message = DVL_WM_RBUTTONUP;
-					lpMsg->lParam = (MouseY << 16) | (MouseX & 0xFFFF);
-					lpMsg->wParam = keystate_for_mouse(0);
-					break;
-			case 9:
-					lpMsg->message = DVL_WM_LBUTTONUP;
-					lpMsg->lParam = (MouseY << 16) | (MouseX & 0xFFFF);
-					lpMsg->wParam = keystate_for_mouse(0);
-					break;
-		}
-		break;
 	case SDL_JOYBUTTONDOWN:
 		switch(e.jbutton.button)
 		{
@@ -285,10 +270,11 @@ WINBOOL PeekMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilter
 			case  9:	// ZR
 				useBeltPotion(true); // use mana potion
 				break;
-			case 10:
-				break;
-			case 11:
+			case 10:	// plus
 				PressKey(VK_ESCAPE);
+				break;
+			case 11:	// minus
+				PressKey(VK_TAB);
 				break;
 			case 16:
 				PressKey(VK_LEFT);
