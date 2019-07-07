f083ea6337c138f961317b26c82acf001385fe7e
erfg12 <newgmobile@gmail.com>
2019-07-06 20:55:25 -0400

    d-pad works, fixed stext bye re-entering talk
    can now compile other systems besides Switch

--- Source/control.cpp
+++ Source/control.cpp
@@ -901,6 +901,10 @@ void DoSpeedBook()
 #endif
 
 	SetCursorPos(X, Y);
+#ifdef SWITCH
+	MouseX = X;
+	MouseY = Y;
+#endif
 }
 
 void DoPanBtn()
--- Source/plrctrls.cpp
+++ Source/plrctrls.cpp
@@ -141,6 +141,8 @@ void HideCursor()
 	if (pcurs >= CURSOR_FIRSTITEM) // if we don't drop the item on cursor, it will be destroyed
 		DropItemBeforeTrig();
 	SetCursorPos(320, 180);
+	MouseX = 320;
+	MouseY = 180;
 	SetCursor_(CURSOR_NONE);
 	newCurHidden = true;
 }
@@ -184,6 +186,8 @@ void attrIncBtnSnap(int key)
 	int x = attribute_inc_rects2[slot][0] + (attribute_inc_rects2[slot][2] / 2);
 	int y = attribute_inc_rects2[slot][1] + (attribute_inc_rects2[slot][3] / 2);
 	SetCursorPos(x, y);
+	MouseX = x;
+	MouseY = y;
 }
 
 // move the cursor around in our inventory
@@ -191,6 +195,12 @@ void attrIncBtnSnap(int key)
 // small inventory squares are 29x29 (roughly)
 void invMove(int key)
 {
+	if (!invflag)
+		return;
+	if (ticks - invmove < 80) {
+		return;
+	}
+	invmove = ticks;
 	int x = MouseX;
 	int y = MouseY;
 
@@ -327,9 +337,16 @@ void invMove(int key)
 
 	if (pcurs > 1) {     // [3] Keep item in the same slot, don't jump it up
 		if (x != MouseX) // without this, the cursor keeps moving -10
-			SetCursorPos((x - 10), (y - 10));
-	} else
+			{
+				SetCursorPos((x - 10), (y - 10));
+				MouseX = x - 10;
+				MouseY = y - 10;
+			}
+	} else {
 		SetCursorPos(x, y);
+		MouseX = x;
+		MouseY = y;
+	}
 }
 // check if hot spell at X Y exists
 bool HSExists(int x, int y)
@@ -410,8 +427,11 @@ void hotSpellMove(int key)
 		y = speedspellscoords[spbslot].y;
 	}
 
-	if (x > 0 && y > 0)
+	if (x > 0 && y > 0) {
 		SetCursorPos(x, y);
+		MouseX = x;
+		MouseY = y;
+	}
 }
 // walk in the direction specified
 
@@ -448,8 +468,30 @@ void useBeltPotion(bool mana)
 	}
 }
 
+void movements(int key) {
+	if (key == VK_UP) {
+		invMove(WALK_N);
+		hotSpellMove(VK_UP);
+		attrIncBtnSnap(VK_UP);
+		walkInDir(WALK_N);
+	} else if (key == VK_RIGHT) {
+		invMove(WALK_E);
+		hotSpellMove(VK_RIGHT);
+		walkInDir(WALK_E);
+	} else if (key == VK_DOWN) {
+		invMove(WALK_S);
+		hotSpellMove(VK_DOWN);
+		attrIncBtnSnap(VK_DOWN);
+		walkInDir(WALK_S);
+	} else if (key == VK_LEFT) {
+		invMove(WALK_W);
+		hotSpellMove(VK_LEFT);
+		walkInDir(WALK_W);
+	}
+}
+
 void charMovement() {
-	if (stextflag || questlog || helpflag || talkflag || qtextflag)
+	if (stextflag > 0 || questlog || helpflag || talkflag || qtextflag)
 		return;
 
 	if (!invflag && !spselflag && !chrflag) {
@@ -465,23 +507,13 @@ void charMovement() {
 	}
 
 	if (GetAsyncKeyState(VK_UP) & 0x8000 || GetAsyncKeyState(0x57) & 0x8000 || leftStickY >= 0.5) {
-		invMove(WALK_N);
-		hotSpellMove(VK_UP);
-		attrIncBtnSnap(VK_UP);
-		walkInDir(WALK_N);
+		movements(VK_UP);
 	} else if (GetAsyncKeyState(VK_RIGHT) & 0x8000 || GetAsyncKeyState(0x44) & 0x8000 || leftStickX >= 0.5) {
-		invMove(WALK_E);
-		hotSpellMove(VK_RIGHT);
-		walkInDir(WALK_E);
+		movements(VK_RIGHT);
 	} else if (GetAsyncKeyState(VK_DOWN) & 0x8000 || GetAsyncKeyState(0x53) & 0x8000 || leftStickY <= -0.5) {
-		invMove(WALK_S);
-		hotSpellMove(VK_DOWN);
-		attrIncBtnSnap(VK_DOWN);
-		walkInDir(WALK_S);
+		movements(VK_DOWN);
 	} else if (GetAsyncKeyState(VK_LEFT) & 0x8000 || GetAsyncKeyState(0x41) & 0x8000 || leftStickX <= -0.5) {
-		invMove(WALK_W);
-		hotSpellMove(VK_LEFT);
-		walkInDir(WALK_W);
+		movements(VK_LEFT);
 	}
 
 	if (GetAsyncKeyState(MK_LBUTTON) & 0x8000) {
@@ -503,10 +535,10 @@ void keyboardExpansion(int vikey)
 	static DWORD statuptimer;
 	ticks = GetTickCount();
 
-	if (stextflag || questlog || helpflag || talkflag || qtextflag)
+	if (stextflag > 0 || questlog || helpflag || talkflag || qtextflag)
 		return;
 	if (vikey == VK_SPACE) { // similar to X button on PS1 ccontroller. Talk to towners, click on inv items, attack.
-		if (invflag) {                         // inventory is open
+		if (invflag) { // inventory is open
 			if (ticks - clickinvtimer >= 300) {
 				clickinvtimer = ticks;
 				if (pcurs == CURSOR_IDENTIFY)
@@ -536,7 +568,7 @@ void keyboardExpansion(int vikey)
 			HideCursor();
 			talktick = GetTickCount(); // this is shared with STextESC, do NOT duplicate or use anywhere else
 			if (!checkMonstersNearby(true)) {
-				if (talktick - talkwait > 1500) { // prevent re-entering talk after finished
+				if (talktick - talkwait > 600) { // prevent re-entering talk after finished
 					talkwait = talktick;
 					checkTownersNearby(true);
 				}
@@ -550,7 +582,6 @@ void keyboardExpansion(int vikey)
 				checkItemsNearby(true);
 			}
 		}
-
 	}
 }
 
