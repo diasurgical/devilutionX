cd7e5621cfc7f60965ec395f0d55795b8740b857
arczi84 <arturjarosik@gmail.com>
2019-07-11 15:32:19 +0200

    fix double declaration

---
+++ CMakeFiles/smacker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smacker.dir/3rdParty/libsmacker/smk_bitstream.c.o"
+  "CMakeFiles/smacker.dir/3rdParty/libsmacker/smk_hufftree.c.o"
+  "CMakeFiles/smacker.dir/3rdParty/libsmacker/smacker.c.o"
+  "libsmacker.pdb"
+  "libsmacker.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smacker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
