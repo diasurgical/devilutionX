7555c81443d3cd80eeb7c27923624692449f494d
klaus <nsklaus@yandex.com>
2019-07-14 13:49:20 +0200

    Revert "fix warnings"
    This reverts commit 462db32aac388bfa2d1defa6ae6c45a0c7c25ff4.

--- SourceX/miniwin/sdl1_wrapper.cpp
+++ SourceX/miniwin/sdl1_wrapper.cpp
@@ -157,8 +157,12 @@ SDL_Surface* SDL_GetWindowSurface(SDL_Window* window)
 int SDL_UpdateWindowSurface(SDL_Window* window)
 {
 	
-	return SDL_Flip(window->surface);
-
+	if (window)		{
+		SDL_Flip(window->surface);
+		return 1;
+	}
+	else
+		return 0;
 }
 
 SDL_Renderer* SDL_CreateRenderer(SDL_Window* window,
@@ -174,7 +178,7 @@ int SDL_SetRenderDrawColor(SDL_Renderer* renderer,
                            Uint8         b,
                            Uint8         a)
 {
-	return SDL_FillRect( renderer, NULL, SDL_MapRGBA( renderer->format, r, g, b, a) );
+	SDL_FillRect( renderer, NULL, SDL_MapRGBA( renderer->format, r, g, b, a) );
 }
 
 SDL_Texture* SDL_CreateTextureFromSurface(SDL_Renderer* renderer,
@@ -335,7 +339,7 @@ int SDL_RenderCopy(SDL_Renderer*   renderer,
                    const SDL_Rect* dstrect)
 {
    //Blit the surface
-    return SDL_BlitSurface( texture, NULL, renderer, NULL );
+    SDL_BlitSurface( texture, NULL, renderer, NULL );
 }
 
 void SDL_RenderPresent(SDL_Renderer* renderer)
