1f1a32650f4d5fed721ffa4fcc9ed5aa66c9155a
rsn8887 <rsn8887@users.noreply.github.com>
2019-07-16 15:20:02 -0500

    Fix inventory controls with stick or dpad

--- Source/plrctrls.cpp
+++ Source/plrctrls.cpp
@@ -21,7 +21,6 @@ int inv_left = INV_LEFT;
 int inv_height = INV_HEIGHT;
 static DWORD attacktick;
 static DWORD invmove;
-DWORD ticks;
 int slot = SLOTXY_INV_FIRST;
 int spbslot = 0;
 coords speedspellscoords[50];
@@ -122,6 +121,7 @@ bool checkMonstersNearby(bool attack)
 		return false;
 	}
 	if (attack) {
+		DWORD ticks = GetTickCount();
 		if (ticks - attacktick > 100) { // prevent accidental double attacks
 			attacktick = ticks;
 			LeftMouseCmd(false);
@@ -154,6 +154,7 @@ void attrIncBtnSnap(int key)
 	if (chrbtnactive && !plr[myplr]._pStatPts)
 		return;
 
+	DWORD ticks = GetTickCount();
 	if (ticks - invmove < 80) {
 		return;
 	}
@@ -196,6 +197,8 @@ void invMove(int key)
 {
 	if (!invflag)
 		return;
+
+	DWORD ticks = GetTickCount();
 	if (ticks - invmove < 80) {
 		return;
 	}
@@ -368,6 +371,7 @@ void hotSpellMove(int key)
 	if (pcurs > 0)
 		HideCursor();
 
+	DWORD ticks = GetTickCount();
 	if (ticks - invmove < 80) {
 		return;
 	}
@@ -438,7 +442,7 @@ void walkInDir(int dir)
 {
 	if (invflag || spselflag || chrflag || questlog) // don't walk if inventory, speedbook or char info windows are open
 		return;
-	ticks = GetTickCount();
+	DWORD ticks = GetTickCount();
 	if (ticks - invmove < 370) {
 		return;
 	}
@@ -451,6 +455,7 @@ void walkInDir(int dir)
 static DWORD menuopenslow;
 void useBeltPotion(bool mana)
 {
+	DWORD ticks = GetTickCount();
 	int invNum = 0;
 	if (ticks - menuopenslow < 300) {
 		return;
@@ -532,7 +537,7 @@ void keyboardExpansion(int vikey)
 	static DWORD opentimer;
 	static DWORD clickinvtimer;
 	static DWORD statuptimer;
-	ticks = GetTickCount();
+	DWORD ticks = GetTickCount();
 
 	if (stextflag > 0 || questlog || helpflag || talkflag || qtextflag)
 		return;
