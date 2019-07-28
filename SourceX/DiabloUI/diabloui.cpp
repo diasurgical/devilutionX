0c5aa5947e5ad1c64680ee0d2b2cf9f6638cab1b
vimino <vitor@vimino.net>
2019-07-28 15:23:02 +0100

    Removed unnecessary repintro defaults

--- SourceX/DiabloUI/diabloui.cpp
+++ SourceX/DiabloUI/diabloui.cpp
@@ -215,6 +215,7 @@ bool UiFocusNavigation(SDL_Event *event)
 	if (event->type == SDL_QUIT)
 		exit(0);
 
+	/*
 	switch(event->type) {
 		case SDL_KEYUP:
 		case SDL_MOUSEBUTTONUP:
@@ -232,6 +233,7 @@ bool UiFocusNavigation(SDL_Event *event)
 		case SDL_WINDOWEVENT:
 			mainmenu_restart_repintro();
 	}
+	*/
 
 	if (event->type == SDL_KEYDOWN) {
 		switch (event->key.keysym.sym) {
