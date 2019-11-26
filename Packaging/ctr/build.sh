775b82461b9ae5113495855d650e8387f02f1ade
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-11-26 21:08:44 +0000

    N3DS cmake build fixes

--- Packaging/ctr/build.sh
+++ Packaging/ctr/build.sh
@@ -18,8 +18,28 @@ build() {
 	mkdir -p ../../build
 	cd ../../build
 	rm -f CMakeCache.txt
-	DEVKITPRO="$DEVKITPRO" cmake .. -DBINARY_RELEASE=ON -DUSE_SDL1=ON -DNONET=ON \
-		-DSDL1_VIDEO_MODE_FLAGS='SDL_SWSURFACE|SDL_CONSOLEBOTTOM' \
+	local -ra defs=(
+		-DBINARY_RELEASE=ON
+		-DUSE_SDL1=ON
+		-DSDL1_VIDEO_MODE_FLAGS='SDL_SWSURFACE|SDL_CONSOLEBOTTOM'
+		-DNONET=ON
+		-DPREFILL_PLAYER_NAME=ON
+		-DHAS_KBCTRL=1
+		-DKBCTRL_BUTTON_DPAD_LEFT=SDLK_LEFT
+		-DKBCTRL_BUTTON_DPAD_RIGHT=SDLK_RIGHT
+		-DKBCTRL_BUTTON_DPAD_UP=SDLK_UP
+		-DKBCTRL_BUTTON_DPAD_DOWN=SDLK_DOWN
+		-DKBCTRL_BUTTON_B=SDLK_a
+		-DKBCTRL_BUTTON_A=SDLK_b
+		-DKBCTRL_BUTTON_Y=SDLK_y
+		-DKBCTRL_BUTTON_X=SDLK_x
+		-DKBCTRL_BUTTON_RIGHTSHOULDER=SDLK_r
+		-DKBCTRL_BUTTON_LEFTSHOULDER=SDLK_l
+		-DKBCTRL_BUTTON_START=SDLK_RETURN
+		-DKBCTRL_BUTTON_BACK=SDLK_ESCAPE
+		-DKBCTRL_MODIFIER_KEY=SDLK_END
+	)
+	DEVKITPRO="$DEVKITPRO" cmake .. "${defs[@]}" \
 		-DCMAKE_TOOLCHAIN_FILE=../CMake/ctr/devkitarm-libctru.cmake
 	DEVKITPRO="$DEVKITPRO" make -j "$(nproc)"
 	cd -
@@ -28,8 +48,8 @@ build() {
 install_deps() {
 	"$DEVKITPRO/pacman/bin/pacman" -S --needed --noconfirm --quiet \
 		3ds-sdl 3ds-sdl_ttf 3ds-sdl_mixer \
-		3ds-freetype 3ds-libvorbis 3ds-libmikmod\
-		libctru devkitARM general-tools 3ds-tools devkitpro-pkgbuild-helpers
+		3ds-freetype 3ds-libogg 3ds-libvorbisidec 3ds-mikmod \
+		libctru citro3d picasso devkitARM general-tools 3dslink 3dstools devkitpro-pkgbuild-helpers
 }
 
 prepare_devkitpro() {
