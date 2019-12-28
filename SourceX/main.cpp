acca89ba51460baf7d7e2e735bf9afe71671deb3
james-cell007 <jimmy-cell@ukr.net>
2019-12-28 21:53:59 +0200

    added functions to check if images exist in sdcard and move them

--- SourceX/main.cpp
+++ SourceX/main.cpp
@@ -1,10 +1,15 @@
 #include <string>
 #include <SDL.h>
-#ifdef __SWITCH__
-#include "platform/switch/network.h"
-#endif
 
 #include "devilution.h"
+
+#ifdef __ANDROID__
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <fstream>
+#endif
+
 
 #if !defined(__APPLE__)
 extern "C" const char *__asan_default_options()
@@ -26,30 +31,90 @@ static std::string build_cmdline(int argc, char **argv)
 }
 
 #ifdef __ANDROID__
-int SDL_main(int argc, char **argv)
 
-
-//Add check paths in later.
-
-
-
-
+inline bool FileExists (const std::string& name) {
+	struct stat buffer;
+	return (stat (name.c_str(), &buffer) == 0);
+}
 
 
 
+int SDL_main(int argc, char* argv[]) {
+// I am placing this here becasue I don't want to stub another file.
 
 
+//This *CAN* be better. I understand.
+if ( FileExists("/sdcard/devilutionx") == false) {
+	const int dir_errz = system("mkdir -p /sdcard/devilutionx");
+	if (-1 == dir_errz) {
+		printf("Error creating directory!\n");
+		exit(1);
+	}
+}
 
+// Copy images if they exist --
+if ( FileExists("/sdcard/devilutionx/dpad.png") == false) {
+	const int dir_errx = system("cp  /sdcard/dpad.png /sdcard/devilutionx/");
+	if (-1 == dir_errx) {
+		printf("Error copying dpad.png !\n");
+		exit(1);
+	}
+}
 
+if ( FileExists("/sdcard/devilutionx/shift.png") == false) {
+	const int dir_errc = system("cp  /sdcard/shift.png /sdcard/devilutionx/");
+	if (-1 == dir_errc) {
+		printf("Error copying shift.png\n");
+		exit(1);
+	}
+}
+if ( FileExists("/sdcard/devilutionx/demosq.png") == false) {
+	const int dir_errv = system("cp  /sdcard/demosq.png /sdcard/devilutionx/");
+	if (-1 == dir_errv) {
+		printf("Error copying demosq.png\n");
+		exit(1);
+	}
+}
+if ( FileExists("/sdcard/devilutionx/input_attack.png") == false) {
+	const int dir_errb = system("cp  /sdcard/input_attack.png /sdcard/devilutionx/");
+	if (-1 == dir_errb) {
+		printf("Error copying input_attack.png\n");
+		exit(1);
+	}
+}
+
+if ( FileExists("/sdcard/devilutionx/input_cast.png/") == false) {
+	const int dir_errcc = system("cp  /sdcard/input_cast.png /sdcard/devilutionx/");
+	if (-1 == dir_errcc) {
+		printf("Error copying input_attack.png\n");
+		exit(1);
+	}
+}
+
+if ( FileExists("/sdcard/devilutionx/diabdat.mpq") == false) {
+	const int dir_errbz = system("cp  /sdcard/diabdat.mpq /sdcard/devilutionx/");
+	if (-1 == dir_errbz) {
+		printf("Error copying diabdat.mpq \n");
+		exit(1);
+	}
+}
+
+//org.diasurgical.devilutionx
+if ( FileExists("/sdcard/Android/data/org.diasurgical.devilutionx") == false) {
+	const int dir_errn = system("mkdir -p /sdcard/Android/data/org.diasurgical.devilutionx");
+	if (-1 == dir_errn) {
+		printf("Error creating directory!\n");
+		exit(1);
+	}
+}
+
+	return dvl::WinMain(NULL, NULL, (char *)"", 0);
+}
 #else
 int main(int argc, char **argv)
-#endif
 {
 	auto cmdline = build_cmdline(argc, argv);
-
-#ifdef __SWITCH__
-	switch_enable_network();
-#endif
-
 	return dvl::WinMain(NULL, NULL, (char *)cmdline.c_str(), 0);
 }
+#endif
+
