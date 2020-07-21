a4cd0bfa44b3f3da425670a34f755c6b7e0b54a4
MrHuu <wilbert.huu@gmail.com>
2020-07-21 20:47:09 +0200

    [3DS] warn and exit on missing DSP firmware 'dspfirm.cdc'

--- SourceX/platform/ctr/system.cpp
+++ SourceX/platform/ctr/system.cpp
@@ -1,10 +1,31 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <3ds.h>
 #include "platform/ctr/system.h"
 
+bool isN3DS;
+
+bool ctr_check_dsp()
+{
+	FILE *dsp = fopen("sdmc:/3ds/dspfirm.cdc", "r");
+	if (dsp == NULL) {
+		fclose(dsp);
+		gfxInitDefault();
+		errorConf error;
+		errorInit(&error, ERROR_TEXT, CFG_LANGUAGE_EN);
+		errorText(&error, "Cannot find DSP firmware!\n\n\"sdmc:/3ds/dspfirm.cdc\"\n\nRun \'DSP1\' atleast once to\ndump your DSP firmware.");
+		errorDisp(&error);
+		gfxExit();
+		return false;
+	}
+	fclose(dsp);
+	return true;
+}
+
 void ctr_sys_init()
 {
-	bool isN3DS;
+	if( ctr_check_dsp() == false )
+		exit(0);
 
 	APT_CheckNew3DS(&isN3DS);
 	if(isN3DS)
