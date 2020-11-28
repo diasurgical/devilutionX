diasurgical/devilutionX

commit 01401a16c70213db7a9a84196dee354a2ebc11a3

Fix various controller issues
* Fixes Right Stick cursor movement
* Fixes cursor flicker when moving the cursor while moving the character
* Fixes START + SELECT menu
* Makes SELECT act as Escape in the in-game menu

Fixes #930

SourceX/controls/controller_motion.cpp
@@ -55,11 +55,48 @@ void ScaleJoystickAxes(float *x, float *y, float deadzone)
 	}
 }
 
+// SELECT + D-Pad to simulate right stick movement.
+bool SimulateRightStickWithDpad(const SDL_Event &event)
+{
+	static bool simulating = false;
+	const ControllerButtonEvent ctrl_event = ToControllerButtonEvent(event);
+	if (ctrl_event.button == ControllerButton_BUTTON_BACK) {
+		if (ctrl_event.up && simulating) {
+			rightStickX = rightStickY = 0;
+			simulating = false;
+		}
+		return false;
+	}
+	if (!IsControllerButtonPressed(ControllerButton_BUTTON_BACK))
+		return false;
+	switch (ctrl_event.button) {
+	case ControllerButton_BUTTON_DPAD_LEFT:
+		rightStickX = ctrl_event.up ? 0 : -1;
+		break;
+	case ControllerButton_BUTTON_DPAD_RIGHT:
+		rightStickX = ctrl_event.up ? 0 : 1;
+		break;
+	case ControllerButton_BUTTON_DPAD_UP:
+		rightStickY = ctrl_event.up ? 0 : 1;
+		break;
+	case ControllerButton_BUTTON_DPAD_DOWN:
+		rightStickY = ctrl_event.up ? 0 : -1;
+		break;
+	default:
+		return false;
+	}
+	simulating = !(rightStickX == 0 && rightStickY == 0);
+
+	return true;
+}
+
 } // namespace
 
 float leftStickX, leftStickY, rightStickX, rightStickY;
 float leftStickXUnscaled, leftStickYUnscaled, rightStickXUnscaled, rightStickYUnscaled;
 bool leftStickNeedsScaling, rightStickNeedsScaling;
+
+namespace {
 
 void ScaleJoysticks()
 {
@@ -81,45 +118,26 @@ void ScaleJoysticks()
 	}
 }
 
+} // namespace
+
 // Updates motion state for mouse and joystick sticks.
 bool ProcessControllerMotion(const SDL_Event &event)
 {
 #ifndef USE_SDL1
-	if (ProcessGameControllerAxisMotion(event))
+	if (ProcessGameControllerAxisMotion(event)) {
+		ScaleJoysticks();
 		return true;
+	}
 #endif
-	if (ProcessJoystickAxisMotion(event))
+	if (ProcessJoystickAxisMotion(event)) {
+		ScaleJoysticks();
 		return true;
+	}
 #if HAS_KBCTRL == 1
 	if (ProcessKbCtrlAxisMotion(event))
 		return true;
 #endif
-
-	// SELECT + D-Pad simulating mouse movement.
-	if (!IsControllerButtonPressed(ControllerButton_BUTTON_BACK)) {
-		rightStickX = 0;
-		rightStickY = 0;
-		return false;
-	}
-
-	const ControllerButtonEvent ctrl_event = ToControllerButtonEvent(event);
-	if (!IsDPadButton(ctrl_event.button))
-		return false;
-	if (IsControllerButtonPressed(ControllerButton_BUTTON_DPAD_LEFT)) {
-		rightStickX = -1;
-	} else if (IsControllerButtonPressed(ControllerButton_BUTTON_DPAD_RIGHT)) {
-		rightStickX = 1;
-	} else {
-		rightStickX = 0;
-	}
-	if (IsControllerButtonPressed(ControllerButton_BUTTON_DPAD_UP)) {
-		rightStickY = 1;
-	} else if (IsControllerButtonPressed(ControllerButton_BUTTON_DPAD_DOWN)) {
-		rightStickY = -1;
-	} else {
-		rightStickY = 0;
-	}
-	return true;
+	return SimulateRightStickWithDpad(event);
 }
 
 } // namespace dvl

SourceX/controls/controller_motion.h
@@ -15,8 +15,6 @@ extern float leftStickX, leftStickY, rightStickX, rightStickY;
 // Whether stick positions have been updated and need rescaling.
 extern bool leftStickNeedsScaling, rightStickNeedsScaling;
 
-void ScaleJoysticks();
-
 // Updates motion state for mouse and joystick sticks.
 bool ProcessControllerMotion(const SDL_Event &event);
 

SourceX/controls/game_controls.cpp
@@ -12,6 +12,9 @@
 
 namespace dvl {
 
+bool start_modifier_active = false;
+bool select_modifier_active = false;
+
 namespace {
 
 DWORD translate_controller_button_to_key(ControllerButton controller_button)
@@ -25,6 +28,7 @@ DWORD translate_controller_button_to_key(ControllerButton controller_button)
 		return DVL_VK_RETURN;
 	case ControllerButton_BUTTON_LEFTSTICK:
 		return DVL_VK_TAB; // Map
+	case ControllerButton_BUTTON_BACK:
 	case ControllerButton_BUTTON_START:
 		return DVL_VK_ESCAPE;
 	case ControllerButton_BUTTON_DPAD_LEFT:
@@ -40,44 +44,69 @@ DWORD translate_controller_button_to_key(ControllerButton controller_button)
 	}
 }
 
-} // namespace
+bool HandleStartAndSelect(const ControllerButtonEvent &ctrl_event, GameAction *action)
+{
+	const bool in_game_menu = InGameMenu();
 
-bool start_modifier_active = false;
-bool select_modifier_active = false;
+	const bool start_is_down = IsControllerButtonPressed(ControllerButton_BUTTON_START);
+	const bool select_is_down = IsControllerButtonPressed(ControllerButton_BUTTON_BACK);
+	start_modifier_active = !in_game_menu && start_is_down;
+	select_modifier_active = !in_game_menu && select_is_down && !start_modifier_active;
+
+	// Tracks whether we've received both START and SELECT down events.
+	//
+	// Using `IsControllerButtonPressed()` for this would be incorrect.
+	// If both buttons are pressed simultaneously, SDL sends 2 events for which both buttons are in the pressed state.
+	// This allows us to avoid triggering START+SELECT action twice in this case.
+	static bool start_down_received = false;
+	static bool select_down_received = false;
+	switch (ctrl_event.button) {
+	case ControllerButton_BUTTON_BACK:
+		select_down_received = !ctrl_event.up;
+		break;
+	case ControllerButton_BUTTON_START:
+		start_down_received = !ctrl_event.up;
+		break;
+	default:
+		return false;
+	}
+
+	if (start_down_received && select_down_received) {
+		*action = GameActionSendKey { DVL_VK_ESCAPE, ctrl_event.up };
+		return true;
+	}
+
+	if (in_game_menu && (start_is_down || select_is_down) && !ctrl_event.up) {
+		// If both are down, do nothing because `both_received` will trigger soon.
+		if (start_is_down && select_is_down) return true;
+		*action = GameActionSendKey { DVL_VK_ESCAPE, ctrl_event.up };
+		return true;
+	}
+
+	return false;
+}
+
+} // namespace
 
 bool GetGameAction(const SDL_Event &event, GameAction *action)
 {
 	const ControllerButtonEvent ctrl_event = ToControllerButtonEvent(event);
 	const bool in_game_menu = InGameMenu();
 
-	start_modifier_active = !in_game_menu && IsControllerButtonPressed(ControllerButton_BUTTON_START);
-	select_modifier_active = !in_game_menu && IsControllerButtonPressed(ControllerButton_BUTTON_BACK);
-
-	// SELECT + D-Pad simulating mouse movement.
-	if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK) && IsDPadButton(ctrl_event.button)) {
+	if (HandleStartAndSelect(ctrl_event, action))
 		return true;
-	}
-
-	// START + SELECT
-	if (!ctrl_event.up
-	    && ((ctrl_event.button == ControllerButton_BUTTON_BACK && IsControllerButtonPressed(ControllerButton_BUTTON_START))
-	           || (ctrl_event.button == ControllerButton_BUTTON_START && IsControllerButtonPressed(ControllerButton_BUTTON_BACK)))) {
-		select_modifier_active = start_modifier_active = false;
-		*action = GameActionSendKey{ DVL_VK_ESCAPE, ctrl_event.up };
-		return true;
-	}
 
 	if (!in_game_menu) {
 		switch (ctrl_event.button) {
 		case ControllerButton_BUTTON_LEFTSHOULDER:
-			if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK)) {
+			if (select_modifier_active) {
 				if (!IsAutomapActive())
 					*action = GameActionSendMouseClick{ GameActionSendMouseClick::LEFT, ctrl_event.up };
 				return true;
 			}
 			break;
 		case ControllerButton_BUTTON_RIGHTSHOULDER:
-			if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK)) {
+			if (select_modifier_active) {
 				if (!IsAutomapActive())
 					*action = GameActionSendMouseClick{ GameActionSendMouseClick::RIGHT, ctrl_event.up };
 				return true;
@@ -85,7 +114,7 @@ bool GetGameAction(const SDL_Event &event, GameAction *action)
 			break;
 		case ControllerButton_AXIS_TRIGGERLEFT: // ZL (aka L2)
 			if (!ctrl_event.up) {
-				if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK))
+				if (select_modifier_active)
 					*action = GameAction(GameActionType_TOGGLE_QUEST_LOG);
 				else
 					*action = GameAction(GameActionType_TOGGLE_CHARACTER_INFO);
@@ -93,33 +122,29 @@ bool GetGameAction(const SDL_Event &event, GameAction *action)
 			return true;
 		case ControllerButton_AXIS_TRIGGERRIGHT: // ZR (aka R2)
 			if (!ctrl_event.up) {
-				if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK))
+				if (select_modifier_active)
 					*action = GameAction(GameActionType_TOGGLE_SPELL_BOOK);
 				else
 					*action = GameAction(GameActionType_TOGGLE_INVENTORY);
 			}
 			return true;
 		case ControllerButton_BUTTON_LEFTSTICK:
-			if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK)) {
+			if (select_modifier_active) {
 				if (!IsAutomapActive())
 					*action = GameActionSendMouseClick{ GameActionSendMouseClick::LEFT, ctrl_event.up };
 				return true;
 			}
 			break;
+		case ControllerButton_IGNORE:
 		case ControllerButton_BUTTON_START:
-			if (IsControllerButtonPressed(ControllerButton_BUTTON_BACK)) {
-				*action = GameActionSendKey{ DVL_VK_ESCAPE, ctrl_event.up };
-			}
+		case ControllerButton_BUTTON_BACK:
 			return true;
 			break;
 		default:
 			break;
 		}
-		if (IsControllerButtonPressed(ControllerButton_BUTTON_START)) {
+		if (start_modifier_active) {
 			switch (ctrl_event.button) {
-			case ControllerButton_IGNORE:
-			case ControllerButton_BUTTON_START:
-				return true;
 			case ControllerButton_BUTTON_DPAD_UP:
 				*action = GameActionSendKey{ DVL_VK_ESCAPE, ctrl_event.up };
 				return true;
@@ -233,11 +258,12 @@ bool GetGameAction(const SDL_Event &event, GameAction *action)
 				break;
 			}
 		}
+
+		if (ctrl_event.button == ControllerButton_BUTTON_BACK) {
+			return true; // Ignore mod button
+		}
 	}
 
-	if (ctrl_event.button == ControllerButton_BUTTON_BACK) {
-		return true; // Ignore mod button
-	}
 
 	// By default, map to a keyboard key.
 	if (ctrl_event.button != ControllerButton_NONE) {

SourceX/miniwin/misc_msg.cpp
@@ -337,10 +337,7 @@ bool PeekMessage(LPMSG lpMsg)
 		return true;
 	}
 
-	if (ProcessControllerMotion(e)) {
-		ScaleJoysticks();
-		return true;
-	}
+	if (ProcessControllerMotion(e)) return true;
 
 	GameAction action;
 	if (GetGameAction(e, &action)) {

Source/scrollrt.cpp
@@ -153,7 +153,7 @@ static void scrollrt_draw_cursor_item()
 		return;
 	}
 
-	if (sgbControllerActive && pcurs != CURSOR_TELEPORT && !invflag && (!chrflag || plr[myplr]._pStatPts <= 0)) {
+	if (sgbControllerActive && !IsMovingMouseCursorWithController() && pcurs != CURSOR_TELEPORT && !invflag && (!chrflag || plr[myplr]._pStatPts <= 0)) {
 		return;
 	}
 

Source/scrollrt.h
@@ -12,7 +12,10 @@ DEVILUTION_BEGIN_NAMESPACE
 extern "C" {
 #endif
 
+// Defined in SourceX/controls/plctrls.cpp
 extern bool sgbControllerActive;
+extern bool IsMovingMouseCursorWithController();
+
 extern int light_table_index;
 extern BYTE *gpBufStart;
 extern BYTE *gpBufEnd;

SourceX/controls/plrctrls.cpp
@@ -963,6 +963,11 @@ bool IsAutomapActive()
 	return automapflag && leveltype != DTYPE_TOWN;
 }
 
+bool IsMovingMouseCursorWithController()
+{
+	return rightStickX != 0 || rightStickY != 0;
+}
+
 void HandleRightStickMotion()
 {
 	static RightStickAccumulator acc;

SourceX/controls/plrctrls.h
@@ -27,6 +27,9 @@ bool InGameMenu();
 // Whether the automap is being displayed.
 bool IsAutomapActive();
 
+// Whether the mouse cursor is being moved with the controller.
+bool IsMovingMouseCursorWithController();
+
 void UseBeltItem(int type);
 
 // Talk to towners, click on inv items, attack, etc.

commit ea6b2af26a98677138b65132ea72235db5568f45

Controller: Fix L2/R2 handling
Broken in https://github.com/diasurgical/devilutionX/commit/01401a16c70213db7a9a84196dee354a2ebc11a3

`ToControllerButtonEvent` must only be called once per SDL event because
of how it handled L2/R2

SourceX/controls/controller.h
@@ -9,6 +9,8 @@ struct ControllerButtonEvent {
 	bool up;
 };
 
+// NOTE: Not idempotent because of how it handles axis triggers.
+// Must be called exactly once per SDL input event.
 ControllerButtonEvent ToControllerButtonEvent(const SDL_Event &event);
 
 bool IsControllerButtonPressed(ControllerButton button);

SourceX/controls/controller_motion.cpp
@@ -56,10 +56,9 @@ void ScaleJoystickAxes(float *x, float *y, float deadzone)
 }
 
 // SELECT + D-Pad to simulate right stick movement.
-bool SimulateRightStickWithDpad(const SDL_Event &event)
+bool SimulateRightStickWithDpad(const SDL_Event &event, ControllerButtonEvent ctrl_event)
 {
 	static bool simulating = false;
-	const ControllerButtonEvent ctrl_event = ToControllerButtonEvent(event);
 	if (ctrl_event.button == ControllerButton_BUTTON_BACK) {
 		if (ctrl_event.up && simulating) {
 			rightStickX = rightStickY = 0;
@@ -121,7 +120,7 @@ void ScaleJoysticks()
 } // namespace
 
 // Updates motion state for mouse and joystick sticks.
-bool ProcessControllerMotion(const SDL_Event &event)
+bool ProcessControllerMotion(const SDL_Event &event, ControllerButtonEvent ctrl_event)
 {
 #ifndef USE_SDL1
 	if (ProcessGameControllerAxisMotion(event)) {
@@ -137,7 +136,7 @@ bool ProcessControllerMotion(const SDL_Event &event)
 	if (ProcessKbCtrlAxisMotion(event))
 		return true;
 #endif
-	return SimulateRightStickWithDpad(event);
+	return SimulateRightStickWithDpad(event, ctrl_event);
 }
 
 } // namespace dvl

SourceX/controls/controller_motion.h
@@ -3,6 +3,7 @@
 // Processes and stores mouse and joystick motion.
 
 #include "all.h"
+#include "controls/controller.h"
 
 namespace dvl {
 
@@ -16,6 +17,6 @@ extern float leftStickX, leftStickY, rightStickX, rightStickY;
 extern bool leftStickNeedsScaling, rightStickNeedsScaling;
 
 // Updates motion state for mouse and joystick sticks.
-bool ProcessControllerMotion(const SDL_Event &event);
+bool ProcessControllerMotion(const SDL_Event &event, ControllerButtonEvent ctrl_event);
 
 } // namespace dvl

SourceX/controls/game_controls.cpp
@@ -88,9 +88,8 @@ bool HandleStartAndSelect(const ControllerButtonEvent &ctrl_event, GameAction *a
 
 } // namespace
 
-bool GetGameAction(const SDL_Event &event, GameAction *action)
+bool GetGameAction(const SDL_Event &event, ControllerButtonEvent ctrl_event, GameAction *action)
 {
-	const ControllerButtonEvent ctrl_event = ToControllerButtonEvent(event);
 	const bool in_game_menu = InGameMenu();
 
 	if (HandleStartAndSelect(ctrl_event, action))

SourceX/controls/game_controls.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "all.h"
+#include "controls/controller.h"
 
 namespace dvl {
 
@@ -65,7 +66,7 @@ struct GameAction {
 	};
 };
 
-bool GetGameAction(const SDL_Event &event, GameAction *action);
+bool GetGameAction(const SDL_Event &event, ControllerButtonEvent ctrl_event, GameAction *action);
 
 enum MoveDirectionX {
 	MoveDirectionX_NONE = 0,

SourceX/miniwin/misc_msg.cpp
@@ -337,10 +337,12 @@ bool PeekMessage(LPMSG lpMsg)
 		return true;
 	}
 
-	if (ProcessControllerMotion(e)) return true;
+	const ControllerButtonEvent ctrl_event = ToControllerButtonEvent(e);
+	if (ProcessControllerMotion(e, ctrl_event))
+		return true;
 
 	GameAction action;
-	if (GetGameAction(e, &action)) {
+	if (GetGameAction(e, ctrl_event, &action)) {
 		if (action.type != GameActionType_NONE) {
 			sgbControllerActive = true;
 