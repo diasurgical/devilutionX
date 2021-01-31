diasurgical/devilutionX

commit 1f532040a84929d13410fdbf1c350166b492a2eb

Fix dynamically added lights being overwritten by default light
This issues could be seen when opening the final room in the Arkaine's
Valor quest.

Source/lighting.cpp
@@ -510,6 +510,22 @@ void RotateRadius(int *x, int *y, int *dx, int *dy, int *lx, int *ly, int *bx, i
 	}
 }
 
+void SetLight(int x, int y, char v)
+{
+	if (InitObjFlag)
+		dPreLight[x][y] = v;
+	else
+		dLight[x][y] = v;
+}
+
+char GetLight(int x, int y)
+{
+	if (InitObjFlag)
+		return dPreLight[x][y];
+	else
+		return dLight[x][y];
+}
+
 void DoLighting(int nXPos, int nYPos, int nRadius, int Lnum)
 {
 	int x, y, v, xoff, yoff, mult, radius_block;
@@ -562,9 +578,9 @@ void DoLighting(int nXPos, int nYPos, int nRadius, int Lnum)
 
 	if (nXPos >= 0 && nXPos < MAXDUNX && nYPos >= 0 && nYPos < MAXDUNY) {
 		if (currlevel < 17) {
-			dLight[nXPos][nYPos] = 0;
-		} else if (dLight[nXPos][nYPos] > lightradius[nRadius][0]) {
-			dLight[nXPos][nYPos] = lightradius[nRadius][0];
+			SetLight(nXPos, nYPos, 0);
+		} else if (GetLight(nXPos, nYPos) > lightradius[nRadius][0]) {
+			SetLight(nXPos, nYPos, lightradius[nRadius][0]);
 		}
 	}
 
@@ -577,8 +593,8 @@ void DoLighting(int nXPos, int nYPos, int nRadius, int Lnum)
 				temp_y = nYPos + y;
 				v = lightradius[nRadius][radius_block];
 				if (temp_x >= 0 && temp_x < MAXDUNX && temp_y >= 0 && temp_y < MAXDUNY)
-					if (v < dLight[temp_x][temp_y])
-						dLight[temp_x][temp_y] = v;
+					if (v < GetLight(temp_x, temp_y))
+						SetLight(temp_x, temp_y, v);
 			}
 		}
 	}
@@ -592,8 +608,8 @@ void DoLighting(int nXPos, int nYPos, int nRadius, int Lnum)
 				temp_y = nYPos - x;
 				v = lightradius[nRadius][radius_block];
 				if (temp_x >= 0 && temp_x < MAXDUNX && temp_y >= 0 && temp_y < MAXDUNY)
-					if (v < dLight[temp_x][temp_y])
-						dLight[temp_x][temp_y] = v;
+					if (v < GetLight(temp_x, temp_y))
+						SetLight(temp_x, temp_y, v);
 			}
 		}
 	}
@@ -607,8 +623,8 @@ void DoLighting(int nXPos, int nYPos, int nRadius, int Lnum)
 				temp_y = nYPos - y;
 				v = lightradius[nRadius][radius_block];
 				if (temp_x >= 0 && temp_x < MAXDUNX && temp_y >= 0 && temp_y < MAXDUNY)
-					if (v < dLight[temp_x][temp_y])
-						dLight[temp_x][temp_y] = v;
+					if (v < GetLight(temp_x, temp_y))
+						SetLight(temp_x, temp_y, v);
 			}
 		}
 	}
@@ -622,8 +638,8 @@ void DoLighting(int nXPos, int nYPos, int nRadius, int Lnum)
 				temp_y = nYPos + x;
 				v = lightradius[nRadius][radius_block];
 				if (temp_x >= 0 && temp_x < MAXDUNX && temp_y >= 0 && temp_y < MAXDUNY)
-					if (v < dLight[temp_x][temp_y])
-						dLight[temp_x][temp_y] = v;
+					if (v < GetLight(temp_x, temp_y))
+						SetLight(temp_x, temp_y, v);
 			}
 		}
 	}

commit 93b82e3b7a72d52490755dce58011d6731922614

Fix regression of static lights from fix of dynamic ones
This issue was introduced in 1f532040a84929d13410fdbf1c350166b492a2eb

Source/lighting.cpp
@@ -512,7 +512,7 @@ void RotateRadius(int *x, int *y, int *dx, int *dy, int *lx, int *ly, int *bx, i
 
 void SetLight(int x, int y, char v)
 {
-	if (InitObjFlag)
+	if (LoadMapObjsFlag)
 		dPreLight[x][y] = v;
 	else
 		dLight[x][y] = v;
@@ -520,7 +520,7 @@ void SetLight(int x, int y, char v)
 
 char GetLight(int x, int y)
 {
-	if (InitObjFlag)
+	if (LoadMapObjsFlag)
 		return dPreLight[x][y];
 	else
 		return dLight[x][y];

Source/objects.cpp
@@ -18,6 +18,7 @@ int leverid;
 int objectavail[MAXOBJECTS];
 ObjectStruct object[MAXOBJECTS];
 BOOL InitObjFlag;
+BOOL LoadMapObjsFlag;
 int numobjfiles;
 int dword_6DE0E0;
 
@@ -690,6 +691,7 @@ void LoadMapObjects(BYTE *pMap, int startx, int starty, int x1, int y1, int w, i
 	BYTE *lm;
 	long mapoff;
 
+	LoadMapObjsFlag = TRUE;
 	InitObjFlag = TRUE;
 
 	lm = pMap;
@@ -714,6 +716,7 @@ void LoadMapObjects(BYTE *pMap, int startx, int starty, int x1, int y1, int w, i
 		}
 	}
 	InitObjFlag = FALSE;
+	LoadMapObjsFlag = FALSE;
 }
 
 void LoadMapObjs(BYTE *pMap, int startx, int starty)
@@ -723,6 +726,7 @@ void LoadMapObjs(BYTE *pMap, int startx, int starty)
 	BYTE *lm;
 	long mapoff;
 
+	LoadMapObjsFlag = TRUE;
 	InitObjFlag = TRUE;
 	lm = pMap;
 	rw = *lm;
@@ -743,6 +747,7 @@ void LoadMapObjs(BYTE *pMap, int startx, int starty)
 		}
 	}
 	InitObjFlag = FALSE;
+	LoadMapObjsFlag = FALSE;
 }
 
 void AddDiabObjs()

Source/objects.h
@@ -17,6 +17,7 @@ extern int nobjects;
 extern int objectavail[MAXOBJECTS];
 extern ObjectStruct object[MAXOBJECTS];
 extern BOOL InitObjFlag;
+extern BOOL LoadMapObjsFlag;
 
 void InitObjectGFX();
 void FreeObjectGFX();