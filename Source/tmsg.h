6c5c4e20dc5d24aa198f0b3fb6b257049e5cf2f8
Anders Jenbo <anders@jenbo.dk>
2021-02-25 05:30:23 +0100

    Use sized types for network messages

--- Source/tmsg.h
+++ Source/tmsg.h
@@ -12,6 +12,23 @@ DEVILUTION_BEGIN_NAMESPACE
 extern "C" {
 #endif
 
+#pragma pack(push, 1)
+typedef struct TMsg TMsg;
+
+typedef struct TMsgHdr {
+	TMsg *pNext;
+	Sint32 dwTime;
+	Uint8 bLen;
+} TMsgHdr;
+
+typedef struct TMsg {
+	TMsgHdr hdr;
+	// this is actually alignment padding, but the message body is appended to the struct
+	// so it's convenient to use byte-alignment and name it "body"
+	Uint8 body[3];
+} TMsg;
+#pragma pack(pop)
+
 int tmsg_get(Uint8 *pbMsg, Uint32 dwMaxLen);
 void tmsg_add(Uint8 *pbMsg, Uint8 bLen);
 void tmsg_start();
