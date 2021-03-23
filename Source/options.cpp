bf135dd99ad205f4d3d094ce0aa364814ae354a5
Juliano Leal Goncalves <julealgon@gmail.com>
2021-03-22 22:54:27 -0300

    :building_construction: Add generic 'BooleanOption' abstraction
    Allows persisting and retrieving boolean options from the ini file automatically.

---
+++ Source/options.cpp
@@ -0,0 +1,59 @@
+#include "options.h"
+
+namespace dvl {
+
+void BooleanOption::Load(const std::string& sectionName)
+{
+	m_value = getIniBool(sectionName.c_str(), this->m_displayName.c_str(), this->m_defaultValue);
+}
+
+void BooleanOption::Save(const std::string& sectionName)
+{
+	setIniInt(sectionName.c_str(), this->m_displayName.c_str(), this->m_value);
+}
+
+BooleanOption& BooleanOption::operator = (bool value)
+{
+	m_value = value;
+
+	return *this;
+}
+
+OptionGroup::OptionGroup(const std::string& sectionName)
+    : m_sectionName(sectionName)
+{
+}
+
+void OptionGroup::Load()
+{
+	for (OptionBase& option : m_options) {
+		option.Load(m_sectionName);
+	}
+
+	for (OptionGroup& optionGroup : m_optionGroups) {
+		optionGroup.Load();
+	}
+}
+
+void OptionGroup::Save()
+{
+	for (OptionBase& option : m_options) {
+		option.Save(m_sectionName);
+	}
+
+	for (OptionGroup& optionGroup : m_optionGroups) {
+		optionGroup.Save();
+	}
+}
+
+void OptionGroup::AddOption(std::reference_wrapper<OptionBase> option)
+{
+	m_options.push_back(option);
+}
+
+void OptionGroup::AddOptionGroup(std::reference_wrapper<OptionGroup> optionGroup)
+{
+	m_optionGroups.push_back(optionGroup);
+}
+
+}
--- Source/options.h
+++ Source/options.h
@@ -1,6 +1,119 @@
 #pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+#include "all.h"
+#include "SDL_stdinc.h"
+#include "../3rdParty/Storm/Source/storm.h"
+
 DEVILUTION_BEGIN_NAMESPACE
+
+/**
+ * @brief Base non-templated class for all options. Provides saving and loading functionality and a way to group every
+ * type of option together.
+*/
+class OptionBase {
+private:
+	/**
+	 * @brief Loads data from the ini into this instance.
+	 * @param sectionName The name of the section that contains the data for this option.
+	*/
+	virtual void Load(const std::string& sectionName) = 0;
+
+	/**
+	 * @brief Saves the data contained in this instance to the appropriate section in the ini.
+	 * @param sectionName The name of the section where the data from this instance will be saved to.
+	*/
+	virtual void Save(const std::string& sectionName) = 0;
+
+	friend class OptionGroup;
+};
+
+/**
+ * @brief Base templated class for all options. Stores information about a given option, including its current value,
+ * default value and the name of the key inside the ini file that corresponds to it.
+ * @tparam T The underlying type of the setting.
+*/
+template <typename T>
+class Option : public OptionBase {
+public:
+	/**
+	 * @brief Initializes a new instance of the Option<T> with the specified display name and default value.
+	 * @param displayName The name to use when saving and loading the option to ini.
+	 * @param defaultValue The default value to use when the option does not yet exist.
+	*/
+	Option(const std::string& displayName, const T& defaultValue)
+	    : m_value(0)
+	    , m_displayName(displayName)
+	    , m_defaultValue(defaultValue)
+	{
+	}
+
+	/**
+	 * @brief Extracts the underlying value from this option instance.
+	 * @return The underlying value of the option.
+	*/
+	const T& operator*() const
+	{
+		return m_value;
+	}
+
+protected:
+	T m_value;
+	std::string m_displayName;
+	T m_defaultValue;
+};
+
+/**
+ * @brief A class that models a boolean option.
+*/
+class BooleanOption final : public Option<bool> {
+	using Option<bool>::Option;
+
+public:
+	/**
+	 * @brief Sets the underlying value of this option instance.
+	 * @param value The new value to be set as the current value of this option.
+	 * @return This instance with the new value set.
+	*/
+	BooleanOption& operator=(bool value);
+
+private:
+	void Load(const std::string &sectionName);
+	void Save(const std::string &sectionName);
+};
+
+/**
+ * @brief A class that models a group of options persisted in an ini section.
+*/
+class OptionGroup {
+public:
+	/**
+	 * @brief Initializes a new instance of the option group with the specified section name.
+	 * @param sectionName The name of the ini section used to read and write values.
+	*/
+	OptionGroup(const std::string& sectionName);
+
+	/**
+	 * @brief Loads the data from the ini file into this option group.
+	*/
+	void Load();
+
+	/**
+	 * @brief Saves the data stored in this option group to the ini file.
+	*/
+	void Save();
+
+protected:
+	void AddOption(std::reference_wrapper<OptionBase>);
+	void AddOptionGroup(std::reference_wrapper<OptionGroup>);
+
+private:
+	std::vector<std::reference_wrapper<OptionBase>> m_options;
+	std::vector<std::reference_wrapper<OptionGroup>> m_optionGroups;
+	std::string m_sectionName;
+};
 
 class AudioOptions final {
 public:
