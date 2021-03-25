7a2166ba29d8b9b0f08f883c014a25957bc38c3a
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-03-25 01:55:09 +0000

    Migrate credits.cpp to SDLSurfaceUniquePtr

--- SourceX/display.cpp
+++ SourceX/display.cpp
@@ -301,6 +301,17 @@ SDL_Surface *CreateScaledSurface(SDL_Surface *src)
 } // namespace
 #endif // USE_SDL1
 
+SDLSurfaceUniquePtr ScaleSurfaceToOutput(SDLSurfaceUniquePtr surface)
+{
+#ifdef USE_SDL1
+	SDL_Surface *ptr = surface.release();
+	ScaleSurfaceToOutput(&ptr);
+	return SDLSurfaceUniquePtr{ptr};
+#else
+	return surface;
+#endif
+}
+
 void ScaleSurfaceToOutput(SDL_Surface **surface)
 {
 #ifdef USE_SDL1
