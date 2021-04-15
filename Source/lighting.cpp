e24f3c83debf0108ea4c144306f7610c5058409b
qndel <stefan551@o2.pl>
2021-04-15 18:29:32 +0200

    fix automap

--- Source/lighting.cpp
+++ Source/lighting.cpp
@@ -678,7 +678,7 @@ void DoVision(int nXPos, int nYPos, int nRadius, bool doautomap, bool visible)
 	if (nXPos >= 0 && nXPos <= MAXDUNX && nYPos >= 0 && nYPos <= MAXDUNY) {
 		if (doautomap) {
 			if (dFlags[nXPos][nYPos] >= 0) {
-				SetAutomapView(nXPos, nXPos);
+				SetAutomapView(nXPos, nYPos);
 			}
 			dFlags[nXPos][nYPos] |= BFLAG_EXPLORED;
 		}
