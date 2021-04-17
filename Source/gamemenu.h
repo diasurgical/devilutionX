0e94302317e2c5ab57c52603f4ead7d457fe2428
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-04-17 01:19:10 +0100

    gamenu.cpp: Move most functions to an anonymous namespace

--- Source/gamemenu.h
+++ Source/gamemenu.h
@@ -10,18 +10,5 @@ namespace devilution {
 void gamemenu_on();
 void gamemenu_off();
 void gamemenu_handle_previous();
-void gamemenu_previous(bool bActivate);
-void gamemenu_new_game(bool bActivate);
-void gamemenu_quit_game(bool bActivate);
-void gamemenu_load_game(bool bActivate);
-void gamemenu_save_game(bool bActivate);
-void gamemenu_restart_town(bool bActivate);
-void gamemenu_options(bool bActivate);
-void gamemenu_music_volume(bool bActivate);
-void gamemenu_sound_volume(bool bActivate);
-void gamemenu_loadjog(bool bActivate);
-void gamemenu_gamma(bool bActivate);
-void gamemenu_speed(bool bActivate);
-void gamemenu_color_cycling(bool bActivate);
 
-}
+} // namespace devilution
