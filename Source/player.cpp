006fa396917e716633029e7655b13ce446095b66
staphen <staphen@gmail.com>
2021-04-18 12:27:39 -0400

    Use SetPlayerHitPoints() for minHP handling in ApplyPlrDamage().

--- Source/player.cpp
+++ Source/player.cpp
@@ -2050,8 +2050,7 @@ void ApplyPlrDamage(int pnum, int dam, int minHP /*= 0*/, int frac /*= 0*/, int 
 	}
 	int minHitPoints = minHP << 6;
 	if (plr[pnum]._pHitPoints < minHitPoints) {
-		plr[pnum]._pHitPoints = minHitPoints;
-		plr[pnum]._pHPBase = minHitPoints + plr[pnum]._pMaxHPBase - plr[pnum]._pMaxHP;
+		SetPlayerHitPoints(pnum, minHitPoints);
 	}
 	if (plr[pnum]._pHitPoints >> 6 <= 0) {
 		SyncPlrKill(pnum, earflag);
