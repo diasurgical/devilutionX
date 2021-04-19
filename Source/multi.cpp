c1e417294d519ac84b6ddb876eef7faac1255148
FluffyQuack <git@fluffyquack.com>
2021-04-19 20:02:30 +0200

    Rewrite to make PR cleaner and take less space.

--- Source/multi.cpp
+++ Source/multi.cpp
@@ -116,29 +116,21 @@ static void NetRecvPlrData(TPkt *pkt)
 {
 	/*
 	* We calculate _ptargx and _ptargy here and prepare for it to be sent to other clients.
-	* If px/py and ptarx/ptary don't match on other clients, then they'll move our
-	* local player to that location.
+	* If px/py and ptarx/ptary don't match on other clients, then they'll move our local player to that location.
 	*/
 	{
-		int realTargetX = plr[myplr]._pfutx;
-		int realTargetY = plr[myplr]._pfuty;
+		// clang-format off
+		int directionOffsetX[8] = {  0,-1, 1, 0,-1, 1, 1,-1 };
+		int directionOffsetY[8] = { -1, 0, 0, 1,-1,-1, 1, 1 };
+		// clang-format on
+		plr[myplr]._ptargx = plr[myplr]._pfutx;
+		plr[myplr]._ptargy = plr[myplr]._pfuty;
 		for (int i = 0; i < MAX_PATH_LENGTH; i++) {
 			if (plr[myplr].walkpath[i] == WALK_NONE)
 				break;
-
-			if (plr[myplr].walkpath[i] == WALK_N || plr[myplr].walkpath[i] == WALK_NW || plr[myplr].walkpath[i] == WALK_W)
-				realTargetX--;
-			else if (plr[myplr].walkpath[i] == WALK_E || plr[myplr].walkpath[i] == WALK_SE || plr[myplr].walkpath[i] == WALK_S)
-				realTargetX++;
-
-			if (plr[myplr].walkpath[i] == WALK_N || plr[myplr].walkpath[i] == WALK_NE || plr[myplr].walkpath[i] == WALK_E)
-				realTargetY--;
-			else if (plr[myplr].walkpath[i] == WALK_S || plr[myplr].walkpath[i] == WALK_SW || plr[myplr].walkpath[i] == WALK_W)
-				realTargetY++;
+			plr[myplr]._ptargx += directionOffsetX[plr[myplr].walkpath[i] - 1];
+			plr[myplr]._ptargy += directionOffsetY[plr[myplr].walkpath[i] - 1];
 		}
-
-		plr[myplr]._ptargx = realTargetX;
-		plr[myplr]._ptargy = realTargetY;
 	}
 
 	pkt->hdr.wCheck = LOAD_BE32("\0\0ip");
