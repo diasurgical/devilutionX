64634a0f78543a6fee1749ffcf7100498778ea2d
Chance4us <wolfgruen29@gmail.com>
2021-04-20 00:48:56 +0200

    reformat

--- Source/objects.cpp
+++ Source/objects.cpp
@@ -4891,9 +4891,9 @@ bool objectIsDisabled(int i)
 		return true;
 	if ((object[i]._otype != OBJ_SHRINEL) && (object[i]._otype != OBJ_SHRINER))
 		return false;
-	if ((object[i]._oVar1 == SHRINE_FASCINATING)
-		|| (object[i]._oVar1 == SHRINE_ORNATE)
-		|| (object[i]._oVar1 == SHRINE_SACRED))
+	if ((object[i]._oVar1 == SHRINE_FASCINATING) ||
+		(object[i]._oVar1 == SHRINE_ORNATE) ||
+		(object[i]._oVar1 == SHRINE_SACRED))
 		return true;
 	return false;
 }
