225a625617137fbd2943a58f6c8c689ae2fd95d1
John Törnblom <john.tornblom@gmail.com>
2021-04-24 15:24:26 +0200

    format syntax to vscode config

--- Source/language.cpp
+++ Source/language.cpp
@@ -2,124 +2,119 @@
 #include "utils/paths.h"
 #include <map>
 
-
 using namespace devilution;
 #define MO_MAGIC 0x950412de
 
-
 namespace {
 
-  struct cstring_cmp {
-    bool operator()(const char *s1, const char *s2) const {
-      return strcmp(s1, s2) < 0;
-    }
-  };
-  
-  std::map<const char*, const char*, cstring_cmp> map;
+struct cstring_cmp {
+	bool operator()(const char *s1, const char *s2) const
+	{
+		return strcmp(s1, s2) < 0;
+	}
+};
 
-  
-  struct mo_head {
-    uint32_t magic;
-    struct {
-      uint16_t major;
-      uint16_t minor;
-    } revision;
-  
-    uint32_t nb_mappings;
-    uint32_t src_offset;
-    uint32_t dst_offset;
-  };
+std::map<const char *, const char *, cstring_cmp> map;
 
-  
-  struct mo_entry {
-    uint32_t length;
-    uint32_t offset;
-  };
+struct mo_head {
+	uint32_t magic;
+	struct {
+		uint16_t major;
+		uint16_t minor;
+	} revision;
 
+	uint32_t nb_mappings;
+	uint32_t src_offset;
+	uint32_t dst_offset;
+};
 
-  char* read_entry(FILE *fp, mo_entry* e) {
-    void *data;
-    
-    if(fseek(fp, e->offset, SEEK_SET)) {
-      return nullptr;
-    }
+struct mo_entry {
+	uint32_t length;
+	uint32_t offset;
+};
 
-    if(!(data = calloc(e->length, sizeof(char)))) {
-      return nullptr;
-    }
-    
-    if(fread(data, sizeof(char), e->length, fp) != e->length) {
-      free(data);
-      return nullptr;
-    }
+char *read_entry(FILE *fp, mo_entry *e)
+{
+	void *data;
 
-    return static_cast<char*>(data);
-  }
+	if (fseek(fp, e->offset, SEEK_SET)) {
+		return nullptr;
+	}
+
+	if (!(data = calloc(e->length, sizeof(char)))) {
+		return nullptr;
+	}
+
+	if (fread(data, sizeof(char), e->length, fp) != e->length) {
+		free(data);
+		return nullptr;
+	}
+
+	return static_cast<char *>(data);
+}
+} // namespace
+
+const char *LanguageTranslate(const char *key)
+{
+	auto it = map.find(key);
+	if (it == map.end()) {
+		return key;
+	}
+
+	return it->second;
 }
 
+void LanguageInitialize()
+{
+	auto path = GetLangPath() + "./" + sgOptions.Language.szCode + ".gmo";
+	mo_head head;
+	FILE *fp;
 
-const char* LanguageTranslate(const char* key) {
-  auto it = map.find(key);
-  if(it == map.end()) {
-    return key;
-  }
+	if (!(fp = fopen(path.c_str(), "rb"))) {
+		perror(path.c_str());
+		return;
+	}
+	// Read header and do sanity checks
+	if (fread(&head, sizeof(mo_head), 1, fp) != 1) {
+		return;
+	}
 
-  return it->second;
+	if (head.magic != MO_MAGIC) {
+		return; // not a MO file
+	}
+
+	if (head.revision.major > 1 || head.revision.minor > 1) {
+		return; // unsupported revision
+	}
+
+	// Read entries of source strings
+	mo_entry src[head.nb_mappings];
+	if (fseek(fp, head.src_offset, SEEK_SET)) {
+		return;
+	}
+	if (fread(src, sizeof(mo_entry), head.nb_mappings, fp) != head.nb_mappings) {
+		return;
+	}
+
+	// Read entries of target strings
+	mo_entry dst[head.nb_mappings];
+	if (fseek(fp, head.dst_offset, SEEK_SET)) {
+		return;
+	}
+
+	if (fread(dst, sizeof(mo_entry), head.nb_mappings, fp) != head.nb_mappings) {
+		return;
+	}
+
+	// Read strings described by entries
+	for (uint32_t i = 0; i < head.nb_mappings; i++) {
+		char *key, *val;
+		if ((key = read_entry(fp, src + i))) {
+			if ((val = read_entry(fp, dst + i)) && *key) {
+				map[key] = val;
+			} else {
+				free(key);
+			}
+		}
+	}
 }
-
-
-void LanguageInitialize() {
-  std::string path = GetLangPath() + "./" + sgOptions.Language.szCode + ".gmo";
-  mo_head head;
-  FILE *fp;
-  
-  if(!(fp = fopen(path.c_str(), "rb"))) {
-    perror(path.c_str());
-    return;
-  }
-  // Read header and do sanity checks
-  if(fread(&head, sizeof(mo_head), 1, fp) != 1) {
-    return;
-  }
-  
-  if(head.magic != MO_MAGIC) {
-    return; // not a MO file
-  }
-  
-  if(head.revision.major > 1 || head.revision.minor > 1) {
-    return; // unsupported revision
-  }
-  
-  // Read entries of source strings
-  mo_entry src[head.nb_mappings];
-  if(fseek(fp, head.src_offset, SEEK_SET)) {
-    return;
-  }
-  if(fread(src, sizeof(mo_entry), head.nb_mappings, fp) != head.nb_mappings) {
-    return;
-  }
-  
-  // Read entries of target strings
-  mo_entry dst[head.nb_mappings];
-  if(fseek(fp, head.dst_offset, SEEK_SET)) {
-    return;
-  }
-  
-  if(fread(dst, sizeof(mo_entry), head.nb_mappings, fp) != head.nb_mappings) {
-    return;
-  }
-
-  // Read strings described by entries
-  for(uint32_t i=0; i<head.nb_mappings; i++) {
-    char *key, *val;
-    if((key = read_entry(fp, src+i))) {
-      if((val = read_entry(fp, dst+i)) && *key) {
-	map[key] = val;
-      } else {
-	free(key);
-      }
-    }
-  }
-}	
-
-
