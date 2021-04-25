2b672289e17c18e140ba4f9b79cc5c873d3d5093
Jmgr <jmgr@jmgr.info>
2021-04-25 20:30:56 +0100

    Fix optional being in experimental/ with some compilers

---
+++ Source/utils/stdcompat/optional.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __has_include
+#if defined(__cplusplus) && __cplusplus >= 201606L && __has_include(<optional>)
+#include <optional> // IWYU pragma: export
+#elif __has_include(<experimental/optional>)
+#include <experimental/optional> // IWYU pragma: export
+#define optional experimental::optional
+#else
+#error "Missing support for <optional> or <experimental/optional>"
+#endif
+#else
+#error "__has_include unavailable"
+#endif
