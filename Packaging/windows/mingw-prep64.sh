595e4649192e23990429f23f25bf9f8f9ec825dc
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-04-25 23:37:40 +0100

    🧹 mingw: Fix `.cmake` config files prefix
    Works around https://github.com/libsdl-org/SDL/issues/3665 by using
    sdl2-config.cmake.in from https://github.com/libsdl-org/SDL/pull/4320

--- Packaging/windows/mingw-prep64.sh
+++ Packaging/windows/mingw-prep64.sh
@@ -3,6 +3,8 @@
 # exit when any command fails
 set -euo pipefail
 
+MINGW_PREFIX=/usr/x86_64-w64-mingw32/
+
 wget https://www.libsdl.org/release/SDL2-devel-2.0.14-mingw.tar.gz
 tar -xzf SDL2-devel-2.0.14-mingw.tar.gz
 wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.15-mingw.tar.gz
@@ -15,5 +17,19 @@ sudo cp -r libsodium-win64/* /usr/x86_64-w64-mingw32
 sudo cp -r SDL2*/x86_64-w64-mingw32 /usr
 
 # Fixup pkgconfig prefix:
-find "/usr/x86_64-w64-mingw32/lib/pkgconfig/" -name '*.pc' -exec \
-  sed -i "s|^prefix=.*|prefix=/usr/x86_64-w64-mingw32|" '{}' \;
+find "${MINGW_PREFIX}/lib/pkgconfig/" -name '*.pc' -exec \
+  sed -i "s|^prefix=.*|prefix=${MINGW_PREFIX}|" '{}' \;
+
+# Fixup CMake prefix:
+find "$MINGW_PREFIX" -name '*.cmake' -exec \
+  sed -i "s|/opt/local/x86_64-w64-mingw32|${MINGW_PREFIX}|" '{}' \;
+
+# Fixup SDL2's cmake file
+# See https://github.com/libsdl-org/SDL/issues/3665
+sed "s|@prefix@|${MINGW_PREFIX}|;
+s|@exec_prefix@|\${prefix}|;
+s|@libdir@|\${exec_prefix}/lib|;
+s|@includedir@|\${prefix}/include|;
+s|@SDL_RLD_FLAGS@||;
+s|@SDL_LIBS@||;
+" "$(dirname "$0")"/sdl2-config.cmake.in > "${MINGW_PREFIX}/lib/cmake/SDL2/sdl2-config.cmake"
