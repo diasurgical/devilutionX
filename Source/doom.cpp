1c3395ced080ed1159b912e0148671e7baf26de3
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2021-04-30 05:04:25 +0300

    Cleanup of doom.cpp

--- Source/doom.cpp
+++ Source/doom.cpp
@@ -11,27 +11,25 @@ namespace devilution {
 
 int doom_quest_time;
 int doom_stars_drawn;
-BYTE *pDoomCel;
+std::unique_ptr<uint8_t[]> doomCel;
 bool doomflag;
 int DoomQuestState;
 
 /*
 void doom_reset_state()
 {
-    if (DoomQuestState <= 0) {
-        DoomQuestState = 0;
-    }
+	DoomQuestState = std::max(DoomQuestState, 0);
 }
 
 void doom_play_movie()
 {
-    if (DoomQuestState < 36001) {
+	if (DoomQuestState >= 36001)
+		return;
         DoomQuestState++;
-        if (DoomQuestState == 36001) {
-            PlayInGameMovie("gendata\\doom.smk");
-            DoomQuestState++;
-        }
-    }
+	if (DoomQuestState == 36001) {
+		PlayInGameMovie("gendata\\doom.smk");
+		DoomQuestState++;
+	}
 }
 */
 
@@ -44,48 +42,32 @@ int doom_get_frame_from_time()
 	return DoomQuestState / 1200;
 }
 
-void doom_cleanup()
-{
-	if (pDoomCel != nullptr) {
-		MemFreeDbg(pDoomCel);
-		pDoomCel = nullptr;
-	}
-}
-
-static bool doom_alloc_cel()
-{
-	doom_cleanup();
-	pDoomCel = DiabloAllocPtr(0x39000);
-	return pDoomCel != nullptr;
-}
-
 static bool doom_load_graphics()
 {
 	bool ret;
 
 	ret = false;
 	strcpy(tempstr, "Items\\Map\\MapZtown.CEL");
-	if (LoadFileWithMem(tempstr, pDoomCel) != 0)
+	if (LoadFileWithMem(tempstr, doomCel.get()) != 0)
 		ret = true;
 	return ret;
 }
 
 void doom_init()
 {
-	if (doom_alloc_cel()) {
-		doom_quest_time = doom_get_frame_from_time() == 31 ? 31 : 0;
-		if (doom_load_graphics()) {
-			doomflag = true;
-		} else {
-			doom_close();
-		}
+	doomCel = std::make_unique<uint8_t[]>(0x39000);
+	doom_quest_time = doom_get_frame_from_time() == 31 ? 31 : 0;
+	if (doom_load_graphics()) {
+		doomflag = true;
+	} else {
+		doom_close();
 	}
 }
 
 void doom_close()
 {
 	doomflag = false;
-	doom_cleanup();
+	doomCel.reset();
 }
 
 void doom_draw(const CelOutputBuffer &out)
@@ -94,7 +76,7 @@ void doom_draw(const CelOutputBuffer &out)
 		return;
 	}
 
-	CelDrawTo(out, PANEL_X, PANEL_Y - 1, pDoomCel, 1, 640);
+	CelDrawTo(out, PANEL_X, PANEL_Y - 1, doomCel.get(), 1, 640);
 }
 
 } // namespace devilution
