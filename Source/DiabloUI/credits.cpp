2b7fad2a68c8056625ae49e324ba7b12c7f726ac
Anders Jenbo <anders@jenbo.dk>
2021-04-30 21:30:31 +0200

    :bug: Do not render text as UTF8 for now

--- Source/DiabloUI/credits.cpp
+++ Source/DiabloUI/credits.cpp
@@ -57,7 +57,7 @@ SDL_Surface *RenderText(const char *text, SDL_Color color)
 {
 	if (text[0] == '\0')
 		return nullptr;
-	SDL_Surface *result = TTF_RenderUTF8_Solid(font, text, color);
+	SDL_Surface *result = TTF_RenderText_Solid(font, text, color);
 	if (result == nullptr)
 		Log("{}", TTF_GetError());
 	return result;
