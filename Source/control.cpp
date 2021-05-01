6efcd5231f5818f30befb750d00acabd2caa75c3
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2021-05-02 02:54:05 +0300

    Use strncpy when copying from string_view

--- Source/control.cpp
+++ Source/control.cpp
@@ -1290,7 +1290,8 @@ void DrawInfoBox(const CelOutputBuffer &out)
 					PrintMonstHistory(monster[pcursmonst].MType->mtype);
 				}
 			} else if (pcursitem == -1) {
-				strcpy(infostr, towners[pcursmonst]._tName.data());
+				std::string_view townerName = towners[pcursmonst]._tName;
+				strncpy(infostr, townerName.data(), townerName.length());
 			}
 		}
 		if (pcursplr != -1) {
