38cf99a4233f673cac918356684b231153d2de42
Trihedraf <forrestgherr@gmail.com>
2021-05-02 03:55:36 -0700

    Add IsWriteable to file_util.cpp

--- Source/utils/file_util.h
+++ Source/utils/file_util.h
@@ -10,6 +10,7 @@
 namespace devilution {
 
 bool FileExists(const char *path);
+bool FileExistsAndIsWriteable(const char *path);
 bool GetFileSize(const char *path, std::uintmax_t *size);
 bool ResizeFile(const char *path, std::uintmax_t size);
 void RemoveFile(const char *lpFileName);
