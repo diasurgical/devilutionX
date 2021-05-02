4fad8023ff44d9126fa569755703b0d3500e89a3
Jmgr <jmgr@jmgr.info>
2021-05-02 11:55:49 +0100

    Fix fmt issue with a nullptr controller mapping

--- Source/controls/devices/game_controller.cpp
+++ Source/controls/devices/game_controller.cpp
@@ -169,7 +169,9 @@ void GameController::Add(int joystickIndex)
 
 	const SDL_JoystickGUID guid = SDL_JoystickGetGUID(sdlJoystick);
 	SDLUniquePtr<char> mapping { SDL_GameControllerMappingForGUID(guid) };
-	Log("Opened game controller with mapping:\n{}", mapping.get());
+	if (mapping) {
+		Log("Opened game controller with mapping:\n{}", mapping.get());
+	}
 }
 
 void GameController::Remove(SDL_JoystickID instanceId)
