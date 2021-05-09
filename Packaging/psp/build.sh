5c445ae18c8899bb7c869a2cdcf62c6f4c2f9763
Stefan Mielke <stefanmielke@fastmail.com>
2021-05-09 16:02:34 -0300

    setting build.sh to be run from root folder

--- Packaging/psp/build.sh
+++ Packaging/psp/build.sh
@@ -1,4 +1,2 @@
-mkdir -p ../../build
-cd ../../build
-psp-cmake .. -DPSPDEV=/usr/local/pspdev -DPSP=ON -DCMAKE_BUILD_TYPE=Release
+psp-cmake . -DPSPDEV=/usr/local/pspdev -DPSP=ON -DCMAKE_BUILD_TYPE=Release
 make
