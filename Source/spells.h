1c3dd544a7a358ddef95f7c61ae0611f613a89bf
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2021-05-25 09:47:04 +0300

    Make GetSpellBitmask constexpr

--- Source/spells.h
+++ Source/spells.h
@@ -11,7 +11,6 @@ namespace devilution {
 
 int GetManaAmount(int id, spell_id sn);
 void UseMana(int id, spell_id sn);
-uint64_t GetSpellBitmask(int spellId);
 bool CheckSpell(int id, spell_id sn, spell_type st, bool manaonly);
 void EnsureValidReadiedSpell(PlayerStruct &player);
 void CastSpell(int id, int spl, int sx, int sy, int dx, int dy, int spllvl);
@@ -19,5 +18,18 @@ void DoResurrect(int pnum, int rid);
 void DoHealOther(int pnum, int rid);
 int GetSpellBookLevel(spell_id s);
 int GetSpellStaffLevel(spell_id s);
+
+/**
+ * @brief Gets a value that represents the specified spellID in 64bit bitmask format.
+ * For example:
+ *  - spell ID  1: 0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0000.0001
+ *  - spell ID 43: 0000.0000.0000.0000.0000.0100.0000.0000.0000.0000.0000.0000.0000.0000.0000.0000
+ * @param spellId The id of the spell to get a bitmask for.
+ * @return A 64bit bitmask representation for the specified spell.
+ */
+constexpr uint64_t GetSpellBitmask(int spellId)
+{
+	return 1ULL << (spellId - 1);
+}
 
 } // namespace devilution
