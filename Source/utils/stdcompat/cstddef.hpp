057598781c7db0f5466f50b4ef08ffabb3b0ece0
Kalebe Alves <kalebeapsoares@gmail.com>
2021-05-27 12:53:53 -0300

    Rework for #2035

--- Source/utils/stdcompat/cstddef.hpp
+++ Source/utils/stdcompat/cstddef.hpp
@@ -1,16 +1,14 @@
 #pragma once
 
 #ifdef __has_include
-#if __has_include(<cstddef>)
 #include <cstddef>
 #if defined(__cplusplus) && __cplusplus >= 201703L
 namespace devilution {
-    using byte = std::byte;
+using byte = std::byte;
 }
 #else
 namespace devilution {
-    using byte = uint8_t;
+using byte = uint8_t;
 }
-#endif
 #endif
 #endif
