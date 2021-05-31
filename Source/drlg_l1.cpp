833f87fcf3a945b6670a62be2e68524bd211b157
qndel <stefan551@o2.pl>
2021-05-31 15:01:08 +0200

    crypt fix

--- Source/drlg_l1.cpp
+++ Source/drlg_l1.cpp
@@ -1950,17 +1950,21 @@ void drlg_l1_crypt_rndset(const BYTE *miniset, int rndper)
 			int kk = sw * sh + 2;
 			if (miniset[kk] >= 84 && miniset[kk] <= 100 && found) {
 				// BUGFIX: accesses to dungeon can go out of bounds (fixed)
-				// BUGFIX: Comparisons vs 100 should use same tile as comparisons vs 84 (fixed)
-				if (sx > 0 && dungeon[sx - 1][sy] >= 84 && dungeon[sx - 1][sy] <= 100) {
+				// BUGFIX: Comparisons vs 100 should use same tile as comparisons vs 84 - NOT A BUG - "fixing" this breaks crypt
+
+				auto comparison_with_bounds_check = [](Point p1, Point p2) {
+					return (p1.x >= 0 && p1.x < DMAXX && p1.y >= 0 && p1.y < DMAXY) && (p2.x >= 0 && p2.x < DMAXX && p2.y >= 0 && p2.y < DMAXY) && (dungeon[p1.x][p1.y] >= 84 && dungeon[p2.x][p2.y] <= 100);
+				};
+				if (comparison_with_bounds_check({ sx - 1, sy }, { sx - 1, sy })) {
 					found = false;
 				}
-				if (sx < DMAXX - 1 && dungeon[sx + 1][sy] >= 84 && dungeon[sx + 1][sy] <= 100) {
+				if (comparison_with_bounds_check({ sx + 1, sy }, { sx - 1, sy })) {
 					found = false;
 				}
-				if (sy < DMAXY - 1 && dungeon[sx][sy + 1] >= 84 && dungeon[sx][sy + 1] <= 100) {
+				if (comparison_with_bounds_check({ sx, sy + 1 }, { sx - 1, sy })) {
 					found = false;
 				}
-				if (sy > 0 && dungeon[sx][sy - 1] >= 84 && dungeon[sx][sy - 1] <= 100) {
+				if (comparison_with_bounds_check({ sx, sy - 1 }, { sx - 1, sy })) {
 					found = false;
 				}
 			}
