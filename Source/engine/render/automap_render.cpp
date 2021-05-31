608c231f467b855ff5ac81315e0e6b66e63d5226
Chance4us <45902449+Chance4us@users.noreply.github.com>
2021-05-31 16:09:55 +0200

    Draw black drop shadow for automap (#2090)

--- Source/engine/render/automap_render.cpp
+++ Source/engine/render/automap_render.cpp
@@ -22,12 +22,15 @@ template <DirectionX DirX, DirectionY DirY>
 void DrawMapLine(const CelOutputBuffer &out, Point from, int height, std::uint8_t colorIndex)
 {
 	while (height-- > 0) {
+		out.SetPixel({ from.x, from.y + 1 }, 0);
 		out.SetPixel(from, colorIndex);
 		from.x += static_cast<int>(DirX);
+		out.SetPixel({ from.x, from.y + 1 }, 0);
 		out.SetPixel(from, colorIndex);
 		from.x += static_cast<int>(DirX);
 		from.y += static_cast<int>(DirY);
 	}
+	out.SetPixel({ from.x, from.y + 1 }, 0);
 	out.SetPixel(from, colorIndex);
 }
 
