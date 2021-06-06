d93e1fdf076fcac3f11f7c7c433b5c3f7ab8bf53
Juliano Leal Goncalves <julealgon@gmail.com>
2021-06-05 21:52:48 -0300

    ✨ Add 'Contains' function for 'Rectangle'
    This will be useful for various bound checks throughout the code.

--- Source/engine/rectangle.hpp
+++ Source/engine/rectangle.hpp
@@ -8,6 +8,14 @@ namespace devilution {
 struct Rectangle {
 	Point position;
 	Size size;
+
+	constexpr bool Contains(Point point) const
+	{
+		return point.x >= this->position.x
+		    && point.x <= (this->position.x + this->size.width)
+		    && point.y >= this->position.y
+		    && point.y <= (this->position.y + this->size.height);
+	}
 };
 
 } // namespace devilution
