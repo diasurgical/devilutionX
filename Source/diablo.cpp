cb37bac1cd37ea81d2c2493c6d7e0c2024742c62
Ivan Epifanov <isage.dna@gmail.com>
2021-06-15 19:08:12 +0300

    Vita: re-enable graphical options, force fullscreen to always true

--- Source/diablo.cpp
+++ Source/diablo.cpp
@@ -507,14 +507,12 @@ static void SaveOptions()
 	setIniInt("Audio", "Channels", sgOptions.Audio.nChannels);
 	setIniInt("Audio", "Buffer Size", sgOptions.Audio.nBufferSize);
 	setIniInt("Audio", "Resampling Quality", sgOptions.Audio.nResamplingQuality);
-#ifndef __vita__
 	setIniInt("Graphics", "Width", sgOptions.Graphics.nWidth);
 	setIniInt("Graphics", "Height", sgOptions.Graphics.nHeight);
-#endif
-	setIniInt("Graphics", "Fullscreen", sgOptions.Graphics.bFullscreen);
 #ifndef __vita__
-	setIniInt("Graphics", "Upscale", sgOptions.Graphics.bUpscale);
+	setIniInt("Graphics", "Fullscreen", sgOptions.Graphics.bFullscreen);
 #endif
+	setIniInt("Graphics", "Upscale", sgOptions.Graphics.bUpscale);
 	setIniInt("Graphics", "Fit to Screen", sgOptions.Graphics.bFitToScreen);
 	setIniValue("Graphics", "Scaling Quality", sgOptions.Graphics.szScaleQuality);
 	setIniInt("Graphics", "Integer Scaling", sgOptions.Graphics.bIntegerScaling);
@@ -587,15 +585,14 @@ static void LoadOptions()
 	sgOptions.Audio.nBufferSize = getIniInt("Audio", "Buffer Size", DEFAULT_AUDIO_BUFFER_SIZE);
 	sgOptions.Audio.nResamplingQuality = getIniInt("Audio", "Resampling Quality", DEFAULT_AUDIO_RESAMPLING_QUALITY);
 
-#ifndef __vita__
 	sgOptions.Graphics.nWidth = getIniInt("Graphics", "Width", DEFAULT_WIDTH);
 	sgOptions.Graphics.nHeight = getIniInt("Graphics", "Height", DEFAULT_HEIGHT);
-#else
-	sgOptions.Graphics.nWidth = DEFAULT_WIDTH;
-	sgOptions.Graphics.nHeight = DEFAULT_HEIGHT;
-#endif
+#ifndef __vita__
 	sgOptions.Graphics.bFullscreen = getIniBool("Graphics", "Fullscreen", true);
-#if !defined(USE_SDL1) && !defined(__vita__)
+#else
+	sgOptions.Graphics.bFullscreen = true;
+#endif
+#if !defined(USE_SDL1)
 	sgOptions.Graphics.bUpscale = getIniBool("Graphics", "Upscale", true);
 #else
 	sgOptions.Graphics.bUpscale = false;
