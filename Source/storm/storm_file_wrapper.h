cdbb56150ceb85a97353e5e54600cf8d2d9b0eb3
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-06-27 14:33:55 +0100

    Enable video streaming for Haiku (#1825)

--- Source/storm/storm_file_wrapper.h
+++ Source/storm/storm_file_wrapper.h
@@ -1,6 +1,6 @@
 /** A pointer to a Storm file as a `FILE *`. Only available on some platforms. */
 #pragma once
-#if (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__) || defined(__DragonFly__)
+#if (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__HAIKU__)
 #include <cstdio>
 
 #include "miniwin/miniwin.h"
