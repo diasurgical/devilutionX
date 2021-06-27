db3d1d405463769329478420c38d13b3767b4485
Anders Jenbo <anders@jenbo.dk>
2021-06-27 12:03:11 +0200

    Revert and disable readability-convert-member-functions-to-static
    This breaks on GCC 6.3

--- Source/dvlnet/tcp_client.cpp
+++ Source/dvlnet/tcp_client.cpp
@@ -102,9 +102,7 @@ void tcp_client::start_recv()
 {
 	sock.async_receive(
 	    asio::buffer(recv_buffer),
-	    [this](auto &&PH1, auto &&PH2) {
-		    handle_recv(std::forward<decltype(PH1)>(PH1), std::forward<decltype(PH2)>(PH2));
-	    });
+	    std::bind(&tcp_client::handle_recv, this, std::placeholders::_1, std::placeholders::_2));
 }
 
 void tcp_client::handle_send(const asio::error_code &error, size_t bytesSent)
