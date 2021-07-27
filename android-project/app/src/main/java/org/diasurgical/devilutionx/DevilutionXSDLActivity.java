8add2c46a17c7c7676b4c9092b47921ed467e964
Anders Jenbo <anders@jenbo.dk>
2021-07-27 07:01:13 +0200

    [android] Game data installation screen

--- android-project/app/src/main/java/org/diasurgical/devilutionx/DevilutionXSDLActivity.java
+++ android-project/app/src/main/java/org/diasurgical/devilutionx/DevilutionXSDLActivity.java
@@ -1,13 +1,12 @@
 package org.diasurgical.devilutionx;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 import org.libsdl.app.SDLActivity;
@@ -20,8 +19,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class DevilutionXSDLActivity extends SDLActivity {
-	private SharedPreferences mPrefs;
-	final private String shouldShowInstallationGuidePref = "shouldShowInstallationGuide";
 	private String externalDir;
 
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,44 +26,28 @@ public class DevilutionXSDLActivity extends SDLActivity {
 
 		migrateAppData();
 
-		if (shouldShowInstallationGuide()) {
-			showInstallationGuide();
-		}
-
 		super.onCreate(savedInstanceState);
 	}
 
 	/**
-	 * Check if this is the first time the app is run and the full game data is not already present
+	 * On app launch make sure the game data is present
 	 */
-	private boolean shouldShowInstallationGuide() {
-		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-		return mPrefs.getBoolean(shouldShowInstallationGuidePref, !hasFullGameData());
+	protected void onStart() {
+		super.onStart();
+
+		if (missingGameData()) {
+			Intent intent = new Intent(this, DataActivity.class);
+			startActivity(intent);
+			this.finish();
+		}
 	}
 
-	private boolean hasFullGameData() {
+	private boolean missingGameData() {
 		File fileLower = new File(externalDir + "/diabdat.mpq");
 		File fileUpper = new File(externalDir + "/DIABDAT.MPQ");
+		File spawnFile = new File(externalDir + "/spawn.mpq");
 
-		return fileUpper.exists() || fileLower.exists();
-	}
-
-	private void installationGuideShown() {
-		SharedPreferences.Editor editor = mPrefs.edit();
-		editor.putBoolean(shouldShowInstallationGuidePref, false);
-		editor.apply();
-	}
-
-	private void showInstallationGuide() {
-		String message = "To play the full version you must place DIABDAT.MPQ from the original game in Android"
-				+ externalDir.split("/Android")[1]
-				+ ".\n\nIf you don't have the original game then you can buy Diablo from GOG.com.";
-		new AlertDialog.Builder(this)
-				.setTitle("Shareware version")
-				.setPositiveButton("OK", null)
-				.setMessage(message)
-				.show();
-		installationGuideShown();
+		return !fileUpper.exists() && !fileLower.exists() && !spawnFile.exists();
 	}
 
 	private boolean copyFile(File src, File dst) {
@@ -106,6 +87,7 @@ public class DevilutionXSDLActivity extends SDLActivity {
 		File newPath = new File(externalDir + "/" + file.getName());
 		if (newPath.exists()) {
 			if (file.canWrite()) {
+				//noinspection ResultOfMethodCallIgnored
 				file.delete();
 			}
 			return;
@@ -115,6 +97,7 @@ public class DevilutionXSDLActivity extends SDLActivity {
 		}
 		if (!file.renameTo(newPath)) {
 			if (copyFile(file, newPath) && file.canWrite()) {
+				//noinspection ResultOfMethodCallIgnored
 				file.delete();
 			}
 		}
@@ -123,6 +106,8 @@ public class DevilutionXSDLActivity extends SDLActivity {
 	/**
 	 * This can be removed Nov 2021 and Google will no longer permit access to the old folder from that point on
 	 */
+	@SuppressWarnings("deprecation")
+	@SuppressLint("SdCardPath")
 	private void migrateAppData() {
 		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
 			if (PackageManager.PERMISSION_GRANTED != checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
