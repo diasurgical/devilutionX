380f5ffaf69f8ce4d5fa36cd41bf9085f4a9057f
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2021-08-05 11:01:29 +0300

    Introduce StringVariant

---
+++ Source/utils/string_variant.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <string>
+#include "utils/stdcompat/string_view.hpp"
+
+namespace devilution {
+
+class StringVariant
+{
+	std::string str;
+	string_view view;
+
+public:
+	StringVariant()
+	{
+	}
+
+	explicit StringVariant(string_view view)
+	    : view(view)
+	{
+	}
+
+	explicit StringVariant(std::string str)
+	    : str(std::move(str))
+	{
+	}
+
+	explicit StringVariant(const char *cstr)
+	    : view(cstr)
+	{
+	}
+
+	StringVariant &operator =(const char *cstr)
+	{
+		*this = StringVariant(cstr);
+		return *this;
+	}
+
+	StringVariant &operator =(const string_view &view)
+	{
+		*this = StringVariant(view);
+		return *this;
+	}
+
+	StringVariant &operator =(const std::string &str)
+	{
+		*this = StringVariant(std::move(str));
+		return *this;
+	}
+
+	size_t length() const
+	{
+		if (str.length() > 0)
+			return str.length();
+		return view.length();
+	}
+
+	operator string_view() const
+	{
+		if (str.length() > 0)
+			return str;
+		return view;
+	}
+};
+
+} // namespace devilution
