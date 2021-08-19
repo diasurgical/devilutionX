diasurgical/devilutionX

commit 012f3aa90c9f76517f5d02867b7112c3eb48a59a

Findsodium.cmake: adjust static libraries list mangling
Our previous processing would throw away all but the last
entry in the found STATIC_LIBRARIES, which in newer versions
of libsodium, might be "pthread" -- hence not trying to link
against libsodium at all.

We now generate a temporary list, mangle it, then return that.

Bug: https://bugs.gentoo.org/791031
Fixes: https://github.com/diasurgical/devilutionX/issues/2615
Signed-off-by: Sam James <sam@gentoo.org>

CMake/Findsodium.cmake
@@ -56,12 +56,19 @@ if (UNIX OR CMAKE_SYSTEM_NAME STREQUAL "Generic" OR AMIGA)
 
     if(sodium_USE_STATIC_LIBS)
       if(sodium_PKG_STATIC_LIBRARIES)
+	# Create a temporary list to manipulate the list of libraries we found
+	set(sodium_PKG_STATIC_LIBRARIES_TMP "")
+
+	# Mangle the library names into the format we need
         foreach(_libname ${sodium_PKG_STATIC_LIBRARIES})
           if (NOT _libname MATCHES "^lib.*\\.a$") # ignore strings already ending with .a
-            list(INSERT sodium_PKG_STATIC_LIBRARIES 0 "lib${_libname}.a")
+            list(APPEND sodium_PKG_STATIC_LIBRARIES_TMP "lib${_libname}.a")
           endif()
         endforeach()
-        list(REMOVE_DUPLICATES sodium_PKG_STATIC_LIBRARIES)
+
+        list(REMOVE_DUPLICATES sodium_PKG_STATIC_LIBRARIES_TMP)
+	# Replace the list with our processed one
+	set(sodium_PKG_STATIC_LIBRARIES ${sodium_PKG_STATIC_LIBRARIES_TMP})
       else()
         # if pkgconfig for libsodium doesn't provide
         # static lib info, then override PKG_STATIC here..
@@ -77,6 +84,8 @@ if (UNIX OR CMAKE_SYSTEM_NAME STREQUAL "Generic" OR AMIGA)
       set(XPREFIX sodium_PKG)
     endif()
 
+    # Feed pkgconfig results (if found) into standard find_* to populate
+    # the right CMake cache variables
     find_path(sodium_INCLUDE_DIR sodium.h
         HINTS ${${XPREFIX}_INCLUDE_DIRS}
     )

commit c453833bb6f20b25a01a8de930642a97975b291e

Findsodium.cmake: Fix tabs->spaces
Fixes a formatting inconsistency I inadvertently
introduced in a previous change. Apologies!

Fixes: 012f3aa90c9f76517f5d02867b7112c3eb48a59a
Signed-off-by: Sam James <sam@gentoo.org>

CMake/Findsodium.cmake
@@ -56,10 +56,10 @@ if (UNIX OR CMAKE_SYSTEM_NAME STREQUAL "Generic" OR AMIGA)
 
     if(sodium_USE_STATIC_LIBS)
       if(sodium_PKG_STATIC_LIBRARIES)
-	# Create a temporary list to manipulate the list of libraries we found
-	set(sodium_PKG_STATIC_LIBRARIES_TMP "")
+        # Create a temporary list to manipulate the list of libraries we found
+        set(sodium_PKG_STATIC_LIBRARIES_TMP "")
 
-	# Mangle the library names into the format we need
+        # Mangle the library names into the format we need
         foreach(_libname ${sodium_PKG_STATIC_LIBRARIES})
           if (NOT _libname MATCHES "^lib.*\\.a$") # ignore strings already ending with .a
             list(APPEND sodium_PKG_STATIC_LIBRARIES_TMP "lib${_libname}.a")
@@ -67,8 +67,8 @@ if (UNIX OR CMAKE_SYSTEM_NAME STREQUAL "Generic" OR AMIGA)
         endforeach()
 
         list(REMOVE_DUPLICATES sodium_PKG_STATIC_LIBRARIES_TMP)
-	# Replace the list with our processed one
-	set(sodium_PKG_STATIC_LIBRARIES ${sodium_PKG_STATIC_LIBRARIES_TMP})
+        # Replace the list with our processed one
+        set(sodium_PKG_STATIC_LIBRARIES ${sodium_PKG_STATIC_LIBRARIES_TMP})
       else()
         # if pkgconfig for libsodium doesn't provide
         # static lib info, then override PKG_STATIC here..