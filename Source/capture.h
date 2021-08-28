68a971f160ec90800b3684add8aa1e95502e0574
BC Ko <gobcko@gmail.com>
2021-08-28 16:35:31 -0700

    documentation cleanup: capture

--- Source/capture.h
+++ Source/capture.h
@@ -7,6 +7,9 @@
 
 namespace devilution {
 
+/**
+ * @brief Save the current screen to a screen??.PCX (00-99) in file if available, then make the screen red for 200ms.
+ */
 void CaptureScreen();
 
 }
