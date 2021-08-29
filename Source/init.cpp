8f1bb43eafa6242ba02188dd7f6cfef51d0ca348
Cong <congusbongus@gmail.com>
2021-08-29 22:17:39 +1000

    Add GOG installation paths to game data search paths (fixes #2709)

--- Source/init.cpp
+++ Source/init.cpp
@@ -12,6 +12,7 @@
 #include "dx.h"
 #include "pfile.h"
 #include "storm/storm.h"
+#include "utils/find_steam_game.h"
 #include "utils/language.h"
 #include "utils/log.hpp"
 #include "utils/paths.h"
@@ -143,6 +144,12 @@ void init_archives()
 	if (paths[0] == paths[1])
 		paths.pop_back();
 
+	char gogpath[_FSG_PATH_MAX];
+	fsg_get_gog_game_path(gogpath, "1412601690");
+	if (strlen(gogpath) > 0) {
+		paths.emplace_back(std::string(gogpath) + "/");
+		paths.emplace_back(std::string(gogpath) + "/hellfire/");
+	}
 #if defined(__linux__) && !defined(__ANDROID__)
 	paths.emplace_back("/usr/share/diasurgical/devilutionx/");
 	paths.emplace_back("/usr/local/share/diasurgical/devilutionx/");
