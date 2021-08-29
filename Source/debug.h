de01f15398eec5f02458a08bfb724ff50a468a53
Anders Jenbo <anders@jenbo.dk>
2021-08-29 17:18:13 +0200

    Make debug compatible with GCC 6.2

--- Source/debug.h
+++ Source/debug.h
@@ -5,13 +5,13 @@
  */
 #pragma once
 
-#include <string_view>
 #include <unordered_map>
 
 #include "engine.h"
 #include "engine/cel_sprite.hpp"
 #include "miniwin/miniwin.h"
 #include "utils/stdcompat/optional.hpp"
+#include "utils/stdcompat/string_view.hpp"
 
 namespace devilution {
 
@@ -30,6 +30,6 @@ void PrintDebugQuest();
 void GetDebugMonster();
 void NextDebugMonster();
 void SetDebugLevelSeedInfos(uint32_t mid1Seed, uint32_t mid2Seed, uint32_t mid3Seed, uint32_t endSeed);
-bool CheckDebugTextCommand(const std::string_view text);
+bool CheckDebugTextCommand(const string_view text);
 
 } // namespace devilution
