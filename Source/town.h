a24c1be2068621db591630b4a471f3335f57153a
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-09-01 09:15:50 +0530

    Moved comments of town.cpp to town.h

--- Source/town.h
+++ Source/town.h
@@ -2,7 +2,40 @@
  * @file town.h
  *
  * Interface of functionality for rendering the town, towners and calling other render routines.
+ *
+ * Function : FillSector
+ * @brief Load level data into dPiece
+ * @param path Path of dun file
+ * @param xi upper left destination
+ * @param yy upper left destination
+ *
+ * Function: FillTile.
+ * @brief Load a tile in to dPiece
+ * @param megas Map from mega tiles to macro tiles
+ * @param xx upper left destination
+ * @param yy upper left destination
+ * @param t tile id
+ *
+ * Function: TownCloseHive(). 
+ * @brief Update the map to show the closed hive
+ *
+ * Function: TownCloseGrave().
+ * @brief Update the map to show the closed grave
+ *
+ * Function : DrlgTPass3().
+ * @brief Initialize all of the levels data
+ *
+ * Function: TownOpenHive().
+ * @brief Update the map to show the open hive
+ *
+ * Function: TownOpenGrave().
+ * @brief Update the map to show the open grave
+ *
+ * Function: CreateTown.
+ * @brief Initialize town level
+ * @param entry Methode of entry
  */
+ 
 #pragma once
 
 #include "gendung.h"
