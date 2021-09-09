b62ee6a66e4cf3fc8f4a15733fada3996364d5ea
ephphatha <ephphatha@thelettereph.com>
2021-09-09 18:26:14 +1000

    Remove use of direction for towners with a single facing

--- Source/towners.h
+++ Source/towners.h
@@ -59,11 +59,6 @@ struct Towner {
 	std::size_t animOrderSize;
 	void (*talk)(Player &player, Towner &towner);
 	_talker_id _ttype;
-
-	byte *GetAnimationPointer(Direction direction)
-	{
-		return this->_tNAnim[static_cast<size_t>(direction)];
-	}
 };
 
 extern Towner Towners[NUM_TOWNERS];
