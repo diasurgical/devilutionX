816c3916d0d67843593f3957f98b13fa9c8a8ba6
Sid672 <Siddharthsingh7898@gmail.com>
2021-09-10 22:46:17 +0530

    function doc of scrollrt.cpp moved to scrollrt.h

--- Source/scrollrt.h
+++ Source/scrollrt.h
@@ -45,19 +45,64 @@ extern bool AutoMapShowItems;
  */
 Displacement GetOffsetForWalking(const AnimationInfo &animationInfo, const Direction dir, bool cameraMode = false);
 
+/**
+ * @brief Clear cursor state
+ */
 void ClearCursor();
+
+/**
+ * @brief Shifting the view area along the logical grid
+ *        Note: this won't allow you to shift between even and odd rows
+ * @param horizontal Shift the screen left or right
+ * @param vertical Shift the screen up or down
+ */
 void ShiftGrid(int *x, int *y, int horizontal, int vertical);
+
+/**
+ * @brief Gets the number of rows covered by the main panel
+ */
 int RowsCoveredByPanel();
+
+/**
+ * @brief Calculate the offset needed for centering tiles in view area
+ * @param offsetX Offset in pixels
+ * @param offsetY Offset in pixels
+ */
 void CalcTileOffset(int *offsetX, int *offsetY);
+
+/**
+ * @brief Calculate the needed diamond tile to cover the view area
+ * @param columns Tiles needed per row
+ * @param rows Both even and odd rows
+ */
 void TilesInView(int *columns, int *rows);
 void CalcViewportGeometry();
 
+/**
+ * @brief Render the whole screen black
+ */
 void ClearScreenBuffer();
 #ifdef _DEBUG
+
+/**
+ * @brief Scroll the screen when mouse is close to the edge
+ */
 void ScrollView();
 #endif
+
+/**
+ * @brief Initialize the FPS meter
+ */
 void EnableFrameCount();
+
+/**
+ * @brief Redraw screen
+ */
 void scrollrt_draw_game_screen();
+
+/**
+ * @brief Render the game
+ */
 void DrawAndBlit();
 
 } // namespace devilution
