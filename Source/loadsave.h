bcb332b7b0aa34e9fcd3876a468130951053f571
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-09-14 13:25:55 +0530

    Move documentation from implementations to declarations

--- Source/loadsave.h
+++ Source/loadsave.h
@@ -25,6 +25,11 @@ void LoadHeroItems(Player &player);
  * @param pnum The id of the player
  */
 void RemoveEmptyInventory(Player &player);
+
+/**
+ * @brief Load game state
+ * @param firstflag Can be set to false if we are simply reloading the current game
+ */
 void LoadGame(bool firstflag);
 void SaveHotkeys();
 void SaveHeroItems(Player &player);
