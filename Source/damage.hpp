6c926f83f8976398f4dc314f4362622e8d2544cb
Juliano Leal Goncalves <julealgon@gmail.com>
2021-09-20 00:10:40 -0300

    🏷️ Add 'Damage' abstraction for fixed and variable damage
    This centralizes functionality in a single abstraction that can be used in all places that handle damage values.

---
+++ Source/damage.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "engine/random.hpp"
+
+namespace devilution {
+
+struct Damage {
+	int minValue;
+	int maxValue;
+
+	Damage() = default;
+
+	constexpr Damage(int fixedValue)
+	    : Damage(fixedValue, fixedValue)
+	{
+	}
+
+	constexpr Damage(int minValue, int maxValue)
+	    : minValue(minValue)
+	    , maxValue(maxValue)
+	{
+	}
+
+	int GetValue() const
+	{
+		return minValue == maxValue
+		    ? minValue
+		    : minValue + GenerateRnd(maxValue - minValue + 1);
+	}
+
+	constexpr bool operator==(const Damage &other) const
+	{
+		return minValue == other.minValue && maxValue == other.maxValue;
+	}
+
+	constexpr bool operator!=(const Damage &other) const
+	{
+		return !(*this == other);
+	}
+
+	constexpr Damage operator/=(const int factor)
+	{
+		minValue /= factor;
+		maxValue /= factor;
+		return *this;
+	}
+
+	constexpr friend Damage operator/(Damage damage, const int factor)
+	{
+		damage /= factor;
+		return damage;
+	}
+};
+
+} // namespace devilution
