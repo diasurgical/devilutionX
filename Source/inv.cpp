f1a0aa3b49afffa7cf5e1f2e8b21163b33c46b51
Bernd Stellwag <burned@zerties.org>
2021-10-09 22:30:31 +0200

    Replace broken link with @see annotation to text representation of the linked image

--- Source/inv.cpp
+++ Source/inv.cpp
@@ -49,7 +49,6 @@ bool drawsbarflag;
  *              55 56 57 58 59 60 61 62 63 64
  *
  * 65 66 67 68 69 70 71 72
- * @see graphics/inv/inventory.png
  */
 const Point InvRect[] = {
 	// clang-format off
