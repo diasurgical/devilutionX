5726a51e2a3c62b72827dc4b2ceace6073040800
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-11-04 10:47:38 +0000

    CMake/ctr/modules: Fix line endings

--- CMake/ctr/modules/try_add_imported_target.cmake
+++ CMake/ctr/modules/try_add_imported_target.cmake
@@ -3,11 +3,11 @@ macro(try_add_imported_target LIBNAME)
   set(${LIBNAME}_TARGET 3ds::${${LIBNAME}_lwr})
   if(${LIBNAME}_FOUND AND NOT TARGET ${${LIBNAME}_TARGET})
     add_library(${${LIBNAME}_TARGET} STATIC IMPORTED GLOBAL)
-    
+
     set_target_properties(${${LIBNAME}_TARGET} PROPERTIES
       IMPORTED_LOCATION "${${LIBNAME}_LIBRARY}"
       INTERFACE_INCLUDE_DIRECTORIES "${${LIBNAME}_INCLUDE_DIR}")
-    
+
     if(${ARGC} GREATER 1)
       target_link_libraries(${${LIBNAME}_TARGET} INTERFACE ${ARGN})
     endif()
