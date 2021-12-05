0e255d681a071b203048d62f6f6b4cb298e2493f
Anders Jenbo <anders@jenbo.dk>
2021-12-05 19:35:57 +0100

    [ios] Enable high resolution support

--- Source/utils/display.cpp
+++ Source/utils/display.cpp
@@ -205,7 +205,7 @@ bool SpawnWindow(const char *lpWindowName)
 		SDL_WM_GrabInput(SDL_GRAB_ON);
 	atexit(SDL_VideoQuit); // Without this video mode is not restored after fullscreen.
 #else
-	int flags = 0;
+	int flags = SDL_WINDOW_ALLOW_HIGHDPI;
 	if (*sgOptions.Graphics.upscale) {
 		if (!gbForceWindowed && *sgOptions.Graphics.fullscreen) {
 			flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
