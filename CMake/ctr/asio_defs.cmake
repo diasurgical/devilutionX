a9a352aa6cbe20392f26f75dfe8849e7e6ca010a
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-12-06 20:52:59 +0000

    Overhaul dependency options
    Changes how dependency options are handled and moves them out of
    `CMakeLists.txt`.
    
    Major changes:
    
    1. The static default now takes `TARGET_SUPPORTS_SHARED_LIBS` into account
    2. The `DIST` setting no longer affects the static default.
    3. The static option is now visible for system dependencies as well.
       (it was previously hidden by `cmake_dependent_option`).
    4. The auto-detection mechanism for `libfmt` and `SDL_image` only
       applies if the setting has not been set.
    5. SDL2 on Android is now also linked statically,
       resulting in a 1.1 MiB smaller APK.
    
    When using non-system dependencies, the default should always be static,
    as there are 2 main use cases for these:
    
    1. Dependencies that we must currently vendor because our forks have
       significant changes that are not yet merged/released upstream.
    2. Platforms that have no system packages, such as Android and iOS.
    
    In both cases, static linking is the most appropriate default.

--- CMake/ctr/asio_defs.cmake
+++ CMake/ctr/asio_defs.cmake
@@ -1,5 +1,6 @@
-# Enables a number of header file definitions required by ASIO
-target_compile_definitions(asio INTERFACE _DEFAULT_SOURCE=ON)
+target_compile_definitions(asio INTERFACE
+  ASIO_DISABLE_THREADS=ON
+  ASIO_HAS_UNISTD_H=ON)
 
 # Missing headers and declarations provided by DevilutionX
 target_include_directories(asio BEFORE INTERFACE ${CMAKE_CURRENT_LIST_DIR}/asio/include)
