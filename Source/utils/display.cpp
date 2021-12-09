2eb1ffb1d961b63492612a1fbc2a4a2a3978d6b8
JBerg <jonathan.bergeron@hotmail.com>
2021-12-08 23:41:30 -0500

    replaced spaces for tabs for identation from my last commit

--- Source/utils/display.cpp
+++ Source/utils/display.cpp
@@ -160,8 +160,8 @@ bool SpawnWindow(const char *lpWindowName)
 #endif
     
 #if SDL_VERSION_ATLEAST(2, 0, 6) && defined(__IPHONEOS__)
-    SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
-    SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
+	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
+	SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
 #endif
 
 #if defined(_WIN32) && !defined(USE_SDL1)
