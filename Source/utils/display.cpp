8dccba1f93770e67cb31557c0b6153ab5860c0c6
JBerg <jonathan.bergeron@hotmail.com>
2021-12-09 10:21:47 -0500

    ios mouse bug fix

--- Source/utils/display.cpp
+++ Source/utils/display.cpp
@@ -158,9 +158,8 @@ bool SpawnWindow(const char *lpWindowName)
 #if SDL_VERSION_ATLEAST(2, 0, 6) && defined(__vita__)
 	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
 #endif
-    
+
 #if SDL_VERSION_ATLEAST(2, 0, 6) && defined(__IPHONEOS__)
-	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
 	SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
 #endif
 
