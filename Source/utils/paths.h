7bfbaa7a18552f280a81b2e92d30a091f56b4e9e
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-12-16 19:39:16 +0000

    AssetsPath: Default to exe dir
    Also adds `SetAssetsPath`, for use by tests.

--- Source/utils/paths.h
+++ Source/utils/paths.h
@@ -17,6 +17,7 @@ const std::optional<std::string> &MpqDir();
 void SetBasePath(const std::string &path);
 void SetPrefPath(const std::string &path);
 void SetConfigPath(const std::string &path);
+void SetAssetsPath(const std::string &path);
 void SetMpqDir(const std::string &path);
 
 } // namespace paths
