bac5c1a2e5848d581baa241e8c8f22a3eff015ea
obligaron <obligaron@live.com>
2021-12-19 08:59:49 +0100

    Fix compiling tests with sdl 1

--- test/main.cpp
+++ test/main.cpp
@@ -7,8 +7,11 @@ int main(int argc, char **argv)
 {
 	// Disable error dialogs.
 	devilution::gbQuietMode = true;
+
+#if SDL_VERSION_ATLEAST(2, 0, 0)
 	// Disable hardware cursor while testing.
 	devilution::sgOptions.Graphics.hardwareCursor.SetValue(false);
+#endif
 
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
