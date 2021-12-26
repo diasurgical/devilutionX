416345c8209bd4b3b3c4762c92a911e4b5c75197
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-12-26 08:13:24 +0000

    Spell book: Fix "Level 0 - Unusable" overlap
    Removes the display of mana and damage for unusable spells
    and moves the "Unusable" text onto the second line, fixing
    the overlap.

--- Source/panels/spell_book.cpp
+++ Source/panels/spell_book.cpp
@@ -158,26 +158,26 @@ void DrawSpellBook(const Surface &out)
 			} break;
 			default: {
 				int mana = GetManaAmount(player, sn) >> 6;
-				if (sn != SPL_BONESPIRIT) {
-					int min;
-					int max;
-					GetDamageAmt(sn, &min, &max);
-					if (min != -1) {
-						if (sn == SPL_HEAL || sn == SPL_HEALOTHER) {
-							PrintSBookStr(out, line1, fmt::format(_(/* TRANSLATORS: UI constrains, keep short please.*/ "Heals: {:d} - {:d}"), min, max), UiFlags::AlignRight);
-						} else {
-							PrintSBookStr(out, line1, fmt::format(_(/* TRANSLATORS: UI constrains, keep short please.*/ "Damage: {:d} - {:d}"), min, max), UiFlags::AlignRight);
-						}
-					}
-				} else {
-					PrintSBookStr(out, line1, _(/* TRANSLATORS: UI constrains, keep short please.*/ "Dmg: 1/3 target hp"), UiFlags::AlignRight);
-				}
-				PrintSBookStr(out, line1, fmt::format(pgettext(/* TRANSLATORS: UI constrains, keep short please.*/ "spellbook", "Mana: {:d}"), mana));
 				int lvl = std::max(player._pSplLvl[sn] + player._pISplLvlAdd, 0);
+				PrintSBookStr(out, line0, fmt::format(pgettext(/* TRANSLATORS: UI constrains, keep short please.*/ "spellbook", "Level {:d}"), lvl), UiFlags::AlignRight);
 				if (lvl == 0) {
-					PrintSBookStr(out, line0, _("Level 0 - Unusable"), UiFlags::AlignRight);
+					PrintSBookStr(out, line1, _("Unusable"), UiFlags::AlignRight);
 				} else {
-					PrintSBookStr(out, line0, fmt::format(pgettext(/* TRANSLATORS: UI constrains, keep short please.*/ "spellbook", "Level {:d}"), lvl), UiFlags::AlignRight);
+					if (sn != SPL_BONESPIRIT) {
+						int min;
+						int max;
+						GetDamageAmt(sn, &min, &max);
+						if (min != -1) {
+							if (sn == SPL_HEAL || sn == SPL_HEALOTHER) {
+								PrintSBookStr(out, line1, fmt::format(_(/* TRANSLATORS: UI constrains, keep short please.*/ "Heals: {:d} - {:d}"), min, max), UiFlags::AlignRight);
+							} else {
+								PrintSBookStr(out, line1, fmt::format(_(/* TRANSLATORS: UI constrains, keep short please.*/ "Damage: {:d} - {:d}"), min, max), UiFlags::AlignRight);
+							}
+						}
+					} else {
+						PrintSBookStr(out, line1, _(/* TRANSLATORS: UI constrains, keep short please.*/ "Dmg: 1/3 target hp"), UiFlags::AlignRight);
+					}
+					PrintSBookStr(out, line1, fmt::format(pgettext(/* TRANSLATORS: UI constrains, keep short please.*/ "spellbook", "Mana: {:d}"), mana));
 				}
 			} break;
 			}
