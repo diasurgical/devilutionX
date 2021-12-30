e1b19c2fdc42df0a3f8852677430e11d8eeed7cf
KPhoenix <ericjrobinson@live.com>
2021-12-29 23:24:30 -0500

    Implemented 3XDAMVDEM Logic for Missiles
    The Deadly Hunter bow contains the 3XDAMVDEM affix power which isn't present in missiles.cpp, preventing the affix from doing anything altogether. This fix adds the logic from player.cpp in a consistent way with how damage gets handled in melee combat.

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -275,6 +275,12 @@ bool MonsterMHit(int pnum, int m, int mindam, int maxdam, int dist, missile_id t
 	if (resist)
 		dam >>= 2;
 
+	if (monster.MData->mMonstClass == MonsterClass::Demon) {
+		if ((player._pIFlags & ISPL_3XDAMVDEM) != 0) {
+			dam *= 3;
+		}
+	}
+
 	if (pnum == MyPlayerId)
 		monster._mhitpoints -= dam;
 
