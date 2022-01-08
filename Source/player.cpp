7de18ca6dd20361bd31b304c968535a670c376b3
KPhoenix <ericjrobinson@live.com>
2022-01-08 15:48:19 -0500

    Revise power leveling cap
    This change revises the power level cap to function differently from vanilla in Multiplayer. Normally, the cap will severely limit the experience gains from monsters of equivalent or greater level between ~0.3x and ~0.8x the monster's exp yield, where in Single Player, players can potentially enjoy exp yields up to around 5x the monster's exp yield. This will change the range of possible exp gained from 0x to 1x the monster's exp yield based on mlvl to clvl difference.

--- Source/player.cpp
+++ Source/player.cpp
@@ -2583,15 +2583,9 @@ void AddPlrExperience(int pnum, int lvl, int exp)
 
 	// Adjust xp based on difference in level between player and monster
 	uint32_t clampedExp = std::max(static_cast<int>(exp * (1 + (lvl - player._pLevel) / 10.0)), 0);
-
-	// Prevent power leveling
-	if (gbIsMultiplayer) {
-		const uint32_t clampedPlayerLevel = clamp(static_cast<int>(player._pLevel), 0, 50);
-
-		// for low level characters experience gain is capped to 1/20 of current levels xp
-		// for high level characters experience gain is capped to 200 * current level - this is a smaller value than 1/20 of the exp needed for the next level after level 5.
-		clampedExp = std::min({ clampedExp, /* level 0-5: */ ExpLvlsTbl[clampedPlayerLevel] / 20U, /* level 6-50: */ 200U * clampedPlayerLevel });
-	}
+	// Prevent power leveling (caps the exp gain per monster at 1x the monster's exp instead of capping exp gain at 200*clvl)
+	if (gbIsMultiplayer)
+		clampedExp = std::min(static_cast<int>(clampedExp), exp);
 
 	constexpr uint32_t MaxExperience = 2000000000U;
 
