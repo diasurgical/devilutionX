e12bf4902859079be4f49cae65d5013527deb6d6
John Törnblom <john.tornblom@gmail.com>
2022-02-01 16:19:48 +0100

    ps4: use in-tree copy of SDL2_image

--- CMake/platforms/ps4.cmake
+++ CMake/platforms/ps4.cmake
@@ -6,6 +6,15 @@ SET(DISABLE_LTO ON)
 set(NONET ON)
 set(NOEXIT ON)
 
+# Packbrew SDK provides SDL_image, but FindSDL2_image() fails to
+# pick up its dependencies (with includes libjpeg, libwebp etc).
+# One way to address this, is to do the following:
+#
+#   target_link_libraries(${BIN_TARGET} PUBLIC ${PC_SDL2_image_LIBRARIES})
+#
+# or simply use the in-tree copy as follows:
+set(DEVILUTIONX_SYSTEM_SDL_IMAGE OFF)
+
 # Action buttons (swapped A/B and X/Y)
 set(JOY_BUTTON_B 1) # ×
 set(JOY_BUTTON_A 2) # ○
