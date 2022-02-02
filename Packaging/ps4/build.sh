02e8ec5505d96e28f8c8705fef3654131aa15efd
John Törnblom <john.tornblom@gmail.com>
2022-02-02 22:13:31 +0100

    ps4 CI: enable verbose makefile

--- Packaging/ps4/build.sh
+++ Packaging/ps4/build.sh
@@ -7,6 +7,7 @@ SCRIPTDIR="$(dirname "${SCRIPTDIR}")"
 cmake -S "${SCRIPTDIR}/../../" \
       -B build-ps4 \
       -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
       -DCMAKE_TOOLCHAIN_FILE="/opt/pacbrew/ps4/openorbis/cmake/ps4.cmake"
 
 cmake --build build-ps4 -j $(getconf _NPROCESSORS_ONLN)
