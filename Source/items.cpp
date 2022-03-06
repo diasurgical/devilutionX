74b8e4aa739acef8dd2b59efc27c37cdefb48e9e
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2022-03-06 01:13:08 -0500

    Update items.cpp
    remove tab that did not adhere to clang format

--- Source/items.cpp
+++ Source/items.cpp
@@ -2173,7 +2173,7 @@ void SpawnOnePremium(Item &premiumItem, int plvl, int playerId)
 	if (gbIsHellfire)
 		maxCount = 150;
 	else
-		maxCount = 65000;	// Diablo originally had no limitation, but it seems wise to have a break in the loop so you don't sit there forever.
+		maxCount = 65000; // Diablo originally had no limitation, but it seems wise to have a break in the loop so you don't sit there forever.
 	
 	for (int count = 0; count < maxCount; count++) {
 		premiumItem = {};
