diasurgical/devilutionX

commit f4bce388754948927a010f483bbf643e69019847

Use `uint16_t` for sprite widths

Source/scrollrt.cpp
@@ -455,12 +455,9 @@ void DrawMonster(const Surface &out, Point tilePosition, Point targetBufferPosit
  */
 void DrawPlayerIconHelper(const Surface &out, int pnum, missile_graphic_id missileGraphicId, Point position, bool lighting)
 {
-	position.x += CalculateWidth2(Players[pnum].AnimInfo.celSprite->Width()) - MissileSpriteData[missileGraphicId].animWidth2;
+	position.x += CalculateWidth2(static_cast<int>(Players[pnum].AnimInfo.celSprite->Width()) - MissileSpriteData[missileGraphicId].animWidth2);
 
-	int width = MissileSpriteData[missileGraphicId].animWidth;
-	const byte *pCelBuff = MissileSpriteData[missileGraphicId].GetFirstFrame();
-
-	CelSprite cel { pCelBuff, width };
+	const CelSprite cel = MissileSpriteData[missileGraphicId].Sprite();
 
 	if (pnum == MyPlayerId) {
 		Cl2Draw(out, position.x, position.y, cel, 1);
@@ -750,7 +747,7 @@ void DrawMonsterHelper(const Surface &out, Point tilePosition, Point targetBuffe
 		auto &towner = Towners[mi];
 		int px = targetBufferPosition.x - CalculateWidth2(towner._tAnimWidth);
 		const Point position { px, targetBufferPosition.y };
-		CelSprite sprite { towner._tAnimData, towner._tAnimWidth };
+		const CelSprite sprite = towner.Sprite();
 		if (mi == pcursmonst) {
 			CelBlitOutlineTo(out, 166, position, sprite, towner._tAnimFrame);
 		}

Source/cursor.cpp
@@ -29,10 +29,10 @@ namespace {
 /** Cursor images CEL */
 std::optional<OwnedCelSprite> pCursCels;
 std::optional<OwnedCelSprite> pCursCels2;
-constexpr int InvItems1Size = 180;
+constexpr uint16_t InvItems1Size = 180;
 
 /** Maps from objcurs.cel frame number to frame width. */
-const int InvItemWidth1[] = {
+const uint16_t InvItemWidth1[] = {
 	// clang-format off
 	// Cursors
 	0, 33, 32, 32, 32, 32, 32, 32, 32, 32, 32, 23,
@@ -55,7 +55,7 @@ const int InvItemWidth1[] = {
 	2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28,
 	2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28, 2 * 28,
 };
-const int InvItemWidth2[] = {
+const uint16_t InvItemWidth2[] = {
 	0,
 	1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28,
 	1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28,
@@ -67,7 +67,7 @@ const int InvItemWidth2[] = {
 };
 
 /** Maps from objcurs.cel frame number to frame height. */
-const int InvItemHeight1[] = {
+const uint16_t InvItemHeight1[] = {
 	// clang-format off
 	// Cursors
 	0, 29, 32, 32, 32, 32, 32, 32, 32, 32, 32, 35,
@@ -90,7 +90,7 @@ const int InvItemHeight1[] = {
 	3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28,
 	3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28, 3 * 28,
 };
-const int InvItemHeight2[] = {
+const uint16_t InvItemHeight2[] = {
 	0,
 	1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28,
 	1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28, 1 * 28,

Source/dead.h
@@ -18,7 +18,7 @@ static constexpr unsigned MaxCorpses = 31;
 struct Corpse {
 	std::array<const byte *, 8> data;
 	int frame;
-	unsigned width;
+	uint16_t width;
 	uint8_t translationPaletteIndex;
 };
 

Source/engine/cel_sprite.hpp
@@ -15,13 +15,13 @@ class OwnedCelSprite;
  */
 class CelSprite {
 public:
-	CelSprite(const byte *data, int width)
+	CelSprite(const byte *data, uint16_t width)
 	    : data_ptr_(data)
 	    , width_(width)
 	{
 	}
 
-	CelSprite(const byte *data, const int *widths)
+	CelSprite(const byte *data, const uint16_t *widths)
 	    : data_ptr_(data)
 	    , widths_(widths)
 	{
@@ -39,7 +39,7 @@ public:
 		return data_ptr_;
 	}
 
-	[[nodiscard]] int Width(std::size_t frame = 1) const
+	[[nodiscard]] uint16_t Width(std::size_t frame = 1) const
 	{
 		return widths_ == nullptr ? width_ : widths_[frame];
 	}
@@ -55,8 +55,8 @@ public:
 
 private:
 	const byte *data_ptr_;
-	int width_ = 0;
-	const int *widths_ = nullptr; // unowned
+	uint16_t width_ = 0;
+	const uint16_t *widths_ = nullptr; // unowned
 };
 
 /**
@@ -65,13 +65,13 @@ private:
  */
 class OwnedCelSprite : public CelSprite {
 public:
-	OwnedCelSprite(std::unique_ptr<byte[]> data, int width)
+	OwnedCelSprite(std::unique_ptr<byte[]> data, uint16_t width)
 	    : CelSprite(data.get(), width)
 	    , data_(std::move(data))
 	{
 	}
 
-	OwnedCelSprite(std::unique_ptr<byte[]> data, const int *widths)
+	OwnedCelSprite(std::unique_ptr<byte[]> data, const uint16_t *widths)
 	    : CelSprite(data.get(), widths)
 	    , data_(std::move(data))
 	{

Source/engine/load_cel.cpp
@@ -4,12 +4,12 @@
 
 namespace devilution {
 
-OwnedCelSprite LoadCel(const char *pszName, int width)
+OwnedCelSprite LoadCel(const char *pszName, uint16_t width)
 {
 	return OwnedCelSprite(LoadFileInMem(pszName), width);
 }
 
-OwnedCelSprite LoadCel(const char *pszName, const int *widths)
+OwnedCelSprite LoadCel(const char *pszName, const uint16_t *widths)
 {
 	return OwnedCelSprite(LoadFileInMem(pszName), widths);
 }

Source/engine/load_cel.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "engine/cel_sprite.hpp"
 
 namespace devilution {
@@ -7,7 +9,7 @@ namespace devilution {
 /**
  * @brief Loads a Cel sprite and sets its width
  */
-OwnedCelSprite LoadCel(const char *pszName, int width);
-OwnedCelSprite LoadCel(const char *pszName, const int *widths);
+OwnedCelSprite LoadCel(const char *pszName, uint16_t width);
+OwnedCelSprite LoadCel(const char *pszName, const uint16_t *widths);
 
 } // namespace devilution

Source/loadsave.cpp
@@ -732,7 +732,7 @@ void LoadObject(LoadHelper &file, Object &object)
 	object._oAnimCnt = file.NextLE<int32_t>();
 	object._oAnimLen = file.NextLE<uint32_t>();
 	object._oAnimFrame = file.NextLE<uint32_t>();
-	object._oAnimWidth = file.NextLE<int32_t>();
+	object._oAnimWidth = static_cast<uint16_t>(file.NextLE<int32_t>());
 	file.Skip(4); // Skip _oAnimWidth2
 	object._oDelFlag = file.NextBool32();
 	object._oBreak = file.NextLE<int8_t>();
@@ -1403,7 +1403,7 @@ void SaveObject(SaveHelper &file, const Object &object)
 	file.WriteLE<uint32_t>(object._oAnimLen);
 	file.WriteLE<uint32_t>(object._oAnimFrame);
 	file.WriteLE<int32_t>(object._oAnimWidth);
-	file.WriteLE<int32_t>(CalculateWidth2(object._oAnimWidth)); // Write _oAnimWidth2 for vanilla compatibility
+	file.WriteLE<int32_t>(CalculateWidth2(static_cast<int>(object._oAnimWidth))); // Write _oAnimWidth2 for vanilla compatibility
 	file.WriteLE<uint32_t>(object._oDelFlag ? 1 : 0);
 	file.WriteLE<int8_t>(object._oBreak);
 	file.Skip(3); // Alignment

Source/misdat.cpp
@@ -217,7 +217,7 @@ std::array<T, 16> maybeAutofill(std::initializer_list<T> list)
 
 MissileFileData::MissileFileData(string_view name, uint8_t animName, uint8_t animFAmt, MissileDataFlags flags,
     std::initializer_list<uint8_t> animDelay, std::initializer_list<uint8_t> animLen,
-    int16_t animWidth, int16_t animWidth2)
+    uint16_t animWidth, int16_t animWidth2)
     : name(name)
     , animName(animName)
     , animFAmt(animFAmt)

commit 52d958876fbe5b56f74c0e950beea9eaee7a9395

Fix reflect icon offset
Regression introduced in f4bce388754948927a010f483bbf643e69019847

Fixes #4132

Source/scrollrt.cpp
@@ -455,7 +455,7 @@ void DrawMonster(const Surface &out, Point tilePosition, Point targetBufferPosit
  */
 void DrawPlayerIconHelper(const Surface &out, int pnum, missile_graphic_id missileGraphicId, Point position, bool lighting)
 {
-	position.x += CalculateWidth2(static_cast<int>(Players[pnum].AnimInfo.celSprite->Width()) - MissileSpriteData[missileGraphicId].animWidth2);
+	position.x += CalculateWidth2(Players[pnum].AnimInfo.celSprite->Width()) - MissileSpriteData[missileGraphicId].animWidth2;
 
 	const CelSprite cel = MissileSpriteData[missileGraphicId].Sprite();
 