e5b54e99ee971057f96e5251146565101e845cce
ephphatha <ephphatha@thelettereph.com>
2022-03-06 15:25:16 +1100

    Fix tests expecting unclean HoldItem uses.
    These test cases were asserting side-effects of the call to CreatePlayer leaving an initialised gold item in the held item slot.

--- test/player_test.cpp
+++ test/player_test.cpp
@@ -88,7 +88,7 @@ static void AssertPlayer(Player &player)
 	ASSERT_EQ(CountItems(player.InvBody, NUM_INVLOC), 1);
 	ASSERT_EQ(CountItems(player.InvList, NUM_INV_GRID_ELEM), 1);
 	ASSERT_EQ(CountItems(player.SpdList, MAXBELTITEMS), 2);
-	ASSERT_EQ(CountItems(&player.HoldItem, 1), 1);
+	ASSERT_EQ(CountItems(&player.HoldItem, 1), 0);
 
 	ASSERT_EQ(player.position.tile.x, 0);
 	ASSERT_EQ(player.position.tile.y, 0);
