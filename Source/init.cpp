ea38ec71cc2d55c4acc50b262e15fe70c148ac1a
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-05-18 03:39:23 +0300

    Set system data paths on FreeBSD as well

--- Source/init.cpp
+++ Source/init.cpp
@@ -90,7 +90,7 @@ std::vector<std::string> GetMPQSearchPaths()
 	if (paths[0] == paths[1])
 		paths.pop_back();
 
-#if defined(__linux__) && !defined(__ANDROID__)
+#if (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__)
 	paths.emplace_back("/usr/share/diasurgical/devilutionx/");
 	paths.emplace_back("/usr/local/share/diasurgical/devilutionx/");
 #elif defined(__3DS__) || defined(__SWITCH__)
