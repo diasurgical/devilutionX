ec1cc70c4c20ac1b51a544df7f37c23d682cc4bb
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-06-19 15:47:23 +0100

    Remove `FallbackPalette`
    Copies `black.pcx` into the MPQ to handle the case where we have
    `devilutionx.mpq` but not `diabdat.mpq`

--- CMake/Assets.cmake
+++ CMake/Assets.cmake
@@ -159,6 +159,7 @@ set(devilutionx_assets
   gendata/cutstartw.pcx
   Levels/L1Data/SklKngT.dun
   Levels/L2Data/BonechaT.dun
+  ui_art/black.pcx
   ui_art/creditsw.pcx
   ui_art/dvl_but_sml.pcx
   ui_art/dvl_lrpopup.pcx
