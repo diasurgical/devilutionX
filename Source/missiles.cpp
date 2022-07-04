4bb2f306577ca57b29ca62b04326b68abda9f7f3
Cesar Canassa <cesar.canassa@gmail.com>
2022-07-04 17:31:14 +0200

    :recycle: Refactor MonsterDeath to receive a Monster reference (#4887)

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -872,7 +872,7 @@ bool MonsterTrapHit(int m, int mindam, int maxdam, int dist, missile_id t, bool 
 		monster._mhitpoints = 0;
 #endif
 	if (monster._mhitpoints >> 6 <= 0) {
-		StartMonsterDeath(m, -1, true);
+		StartMonsterDeath(monster, -1, true);
 	} else if (resist) {
 		PlayEffect(monster, 1);
 	} else {
