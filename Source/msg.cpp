63abd320a64145c9158a36d9a8943d1ec72a46cf
Cesar Canassa <cesar.canassa@gmail.com>
2022-07-04 15:23:02 +0200

    :recycle: Refactor M_StartHit to receive a Monster reference

--- Source/msg.cpp
+++ Source/msg.cpp
@@ -1434,8 +1434,9 @@ DWORD OnKnockback(const TCmd *pCmd, int pnum)
 	const auto &message = *reinterpret_cast<const TCmdParam1 *>(pCmd);
 
 	if (gbBufferMsgs != 1 && Players[pnum].isOnActiveLevel() && message.wParam1 < MaxMonsters) {
-		M_GetKnockback(Monsters[message.wParam1]);
-		M_StartHit(message.wParam1, pnum, 0);
+		Monster &monster = Monsters[message.wParam1];
+		M_GetKnockback(monster);
+		M_StartHit(monster, pnum, 0);
 	}
 
 	return sizeof(message);
