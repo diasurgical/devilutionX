64f3b41d8ed724cb7951f1c384ea0f6c35c65363
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-07-04 10:54:39 +0100

    Xbox NXDK: Handle drives
    1. Automount D drive
    2. Mount E drive
    3. Set preference and config paths to `E:\UDATA\devilutionx`
    
    Co-authored-by: Ryzee119 <wendland@live.com.au>

--- Source/main.cpp
+++ Source/main.cpp
@@ -13,6 +13,9 @@
 #include "platform/vita/network.h"
 #include "platform/vita/random.hpp"
 #endif
+#ifdef NXDK
+#include <nxdk/mount.h>
+#endif
 #ifdef GPERF_HEAP_MAIN
 #include <gperftools/heap-profiler.h>
 #endif
@@ -39,6 +42,9 @@ extern "C" int main(int argc, char **argv)
 #ifdef __vita__
 	vita_enable_network();
 	randombytes_vitarandom_init();
+#endif
+#ifdef NXDK
+	nxMountDrive('E', "\\Device\\Harddisk0\\Partition1\\");
 #endif
 #ifdef GPERF_HEAP_MAIN
 	HeapProfilerStart("main");
