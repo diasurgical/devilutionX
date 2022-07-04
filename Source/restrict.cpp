a1aed18c8b2cf8352cf4d24d12a4a157be26be6f
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-07-04 11:41:50 +0100

    ReadOnlyTest: Use "w" mode instead of "wt"
    There is no reason to open that file in text mode explicitly

--- Source/restrict.cpp
+++ Source/restrict.cpp
@@ -13,7 +13,7 @@ namespace devilution {
 void ReadOnlyTest()
 {
 	const std::string path = paths::PrefPath() + "Diablo1ReadOnlyTest.foo";
-	FILE *f = FOpen(path.c_str(), "wt");
+	FILE *f = FOpen(path.c_str(), "w");
 	if (f == nullptr) {
 		DirErrorDlg(paths::PrefPath());
 	}
