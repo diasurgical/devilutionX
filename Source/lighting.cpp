4cc3d829636dc4697f7b92c47287cc2ec78ee02e
ephphatha <ephphatha@thelettereph.com>
2022-07-10 15:08:22 +1000

    Move VisionCrawlTable to anonymous namespace

--- Source/lighting.cpp
+++ Source/lighting.cpp
@@ -24,6 +24,8 @@ std::array<uint8_t, LIGHTSIZE> LightTables;
 bool DisableLighting;
 bool UpdateLighting;
 
+namespace {
+
 /*
  * X- Y-coordinate offsets of lighting visions.
  * The last entry-pair is only for alignment.
@@ -55,8 +57,6 @@ const uint8_t VisionCrawlTable[23][30] = {
 	{ 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9,  0, 10,  0, 11,  0, 12,  0, 13,  0, 14,  0, 15 },
 	// clang-format on
 };
-
-namespace {
 
 uint8_t lightradius[16][128];
 bool dovision;
