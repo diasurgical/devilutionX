a68a332428258b787ff04aa1dcce5cb3c46cebf4
obligaron <obligaron@live.com>
2022-07-13 20:13:59 +0200

    Add walking offset calculation to ActorPosition (and validate it)

--- Source/engine/actor_position.hpp
+++ Source/engine/actor_position.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "engine/animationinfo.h"
 #include "engine/point.hpp"
 #include "engine/world_tile.hpp"
 
@@ -21,6 +22,17 @@ struct ActorPosition {
 	DisplacementOf<int16_t> offset2;
 	/** Pixel velocity while walking. Indirectly applied to offset via _pvar6/7 */
 	DisplacementOf<int16_t> velocity;
+
+	/** @brief Calculates the offset for the walking animation. */
+	DisplacementOf<int8_t> CalculateWalkingOffset(Direction dir, const AnimationInfo &animInfo, bool pendingProcessAnimation = false) const;
+	/** @brief Calculates the offset for the walking animation. */
+	DisplacementOf<int16_t> CalculateWalkingOffsetShifted4(Direction dir, const AnimationInfo &animInfo, bool pendingProcessAnimation = false) const;
+	/** @brief Calculates the offset for the walking animation. */
+	DisplacementOf<int16_t> CalculateWalkingOffsetShifted8(Direction dir, const AnimationInfo &animInfo, bool pendingProcessAnimation = false) const;
+	/** @brief Returns Pixel velocity while walking. */
+	DisplacementOf<int16_t> GetWalkingVelocityShifted4(Direction dir, const AnimationInfo &animInfo) const;
+	/** @brief Returns Pixel velocity while walking. */
+	DisplacementOf<int16_t> GetWalkingVelocityShifted8(Direction dir, const AnimationInfo &animInfo) const;
 };
 
 } // namespace devilution
