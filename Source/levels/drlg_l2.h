78f2fa6221d679e70eb10323292a1c46de332a53
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2022-07-16 14:44:12 +0300

    drlg_l2: refactor ROOMNODE and HALLNODE

--- Source/levels/drlg_l2.h
+++ Source/levels/drlg_l2.h
@@ -9,21 +9,6 @@
 
 namespace devilution {
 
-struct HALLNODE {
-	int nHallx1;
-	int nHally1;
-	int nHallx2;
-	int nHally2;
-	int nHalldir;
-};
-
-struct ROOMNODE {
-	int nRoomx1;
-	int nRoomy1;
-	int nRoomx2;
-	int nRoomy2;
-};
-
 void CreateL2Dungeon(uint32_t rseed, lvl_entry entry);
 void LoadPreL2Dungeon(const char *path);
 void LoadL2Dungeon(const char *path, Point spawn);
