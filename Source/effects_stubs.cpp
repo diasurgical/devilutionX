f9f6949af2b305ebb158f28727ee2e4352eef5a9
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-07-16 09:24:24 +0100

    Clean up `effects_play_sound`, `gfnSoundFunction`

--- Source/effects_stubs.cpp
+++ Source/effects_stubs.cpp
@@ -50,7 +50,7 @@ void sound_update() { }
 void effects_cleanup_sfx() { }
 void sound_init() { }
 void ui_sound_init() { }
-void effects_play_sound(const char *snd_file) { }
+void effects_play_sound(_sfx_id id) { }
 int GetSFXLength(int nSFX) { return 0; }
 // clang-format off
 
