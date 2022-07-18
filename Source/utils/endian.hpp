1e600bf204c81c6f7d0c89f86116f33bc14a459c
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-07-18 09:47:57 +0100

    Demo mode: Handle endianness

--- Source/utils/endian.hpp
+++ Source/utils/endian.hpp
@@ -1,31 +1,46 @@
 #pragma once
 
 #include <cstdint>
+#include <cstring>
+
+#include <SDL_endian.h>
 
 namespace devilution {
 
 template <typename T>
-constexpr std::uint16_t LoadLE16(const T *b)
+constexpr uint16_t LoadLE16(const T *b)
 {
 	static_assert(sizeof(T) == 1, "invalid argument");
 	// NOLINTNEXTLINE(readability-magic-numbers)
-	return (static_cast<std::uint16_t>(b[1]) << 8) | static_cast<std::uint16_t>(b[0]);
+	return (static_cast<uint8_t>(b[1]) << 8) | static_cast<uint8_t>(b[0]);
 }
 
 template <typename T>
-constexpr std::uint32_t LoadLE32(const T *b)
+constexpr uint32_t LoadLE32(const T *b)
 {
 	static_assert(sizeof(T) == 1, "invalid argument");
 	// NOLINTNEXTLINE(readability-magic-numbers)
-	return (static_cast<std::uint32_t>(b[3]) << 24) | (static_cast<std::uint32_t>(b[2]) << 16) | (static_cast<std::uint32_t>(b[1]) << 8) | static_cast<std::uint32_t>(b[0]);
+	return (static_cast<uint8_t>(b[3]) << 24) | (static_cast<uint8_t>(b[2]) << 16) | (static_cast<uint8_t>(b[1]) << 8) | static_cast<uint8_t>(b[0]);
 }
 
 template <typename T>
-constexpr std::uint32_t LoadBE32(const T *b)
+constexpr uint32_t LoadBE32(const T *b)
 {
 	static_assert(sizeof(T) == 1, "invalid argument");
 	// NOLINTNEXTLINE(readability-magic-numbers)
-	return (static_cast<std::uint32_t>(b[0]) << 24) | (static_cast<std::uint32_t>(b[1]) << 16) | (static_cast<std::uint32_t>(b[2]) << 8) | static_cast<std::uint32_t>(b[3]);
+	return (static_cast<uint8_t>(b[0]) << 24) | (static_cast<uint8_t>(b[1]) << 16) | (static_cast<uint8_t>(b[2]) << 8) | static_cast<uint8_t>(b[3]);
+}
+
+inline void WriteLE16(void *out, uint16_t val)
+{
+	const uint16_t littleEndian = SDL_SwapLE16(val);
+	memcpy(out, &littleEndian, 2);
+}
+
+inline void WriteLE32(void *out, uint32_t val)
+{
+	const uint32_t littleEndian = SDL_SwapLE32(val);
+	memcpy(out, &littleEndian, 4);
 }
 
 } // namespace devilution
