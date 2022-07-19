b4a337c937517382ddea897afdba02249604a081
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-07-19 08:37:15 +0100

    Remove CEL rendering code
    We no longer render CEL

--- Source/engine/load_cel.hpp
+++ Source/engine/load_cel.hpp
@@ -6,12 +6,6 @@
 
 namespace devilution {
 
-/**
- * @brief Loads a Cel sprite and sets its width
- */
-OwnedCelSprite LoadCel(const char *pszName, uint16_t width);
-OwnedCelSprite LoadCel(const char *pszName, const uint16_t *widths);
-
 OwnedCelSprite LoadCelAsCl2(const char *pszName, uint16_t width);
 OwnedCelSprite LoadCelAsCl2(const char *pszName, const uint16_t *widths);
 
