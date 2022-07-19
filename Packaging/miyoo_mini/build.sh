51e5696d199aa7137d36468ddc6ef970fafbf947
low_k0 <heiko.brockmann@posteo.de>
2022-07-19 14:36:34 +0200

    Added build step for custom SDL when packaging for Miyoo Mini

--- Packaging/miyoo_mini/build.sh
+++ Packaging/miyoo_mini/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")/../.."
+progdir=`cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P`
+
+# ensure we are in deliutionx root
+cd "$progdir/../.."
 
 declare -r CFLAGS="-O3 -marm -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7ve -Wall"
 declare -r LDFLAGS="-lSDL -lmi_sys -lmi_gfx -s -lSDL -lSDL_image"
 declare -r BUILD_DIR="build-miyoo-mini"
+declare -r MIYOO_CUSTOM_SDL_REPO="https://github.com/Brocky/SDL-1.2-miyoo-mini.git"
+declare -r MIYOO_CUSTOM_SDL_BRANCH="miniui-miyoomini"
 
 main(){
 	rm -f "$BUILD_DIR/CMakeCache.txt"
@@ -30,17 +35,47 @@ cmake_build(){
 	cmake --build "$BUILD_DIR"
 }
 
-package_onion(){
+build_custom_sdl(){
+	if [[ ! -d "$BUILD_DIR/CustomSDL" ]];
+	then
+		mkdir $BUILD_DIR/CustomSDL
+	fi
+	cd $BUILD_DIR/CustomSDL
+	git clone $MIYOO_CUSTOM_SDL_REPO --branch $MIYOO_CUSTOM_SDL_BRANCH --single-branch .
+	./make.sh
+	# change back to devilutionx root
+	cd "$progdir/../.."
+	yes | cp -rfL "$BUILD_DIR/CustomSDL/build/.libs/libSDL-1.2.so.0" "$BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/lib/libSDL-1.2.so.0"
+}
+
+prepare_onion_skeleton(){
 	if [[ ! -d "$BUILD_DIR/SDROOT" ]];
 	then
 		mkdir $BUILD_DIR/SDROOT
 	fi
+	
+	# Copy basic skeleton
 	yes | cp -rf  Packaging/miyoo_mini/skeleton/* $BUILD_DIR/SDROOT
+	
+	# ensure divlutionx asset dir
 	if [[ ! -d "$BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/FILES_HERE/assets" ]];
 	then
 		mkdir $BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/FILES_HERE/assets
 	fi
+	
+	# ensure lib dir for custom SDL
+	if [[ ! -d "$BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/lib" ]];
+	then
+		mkdir -p $BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/lib
+	fi
+}
+
+package_onion(){
+	prepare_onion_skeleton
+	build_custom_sdl
+	# copy assets
 	yes | cp -rf $BUILD_DIR/assets/* $BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/FILES_HERE/assets
+	# copy executable
 	yes | cp -rf $BUILD_DIR/devilutionx $BUILD_DIR/SDROOT/Emu/PORTS/Binaries/Diablo.port/devilutionx
 }
 
