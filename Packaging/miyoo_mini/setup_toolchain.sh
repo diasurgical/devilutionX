9e3138bf789f939667ae0eb8546fa85c64c39e80
low_k0 <heiko.brockmann@posteo.de>
2022-07-22 10:49:37 +0200

    Fixed issue with installing toolchain scripts

--- Packaging/miyoo_mini/setup_toolchain.sh
+++ Packaging/miyoo_mini/setup_toolchain.sh
@@ -6,17 +6,17 @@ main() {
 }
 
 install_toolchain() {
-	declare -r TOOLCHAIN_VERSION=v0.0.2
-	declare -r TOOLCHAIN_TAR="miyoomini-toolchain.tar.xz"
+	TOOLCHAIN_VERSION=v0.0.2
+	TOOLCHAIN_TAR="miyoomini-toolchain.tar.xz"
 
-	declare -r TOOLCHAIN_ARCH=`uname -m`
+	TOOLCHAIN_ARCH=`uname -m`
 	if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
-		declare -r TOOLCHAIN_REPO=miyoomini-toolchain-buildroot-aarch64
+		TOOLCHAIN_REPO=miyoomini-toolchain-buildroot-aarch64
 	else
-		declare -r TOOLCHAIN_REPO=miyoomini-toolchain-buildroot
+		TOOLCHAIN_REPO=miyoomini-toolchain-buildroot
 	fi
 
-	declare -r TOOLCHAIN_URL="https://github.com/shauninman/$TOOLCHAIN_REPO/releases/download/$TOOLCHAIN_VERSION/$TOOLCHAIN_TAR"
+	TOOLCHAIN_URL="https://github.com/shauninman/$TOOLCHAIN_REPO/releases/download/$TOOLCHAIN_VERSION/$TOOLCHAIN_TAR"
 
 	cd /opt
 	wget "$TOOLCHAIN_URL"
