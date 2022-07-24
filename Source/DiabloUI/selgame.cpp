93915a737274c1edc9346a62e7801cbfa3de4dcd
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2022-07-24 15:01:23 -0400

    Update selgame.cpp
    change iteration counter to unsigned int

--- Source/DiabloUI/selgame.cpp
+++ Source/DiabloUI/selgame.cpp
@@ -613,7 +613,7 @@ void selgame_Password_Select(int /*value*/)
 
 	if (selgame_selectedGame > 1) {
 		if (provider == SELCONN_ZT) {
-			for (char i = 0; i < (sizeof(selgame_Ip) / sizeof(selgame_Ip[0])); i++) {
+			for (unsigned int i = 0; i < (sizeof(selgame_Ip) / sizeof(selgame_Ip[0])); i++) {
 				selgame_Ip[i] = std::tolower(selgame_Ip[i]);
 			}
 			strcpy(sgOptions.Network.szPreviousZTGame, selgame_Ip);
