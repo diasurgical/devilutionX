59482b5e9f9bb9c4d0b3a54eba5902f8df93998e
Yuvraj Tetarwal <69211421+YuviTz1@users.noreply.github.com>
2022-07-31 21:56:07 +0530

    Auto oil pickup (#5159)
    * Fixed formatting

--- Source/options.h
+++ Source/options.h
@@ -535,6 +535,8 @@ struct GameplayOptions : OptionCategoryBase {
 	OptionEntryBoolean autoGoldPickup;
 	/** @brief Auto-pickup elixirs */
 	OptionEntryBoolean autoElixirPickup;
+	/** @brief Auto-pickup oils */
+	OptionEntryBoolean autoOilPickup;
 	/** @brief Enable or Disable auto-pickup in town */
 	OptionEntryBoolean autoPickupInTown;
 	/** @brief Recover mana when talking to Adria. */
