2121d8dc6776932b3d5f4717f454780764391449
ephphatha <ephphatha@thelettereph.com>
2022-08-02 09:30:58 +1000

    Use position for object delta messages
    Co-authored-by: Stephen C. Wills <swills@gridprotectionalliance.org>
    Co-authored-by: Anders Jenbo <anders@jenbo.dk>

--- Source/engine/world_tile.hpp
+++ Source/engine/world_tile.hpp
@@ -11,3 +11,18 @@ using WorldTileOffset = int8_t;
 using WorldTileDisplacement = DisplacementOf<WorldTileOffset>;
 
 } // namespace devilution
+
+namespace std {
+
+/**
+ * @brief Allows using WorldTilePosition as a map key for contexts where we want to lookup an entity by physical dungeon location
+ */
+template <>
+struct hash<devilution::WorldTilePosition> {
+	size_t operator()(const devilution::WorldTilePosition &position) const noexcept
+	{
+		return static_cast<size_t>(position.x) << 8 | position.y;
+	}
+};
+
+} // namespace std
