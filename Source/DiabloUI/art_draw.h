fcd163cac657c420fabb085e64ea80360dd21b1e
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-08-04 03:17:23 +0100

    Remove unused `DrawArt` overload

--- Source/DiabloUI/art_draw.h
+++ Source/DiabloUI/art_draw.h
@@ -9,8 +9,6 @@ void UpdatePalette(Art *art, const SDL_Surface *output = nullptr);
 
 void DrawArt(Point screenPosition, Art *art, int nFrame = 0, Uint16 srcW = 0, Uint16 srcH = 0);
 
-void DrawArt(const Surface &out, Point position, Art *art, int nFrame = 0, Uint16 srcW = 0, Uint16 srcH = 0);
-
 int GetAnimationFrame(int frames, int fps = 60);
 
 } // namespace devilution
