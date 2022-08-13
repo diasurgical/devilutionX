6acb467216e097730bde5c3c15c79c3ff24d3a47
Anders Jenbo <anders@jenbo.dk>
2022-08-13 23:12:56 +0200

    Use SDL for creating file during RW test

--- Source/utils/file_util.h
+++ Source/utils/file_util.h
@@ -17,7 +17,6 @@ bool GetFileSize(const char *path, std::uintmax_t *size);
 bool ResizeFile(const char *path, std::uintmax_t size);
 void RemoveFile(const char *path);
 std::optional<std::fstream> CreateFileStream(const char *path, std::ios::openmode mode);
-FILE *FOpen(const char *path, const char *mode);
 
 #if (defined(_WIN64) || defined(_WIN32)) && !defined(NXDK)
 std::unique_ptr<wchar_t[]> ToWideChar(string_view path);
