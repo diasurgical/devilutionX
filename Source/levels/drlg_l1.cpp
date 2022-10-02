d9558fc204909b3e7bd8d1d631c93ffc7d6dd65a
Anders Jenbo <anders@jenbo.dk>
2022-10-02 14:25:09 +0200

    Update Source/levels/drlg_l1.cpp

--- Source/levels/drlg_l1.cpp
+++ Source/levels/drlg_l1.cpp
@@ -1260,7 +1260,7 @@ Point SelectChamber()
 {
 	int chamber;
 	if (HasChamber1 && HasChamber2 && HasChamber3) {
-		chamber = PickRandomlyAmong({ 1, 2, 3 });
+		chamber = GenerateRnd(3) + 1;
 	} else if (HasChamber1 && HasChamber2) {
 		chamber = PickRandomlyAmong({ 2, 1 }); // Reverse odrder to match vanilla
 	} else if (HasChamber1 && HasChamber3) {
