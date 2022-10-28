a036659b14ec3d00bd36d72124340461051e50c1
ASHWIN492 <94861259+ASHWIN492@users.noreply.github.com>
2022-10-28 08:20:28 +0530

    Create Bubblesort.java

---
+++ Bubblesort.java
@@ -0,0 +1,35 @@
+// Java program for implementation of Bubble Sort
+class BubbleSort {
+	void bubbleSort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < n - i - 1; j++)
+				if (arr[j] > arr[j + 1]) {
+					// swap arr[j+1] and arr[j]
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
+	/* Prints the array */
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+	// Driver method to test above
+	public static void main(String args[])
+	{
+		BubbleSort ob = new BubbleSort();
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+		ob.bubbleSort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
+}
+/* This code is contributed by Rajat Mishra */
