74ca2ab0dbdf49977a61d8bab6004848199e76a4
Gleb Mazovetskiy <glex.spb@gmail.com>
2022-11-09 20:04:06 +0000

    Use no-background spell icons with UNPACKED_MPQS
    Spell icons size: 200 KiB -> 81 KiB
    
    Fixes #5470

--- Source/panels/spell_book.hpp
+++ Source/panels/spell_book.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "engine/clx_sprite.hpp"
 #include "engine/surface.hpp"
 
 namespace devilution {
+
+#ifdef UNPACKED_MPQS
+extern OptionalOwnedClxSpriteList pSBkIconsBackground;
+extern OptionalOwnedClxSpriteList pSBkIconsForeground;
+#else
+extern OptionalOwnedClxSpriteList pSBkIconCels;
+#endif
 
 void InitSpellBook();
 void FreeSpellBook();
