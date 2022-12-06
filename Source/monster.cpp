0379f964a1837e595f38bc09db967523821835d6
KPhoenix <ericjrobinson@live.com>
2022-12-06 17:29:36 -0500

    Update monster.cpp

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -3043,9 +3043,9 @@ bool IsRelativeMoveOK(const Monster &monster, Point position, Direction mdir)
 	Point futurePosition = position + mdir;
 	if (!InDungeonBounds(futurePosition) || !IsTileAvailable(monster, futurePosition))
 		return false;
-	// Fixes exploit that allows a monster to become "stuck" in a trigger tile
+	// Fixes exploit that allows a monster to become "stuck" in a trigger tile in Cathedral and Crypt levels
 	for (int i = 0; i < numtrigs; i++) {
-		if (futurePosition == trigs[i].position) {
+		if (futurePosition == trigs[i].position && ((currlevel >= 1 && currlevel <= 4) || (currlevel >= 21 && currlevel <= 24))) {
 			return false;
 		}
 	}
