aee602c3db24bbb5d092bb35beb7d0c73db761a2
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-02-03 10:00:51 +0000

    Clean up missile data access
    Replace enum static casts with `GetMissile(Sprite)Data`.

--- Source/dead.cpp
+++ Source/dead.cpp
@@ -52,7 +52,7 @@ void InitCorpses()
 	nd++; // Unused blood spatter
 
 	if (!HeadlessMode)
-		Corpses[nd].sprites.emplace(*MissileSpriteData[static_cast<uint8_t>(MissileGraphicID::StoneCurseShatter)].sprites);
+		Corpses[nd].sprites.emplace(*GetMissileSpriteData(MissileGraphicID::StoneCurseShatter).sprites);
 	Corpses[nd].frame = 11;
 	Corpses[nd].width = 128;
 	Corpses[nd].translationPaletteIndex = 0;
