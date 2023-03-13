65861c80435e1a239ee65a115834ad133ca4458f
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-03-13 01:03:33 +0000

    StrCat improvements
    1. Use `fmt::format_int` directly instead of parsing a format string.
    2. Use `AppendStrView`.
    3. Define the varargs versions using fold expressions when available.
    4. Add tests.

--- Source/utils/str_cat.cpp
+++ Source/utils/str_cat.cpp
@@ -1,28 +1,20 @@
 #include "utils/str_cat.hpp"
 
-#include <cstddef>
-
-#include <fmt/compile.h>
-#include <fmt/core.h>
+#include <fmt/format.h>
 
 namespace devilution {
 
-namespace {
-
-template <typename T>
-constexpr size_t MaxDecimalDigits = 241 * sizeof(T) / 100 + 1;
-
-} // namespace
-
 char *BufCopy(char *out, int value)
 {
-	return fmt::format_to(out, FMT_COMPILE("{}"), value);
+	const fmt::format_int formatted { value };
+	std::memcpy(out, formatted.data(), formatted.size());
+	return out + formatted.size();
 }
 
 void StrAppend(std::string &out, int value)
 {
-	char buf[MaxDecimalDigits<int> + 1];
-	out.append(&buf[0], BufCopy(buf, value) - &buf[0]);
+	const fmt::format_int formatted { value };
+	out.append(formatted.data(), formatted.size());
 }
 
 } // namespace devilution
