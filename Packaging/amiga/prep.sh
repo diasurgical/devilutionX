2f2dd6072193ff548fb110e23b7df2e9e30b7a59
Anders Jenbo <anders@jenbo.dk>
2023-04-27 20:24:27 +0200

    Update prep.sh

--- Packaging/amiga/prep.sh
+++ Packaging/amiga/prep.sh
@@ -13,10 +13,7 @@ export M68K_COMMON="-s -ffast-math -fomit-frame-pointer -fbbb=-"
 export M68K_CFLAGS="${M68K_CPU_FPU} ${M68K_COMMON}"
 export M68K_CXXFLAGS="${M68K_CPU_FPU} ${M68K_COMMON}"
 
-mkdir -p /opt/m68k-amigaos/bin/
-ln -s /opt/m68k-amigaos-gcc10/bin/m68k-amigaos-gcc /opt/m68k-amigaos/bin/m68k-amigaos-gcc
-ln -s /opt/m68k-amigaos-gcc10/bin/m68k-amigaos-as /opt/m68k-amigaos/bin/m68k-amigaos-as
-ln -s /opt/m68k-amigaos-gcc10/bin/vasmm68k_mot /opt/m68k-amigaos/bin/vasmm68k_mot
+ln -s /opt/m68k-amigaos-gcc10 /opt/m68k-amigaos
 
 PARALLELISM="$(getconf _NPROCESSORS_ONLN)"
 
