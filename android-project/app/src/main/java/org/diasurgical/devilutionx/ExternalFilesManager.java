5fa221d9b25b6ac2d70f2bef39f1a65a41969036
Anders Jenbo <anders@jenbo.dk>
2023-06-13 15:40:36 +0200

    Perform additional null checks in ExternalFilesManager

--- android-project/app/src/main/java/org/diasurgical/devilutionx/ExternalFilesManager.java
+++ android-project/app/src/main/java/org/diasurgical/devilutionx/ExternalFilesManager.java
@@ -59,20 +59,24 @@ public class ExternalFilesManager {
 		if (Build.VERSION.SDK_INT >= 19) {
 			File[] externalDirs = context.getExternalFilesDirs(null);
 
-			for (int i = 0; i < externalDirs.length; i++) {
-				File dir = externalDirs[i];
-				File[] iniFiles = dir.listFiles((dir1, name) -> name.equals("diablo.ini"));
-				if (iniFiles.length > 0)
-					return dir.getAbsolutePath();
-			}
+			if (externalDirs != null && externalDirs.length > 0) {
+				for (File dir : externalDirs) {
+					File[] iniFiles = dir.listFiles((dir1, name) -> name.equals("diablo.ini"));
+					if (iniFiles != null && iniFiles.length > 0) {
+						return dir.getAbsolutePath();
+					}
+				}
 
-			for (int i = 0; i < externalDirs.length; i++) {
-				File dir = externalDirs[i];
-				if (dir.listFiles().length > 0)
-					return dir.getAbsolutePath();
+				for (File dir : externalDirs) {
+					File[] lastFiles = dir.listFiles();
+					if (lastFiles != null && lastFiles.length > 0) {
+						return dir.getAbsolutePath();
+					}
+				}
 			}
 		}
 
+		// Fallback to the primary external storage directory
 		return context.getExternalFilesDir(null).getAbsolutePath();
 	}
 
