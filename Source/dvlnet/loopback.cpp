e43d953b51b811f15c070318282d3bf120589035
KPhoenix <ericjrobinson@live.com>
2023-07-01 17:07:44 -0400

    Revised for translations

--- Source/dvlnet/loopback.cpp
+++ Source/dvlnet/loopback.cpp
@@ -12,6 +12,7 @@ namespace net {
 
 int loopback::create(std::string /*addrstr*/)
 {
+	isLoopback = true;
 	return plr_single;
 }
 
@@ -87,6 +88,7 @@ bool loopback::SNetUnregisterEventHandler(event_type /*evtype*/)
 
 bool loopback::SNetLeaveGame(int /*type*/)
 {
+	isLoopback = false;
 	return true;
 }
 
