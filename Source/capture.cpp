31cfbec83c734383ae753ede85160080ab240d43
KPhoenix <ericjrobinson@live.com>
2023-07-05 03:48:20 -0400

    Update capture.cpp

--- Source/capture.cpp
+++ Source/capture.cpp
@@ -1,58 +1,92 @@
-#include "Capture.h"
-#include "DiabloUI/diabloui.h"
-#include "engine/render/text_render.hpp"
-#include "engine/dx.h"
-#include "options.h"
-#include "utils/png.h"
-#include "utils/sdl_geometry.h"
-#include "utils/sdl_ptrs.h"
-#include "controls/touch/renderers.h"
-#include <SDL_image.h> 
-
-#include <cstddef>
+/**
+ * @file capture.cpp
+ *
+ * Implementation of the screenshot function.
+ */
 #include <cstdint>
-#include <memory>
+#include <cstdio>
+#include <ctime>
 
-#ifndef USE_SDL1
-#include <SDL_image.h>
-#else
-#include "stubs.h"
-#endif
-#include "utils/image_utils.hpp"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_messagebox.h>
+#include <fmt/format.h>
 
+#include "DiabloUI/diabloui.h"
+#include "engine/backbuffer_state.hpp"
+#include "engine/dx.h"
+#include "engine/palette.h"
+#include "utils/file_util.h"
+#include "utils/log.hpp"
+#include "utils/paths.h"
+#include "utils/str_cat.hpp"
+#include "utils/ui_fwd.h"
 
 namespace devilution {
+namespace {
 
-SDLSurfaceUniquePtr ScaleSurfaceToOutput(Surface srcSurface)
+std::string GetScreenshotFileName()
 {
-	const SDL_Rect outputViewport = Renderer::GetOutputViewport();
-
-	int w = outputViewport.w;
-	int h = outputViewport.h;
-
-	SDLSurfaceUniquePtr dstSurface(SDLWrap::CreateRGBSurfaceWithFormat(0, w, h, 8, SDL_PIXELFORMAT_INDEX8));
-
-	if (SDL_BlitScaled(srcSurface.getSDLSurface(), nullptr, dstSurface.get(), nullptr) <= -1) {
-		ErrSdl();
+	const std::time_t tt = std::time(nullptr);
+	const std::tm *tm = std::localtime(&tt);
+	const std::string filename = tm != nullptr ? fmt::format("Screenshot from {:04}-{:02}-{:02} {:02}-{:02}-{:02}", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec) : "Screenshot";
+	std::string dstPath = StrCat(paths::PrefPath(), filename, ".png");
+	int i = 0;
+	while (FileExists(dstPath.c_str())) {
+		i++;
+		dstPath = StrCat(paths::PrefPath(), filename, "-", i, ".png");
 	}
-
-	return dstSurface;
+	return dstPath;
 }
 
-void CaptureScreenshot(std::string path)
+bool CaptureScreenToPNG(SDL_Color *palette)
 {
-	Surface srcSurface = GlobalBackBuffer();
+	SDL_Surface *surface = SDL_GetWindowSurface(window);
+	if (!surface) {
+		Log("Failed to capture screen surface.");
+		return false;
+	}
 
-	if (*sgOptions.Graphics.upscale) {
-		SDLSurfaceUniquePtr dstSurface = ScaleSurfaceToOutput(srcSurface);
+	std::string fileName = GetScreenshotFileName();
+	if (IMG_SavePNG(surface, fileName.c_str()) != 0) {
+		Log("Failed to save screenshot to {}", fileName);
+		SDL_FreeSurface(surface);
+		return false;
+	}
 
-		if (SDL_SavePNG(dstSurface.get(), path.c_str()) <= -1) {
-			ErrSdl();
+	Log("Screenshot saved to {}", fileName);
+	SDL_FreeSurface(surface);
+	return true;
+}
+
+/**
+ * @brief Make a red version of the given palette and apply it to the screen.
+ */
+void RedPalette()
+{
+	for (int i = 0; i < 256; i++) {
+		system_palette[i].g = 0;
+		system_palette[i].b = 0;
+	}
+	palette_update();
+	BltFast(nullptr, nullptr);
+	RenderPresent();
+}
+} // namespace
+
+void CaptureScreen()
+{
+	SDL_Color palette[256];
+	DrawAndBlit();
+	RedPalette();
+
+	if (CaptureScreenToPNG(palette)) {
+		SDL_Delay(300);
+		for (int i = 0; i < 256; i++) {
+			system_palette[i] = palette[i];
 		}
-	} else {
-		if (SDL_SavePNG(srcSurface.getSDLSurface(), path.c_str()) <= -1) {
-			ErrSdl();
-		}
+		palette_update();
+		RedrawEverything();
 	}
 }
 
