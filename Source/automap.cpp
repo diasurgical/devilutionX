219c2bc3147f89da35190227042699809d966f12
KPhoenix <ericjrobinson@live.com>
2023-08-26 22:08:14 -0400

    Update automap.cpp

--- Source/automap.cpp
+++ Source/automap.cpp
@@ -897,6 +897,7 @@ void DrawAutomap(const Surface &out)
 	Displacement myPlayerOffset = {};
 	if (myPlayer.isWalking())
 		myPlayerOffset = GetOffsetForWalking(myPlayer.AnimInfo, myPlayer._pdir, true);
+	myPlayerOffset += Displacement { -1, (leveltype != DTYPE_CAVES) ? TILE_HEIGHT - 1 : -1 };
 
 	int d = (AutoMapScale * 64) / 100;
 	int cells = 2 * (gnScreenWidth / 2 / d) + 1;
@@ -960,6 +961,8 @@ void DrawAutomap(const Surface &out)
 		screen.y += AmLine(32);
 	}
 
+	if (leveltype == DTYPE_CAVES)
+		myPlayerOffset.deltaY += TILE_HEIGHT;
 	for (size_t playerId = 0; playerId < Players.size(); playerId++) {
 		Player &player = Players[playerId];
 		if (player.isOnActiveLevel() && player.plractive && !player._pLvlChanging && (&player == MyPlayer || player.friendlyMode)) {
@@ -967,6 +970,7 @@ void DrawAutomap(const Surface &out)
 		}
 	}
 
+	myPlayerOffset.deltaY -= TILE_HEIGHT / 2;
 	if (AutoMapShowItems)
 		SearchAutomapItem(out, myPlayerOffset, 8, [](Point position) { return dItem[position.x][position.y] != 0; });
 #ifdef _DEBUG
