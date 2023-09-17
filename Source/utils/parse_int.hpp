bb16b93836480f547ae0f1b98d854906b884f017
Andrew James <ephphatha@thelettereph.com>
2023-09-17 22:43:05 +1000

    move definition of non-templated function to cpp file (#6620)

--- Source/utils/parse_int.hpp
+++ Source/utils/parse_int.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <algorithm>
 #include <charconv>
+#include <cstdint>
 #include <string_view>
 #include <system_error>
 
@@ -36,32 +36,13 @@ ParseIntResult<IntT> ParseInt(
 	return value;
 }
 
-inline uint8_t ParseFixed6Fraction(std::string_view str, const char **endOfParse = nullptr)
-{
-	unsigned numDigits = 0;
-	uint32_t decimalFraction = 0;
-
-	// Read at most 7 digits, at that threshold we're able to determine an exact rounding for 6 bit fixed point numbers
-	while (!str.empty() && numDigits < 7) {
-		if (str[0] < '0' || str[0] > '9') {
-			break;
-		}
-		decimalFraction = decimalFraction * 10 + str[0] - '0';
-		++numDigits;
-		str.remove_prefix(1);
-	}
-	if (endOfParse != nullptr) {
-		// to mimic the behaviour of std::from_chars consume all remaining digits in case the value was overly precise.
-		*endOfParse = std::find_if_not(str.data(), str.data() + str.size(), [](char character) { return character >= '0' && character <= '9'; });
-	}
-	// to ensure rounding to nearest we normalise all values to 7 decimal places
-	while (numDigits < 7) {
-		decimalFraction *= 10;
-		++numDigits;
-	}
-	// we add half the step between representable values to use integer truncation as a substitute for rounding to nearest.
-	return (decimalFraction + 78125) / 156250;
-}
+/**
+ * @brief Parses a sequence of decimal characters into a 6 bit fixed point number in the range [0, 1.0]
+ * @param str a potentially empty string of base 10 digits, optionally followed by non-digit characters
+ * @param[out] endOfParse equivalent to std::from_chars_result::ptr, used to tell where parsing stopped
+ * @return a value in the range [0, 64], representing a 2.6 fixed value in the range [0, 1.0]
+ */
+uint8_t ParseFixed6Fraction(std::string_view str, const char **endOfParse = nullptr);
 
 template <typename IntT>
 ParseIntResult<IntT> ParseFixed6(std::string_view str, const char **endOfParse = nullptr)
