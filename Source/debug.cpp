4235e50f29bdbdd58b0f0ad4f80aa8f56071967c
staphen <staphen@gmail.com>
2023-09-18 11:03:25 -0400

    Indicate network validation success or failure in iteminfo

--- Source/debug.cpp
+++ Source/debug.cpp
@@ -25,6 +25,7 @@
 #include "lighting.h"
 #include "monstdat.h"
 #include "monster.h"
+#include "pack.h"
 #include "plrmsg.h"
 #include "quests.h"
 #include "spells.h"
@@ -948,6 +949,13 @@ std::string DebugCmdItemInfo(const std::string_view parameter)
 		pItem = &Items[pcursitem];
 	}
 	if (pItem != nullptr) {
+		std::string_view netPackValidation { "N/A" };
+		if (gbIsMultiplayer) {
+			ItemNetPack itemPack;
+			Item unpacked;
+			PackNetItem(*pItem, itemPack);
+			netPackValidation = UnPackNetItem(myPlayer, itemPack, unpacked) ? "Success" : "Failure";
+		}
 		return StrCat("Name: ", pItem->_iIName,
 		    "\nIDidx: ", pItem->IDidx, " (", AllItemsList[pItem->IDidx].iName, ")",
 		    "\nSeed: ", pItem->_iSeed,
@@ -962,7 +970,8 @@ std::string DebugCmdItemInfo(const std::string_view parameter)
 		    "\nBoy: ", ((pItem->_iCreateInfo & CF_BOY) == 0) ? "False" : "True",
 		    "\nWitch: ", ((pItem->_iCreateInfo & CF_WITCH) == 0) ? "False" : "True",
 		    "\nHealer: ", ((pItem->_iCreateInfo & CF_HEALER) == 0) ? "False" : "True",
-		    "\nPregen: ", ((pItem->_iCreateInfo & CF_PREGEN) == 0) ? "False" : "True");
+		    "\nPregen: ", ((pItem->_iCreateInfo & CF_PREGEN) == 0) ? "False" : "True",
+		    "\nNet Validation: ", netPackValidation);
 	}
 	return StrCat("Numitems: ", ActiveItemCount);
 }
