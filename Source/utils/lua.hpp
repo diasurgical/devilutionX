493aaf554e6d9b3206d445a9267f8af5f7807c02
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-10-15 23:29:44 +0100

    Add a lua debug command
    Ideally we should have a better UI for this in the future.

--- Source/utils/lua.hpp
+++ Source/utils/lua.hpp
@@ -2,10 +2,13 @@
 
 #include <string_view>
 
+#include <expected.hpp>
+
 namespace devilution {
 
 void LuaInitialize();
 void LuaShutdown();
 void LuaEvent(std::string_view name);
+tl::expected<std::string, std::string> RunLua(std::string_view code);
 
 } // namespace devilution
