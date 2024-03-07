4c1f8e19261c46155b75374a58f066b87580a27d
KPhoenix <ericjrobinson@live.com>
2024-03-07 15:11:33 -0500

    Use FriendlyFire option

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -206,7 +206,7 @@ bool MonsterMHit(const Player &player, int monsterId, int mindam, int maxdam, in
 {
 	auto &monster = Monsters[monsterId];
 
-	if (!monster.isPossibleToHit() || monster.isImmune(t, damageType) || monster.belongsToPlayer(player) || (!monster.belongsToPlayer(player) && player.friendlyMode))
+	if (!monster.isPossibleToHit() || monster.isImmune(t, damageType) || monster.belongsToPlayer(player) || (sgGameInitInfo.bFriendlyFire == 0 && player.friendlyMode))
 		return false;
 
 	int hit = RandomIntLessThan(100);
