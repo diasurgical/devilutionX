f008914f2ee613762a68a1c04eb7811221f30c7b
Eric Robinson <68359262+kphoenix137@users.noreply.github.com>
2024-03-26 20:54:47 -0400

    sort inventory (#11)

--- Source/diablo.cpp
+++ Source/diablo.cpp
@@ -42,6 +42,7 @@
 #include "help.h"
 #include "hwcursor.hpp"
 #include "init.h"
+#include "inv.h"
 #include "levels/drlg_l1.h"
 #include "levels/drlg_l2.h"
 #include "levels/drlg_l3.h"
@@ -1965,6 +1966,14 @@ void InitKeymapActions()
 	    'L',
 	    [] {
 		    ToggleChatLog();
+	    });
+	sgOptions.Keymapper.AddAction(
+	    "SortInv",
+	    N_("Sort Inventory"),
+	    N_("Sorts the inventory."),
+	    'R',
+	    [] {
+		    ReorganizeInventory(*MyPlayer);
 	    });
 #ifdef _DEBUG
 	sgOptions.Keymapper.AddAction(
