093a170df137cb882618ac82ab34687b901c4eb9
KPhoenix <ericjrobinson@live.com>
2024-05-12 15:02:33 -0400

    Fix 3

--- Source/items.cpp
+++ Source/items.cpp
@@ -1514,7 +1514,7 @@ int GetItemBLevel(int lvl, item_misc_id miscId, bool onlygood, bool uper15)
 	return iblvl;
 }
 
-void SetupAllItems(const Player &player, Item &item, _item_indexes idx, uint32_t iseed, int lvl, int uper, bool onlygood, bool pregen, int uidOffset = 0, bool recreate, bool isUnique = false)
+void SetupAllItems(const Player &player, Item &item, _item_indexes idx, uint32_t iseed, int lvl, int uper, bool onlygood, bool pregen, int uidOffset = 0, bool recreate = false, bool recreatedIsUnique = false)
 {
 	item._iSeed = iseed;
 	SetRndSeed(iseed);
@@ -1535,7 +1535,7 @@ void SetupAllItems(const Player &player, Item &item, _item_indexes idx, uint32_t
 		int iblvl = GetItemBLevel(lvl, item._iMiscId, onlygood, uper == 15);
 		if (iblvl != -1) {
 			_unique_items uid = CheckUnique(item, iblvl, uper, uidOffset);
-			bool recreatedItemIsNotUnique = recreate && !isUnique;
+			bool recreatedItemIsNotUnique = recreate && !recreatedIsUnique;
 			if (uid == UITEM_INVALID || recreatedItemIsNotUnique) {
 				GetItemBonus(player, item, iblvl / 2, iblvl, onlygood, true);
 			} else {
