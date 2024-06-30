60113bb0b8defadbff7eeb5dd8807cd43dae66be
staphen <staphen@gmail.com>
2024-06-30 17:09:01 -0400

    Fix brightness of whu casting animations

--- Source/engine/trn.cpp
+++ Source/engine/trn.cpp
@@ -65,4 +65,16 @@ std::optional<std::array<uint8_t, 256>> GetClassTRN(Player &player)
 	return std::nullopt;
 }
 
+std::optional<std::array<uint8_t, 256>> GetPlayerGraphicTRN(const char *pszName)
+{
+	char path[MaxMpqPathSize];
+	*BufCopy(path, pszName, ".trn") = '\0';
+
+	std::array<uint8_t, 256> trn;
+	if (LoadOptionalFileInMem(path, &trn[0], 256)) {
+		return trn;
+	}
+	return std::nullopt;
+}
+
 } // namespace devilution
--- Source/player.cpp
+++ Source/player.cpp
@@ -2134,9 +2134,13 @@ void LoadPlrGFX(Player &player, player_graphic graphic)
 	*fmt::format_to(pszName, R"(plrgfx\{0}\{1}\{1}{2})", path, std::string_view(prefix, 3), szCel) = 0;
 	const uint16_t animationWidth = GetPlayerSpriteWidth(cls, graphic, animWeaponId);
 	animationData.sprites = LoadCl2Sheet(pszName, animationWidth);
-	std::optional<std::array<uint8_t, 256>> trn = GetClassTRN(player);
-	if (trn) {
-		ClxApplyTrans(*animationData.sprites, trn->data());
+	std::optional<std::array<uint8_t, 256>> graphicTRN = GetPlayerGraphicTRN(pszName);
+	if (graphicTRN) {
+		ClxApplyTrans(*animationData.sprites, graphicTRN->data());
+	}
+	std::optional<std::array<uint8_t, 256>> classTRN = GetClassTRN(player);
+	if (classTRN) {
+		ClxApplyTrans(*animationData.sprites, classTRN->data());
 	}
 }
 
