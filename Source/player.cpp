1510fcba82aaae3191b0dce2f786afba1b9af9ae
Eric Robinson <ericjrobinson@live.com>
2024-07-25 14:55:07 -0400

    Clean up StartRangeAttack()

--- Source/player.cpp
+++ Source/player.cpp
@@ -211,11 +211,13 @@ void StartRangeAttack(Player &player, Direction d, WorldTileCoord cx, WorldTileC
 	}
 
 	int8_t skippedAnimationFrames = 0;
+	const auto flags = player._pIFlags;
+
 	if (!gbIsHellfire) {
-		if (includesFirstFrame && HasAnyOf(player._pIFlags, ItemSpecialEffect::QuickAttack | ItemSpecialEffect::FastAttack)) {
+		if (includesFirstFrame && HasAnyOf(flags, ItemSpecialEffect::QuickAttack | ItemSpecialEffect::FastAttack)) {
 			skippedAnimationFrames += 1;
 		}
-		if (HasAnyOf(player._pIFlags, ItemSpecialEffect::FastAttack)) {
+		if (HasAnyOf(flags, ItemSpecialEffect::FastAttack)) {
 			skippedAnimationFrames += 1;
 		}
 	}
@@ -230,6 +232,7 @@ void StartRangeAttack(Player &player, Direction d, WorldTileCoord cx, WorldTileC
 	SetPlayerOld(player);
 	player.position.temp = WorldTilePosition { cx, cy };
 }
+
 
 player_graphic GetPlayerGraphicForSpell(SpellID spellId)
 {
