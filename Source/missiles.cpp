6bed95468e1ceb915b0444f88e9891fd86e691a7
Eric Robinson <ericjrobinson@live.com>
2024-07-25 15:47:31 -0400

    Fix Wall Spells

--- Source/missiles.cpp
+++ Source/missiles.cpp
@@ -690,7 +690,7 @@ bool GuardianTryFireAt(Missile &missile, Point target)
 	return true;
 }
 
-bool GrowWall(int id, Point position, Point target, MissileID type, int spellLevel, int damage)
+bool GrowWall(int id, Point position, Point target, MissileID type, int spellLevel, int damage, bool skip = false)
 {
 	[[maybe_unused]] int dp = dPiece[position.x][position.y];
 	assert(dp <= MAXTILES && dp >= 0);
@@ -699,7 +699,8 @@ bool GrowWall(int id, Point position, Point target, MissileID type, int spellLev
 		return false;
 	}
 
-	AddMissile(position, position, Direction::South, type, TARGET_BOTH, id, damage, spellLevel);
+	if (!skip)
+		AddMissile(position, position, Direction::South, type, TARGET_BOTH, id, damage, spellLevel);
 	return true;
 }
 
@@ -3188,28 +3189,29 @@ void ProcessLightningWallControl(Missile &missile)
 	int lvl = !missile.IsTrap() ? Players[id].getCharacterLevel() : 0;
 	int dmg = 16 * (GenerateRndSum(10, 2) + lvl + 2);
 
-	{
-		Point position = { missile.var1, missile.var2 };
-		Point target = position + static_cast<Direction>(missile.var3);
+	Point position1 = { missile.var1, missile.var2 };
+	Point target1 = position1 + static_cast<Direction>(missile.var3);
 
-		if (!missile.limitReached && GrowWall(id, position, target, MissileID::LightningWall, missile._mispllvl, dmg)) {
-			missile.var1 = target.x;
-			missile.var2 = target.y;
-		} else {
-			missile.limitReached = true;
-		}
+	if (!missile.limitReached && GrowWall(id, position1, target1, MissileID::LightningWall, missile._mispllvl, dmg)) {
+		missile.var1 = target1.x;
+		missile.var2 = target1.y;
+	} else {
+		missile.limitReached = true;
 	}
 
-	{
-		Point position = { missile.var5, missile.var6 };
-		Point target = position + static_cast<Direction>(missile.var4);
+	Point position2 = { missile.var5, missile.var6 };
+	Point target2 = position2 + static_cast<Direction>(missile.var4);
 
-		if (missile.var7 == 0 && GrowWall(id, position, target, MissileID::LightningWall, missile._mispllvl, dmg)) {
-			missile.var5 = target.x;
-			missile.var6 = target.y;
-		} else {
-			missile.var7 = 1;
-		}
+	bool skip = false;
+
+	if (position1 == position2)
+		skip = true;
+
+	if (missile.var7 == 0 && GrowWall(id, position2, target2, MissileID::LightningWall, missile._mispllvl, dmg)) {
+		missile.var5 = target2.x;
+		missile.var6 = target2.y;
+	} else {
+		missile.var7 = 1;
 	}
 }
 
@@ -3734,28 +3736,29 @@ void ProcessFireWallControl(Missile &missile)
 
 	int id = missile._misource;
 
-	{
-		Point position = { missile.var1, missile.var2 };
-		Point target = position + static_cast<Direction>(missile.var3);
+	Point position1 = { missile.var1, missile.var2 };
+	Point target1 = position1 + static_cast<Direction>(missile.var3);
 
-		if (!missile.limitReached && GrowWall(id, position, target, MissileID::FireWall, missile._mispllvl, 0)) {
-			missile.var1 = target.x;
-			missile.var2 = target.y;
-		} else {
-			missile.limitReached = true;
-		}
+	if (!missile.limitReached && GrowWall(id, position1, target1, MissileID::FireWall, missile._mispllvl, 0)) {
+		missile.var1 = target1.x;
+		missile.var2 = target1.y;
+	} else {
+		missile.limitReached = true;
 	}
 
-	{
-		Point position = { missile.var5, missile.var6 };
-		Point target = position + static_cast<Direction>(missile.var4);
+	Point position2 = { missile.var5, missile.var6 };
+	Point target2 = position2 + static_cast<Direction>(missile.var4);
 
-		if (missile.var7 == 0 && GrowWall(id, position, target, MissileID::FireWall, missile._mispllvl, 0)) {
-			missile.var5 = target.x;
-			missile.var6 = target.y;
-		} else {
-			missile.var7 = 1;
-		}
+	bool skip = false;
+
+	if (position1 == position2)
+		skip = true;
+
+	if (missile.var7 == 0 && GrowWall(id, position2, target2, MissileID::FireWall, missile._mispllvl, 0, skip)) {
+		missile.var5 = target2.x;
+		missile.var6 = target2.y;
+	} else {
+		missile.var7 = 1;
 	}
 }
 
