747efeb266d4a733888abffb61d1b3f90d3c50c4
Gleb Mazovetskiy <glex.spb@gmail.com>
2024-08-08 03:40:27 +0100

    clx_render: Slightly optimize `GetSkipSize`
    This gives a very slight FPS boost.
    1140 to 1143 FPS on my machine as measured by:
    
    ```bash
    tools/linux_reduced_cpu_variance_run.sh tools/measure_timedemo_performance.py -n 5 --binary build-rel/devilutionx
    ```

--- Source/utils/clx_decode.hpp
+++ Source/utils/clx_decode.hpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 
+#include "appfat.h"
+#include "utils/attributes.h"
+
 namespace devilution {
 
 [[nodiscard]] constexpr bool IsClxOpaque(uint8_t control)
@@ -25,6 +28,31 @@ namespace devilution {
 {
 	constexpr uint8_t ClxFillEnd = 0xBF;
 	return ClxFillEnd - control;
+}
+
+struct SkipSize {
+	int_fast16_t wholeLines;
+	int_fast16_t xOffset;
+};
+
+// Returns the number of lines and the x-offset by which the rendering has overrun
+// the current line (when a CLX command overruns the current line).
+//
+// Requires: remainingWidth <= 0.
+DVL_ALWAYS_INLINE SkipSize GetSkipSize(int_fast16_t remainingWidth, int_fast16_t srcWidth)
+{
+	// If `remainingWidth` is negative, `-remainingWidth` is the overrun.
+	// Otherwise, `remainingWidth` is always 0.
+
+	// Remaining width of 0 (= no overrun) is a common case.
+	// The calculation below would result in the same result.
+	// However, checking for 0 and skipping it entirely turns out to be faster.
+	if (remainingWidth == 0) return { 1, 0 };
+
+	const auto overrun = static_cast<uint_fast16_t>(-remainingWidth);
+	const uint_fast16_t overrunLines = overrun / srcWidth + 1;
+	const uint_fast16_t xOffset = overrun % srcWidth;
+	return { static_cast<int_fast16_t>(overrunLines), static_cast<int_fast16_t>(xOffset) };
 }
 
 } // namespace devilution
