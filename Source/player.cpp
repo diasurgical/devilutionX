d54e96ed90f31c2e093375fff3fa50668382a3a8
Eric Robinson <68359262+kphoenix137@users.noreply.github.com>
2024-08-12 07:30:53 -0400

    Optimize RespawnDeadItem() (#7212)

--- Source/player.cpp
+++ Source/player.cpp
@@ -293,13 +293,14 @@ void RespawnDeadItem(Item &&itm, Point target)
 		return;
 
 	int ii = AllocateItem();
+	Item &item = Items[ii];
 
 	dItem[target.x][target.y] = ii + 1;
 
-	Items[ii] = itm;
-	Items[ii].position = target;
-	RespawnItem(Items[ii], true);
-	NetSendCmdPItem(false, CMD_SPAWNITEM, target, Items[ii]);
+	item = itm;
+	item.position = target;
+	RespawnItem(item, true);
+	NetSendCmdPItem(false, CMD_SPAWNITEM, target, item);
 }
 
 void DeadItem(Player &player, Item &&item, Displacement direction)
