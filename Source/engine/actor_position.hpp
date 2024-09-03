797f12f85560fb77ff0bea8a26319239edf4fb6d
Eric Robinson <ericjrobinson@live.com>
2024-09-02 23:18:04 -0400

    Update actor_position.hpp

--- Source/engine/actor_position.hpp
+++ Source/engine/actor_position.hpp
@@ -8,7 +8,8 @@
 
 namespace devilution {
 
-struct ActorPosition {
+class ActorPosition {
+public: // TODO: Make private when all direct usage of members variables are removed.
 	WorldTilePosition tile;
 	/** Future tile position. Set at start of walking animation. */
 	WorldTilePosition future;
@@ -18,6 +19,22 @@ struct ActorPosition {
 	WorldTilePosition old;
 	/** Used for referring to position of player when finishing moving one tile (also used to define target coordinates for spells and ranged attacks) */
 	WorldTilePosition temp;
+
+	// TODO: Set public here when all direct usage of members variables are removed.
+	WorldTilePosition getTile() const { return tile; }
+	void setTile(const WorldTilePosition &pos) { tile = pos; }
+
+	WorldTilePosition getFuture() const { return future; }
+	void setFuture(const WorldTilePosition &pos) { future = pos; }
+
+	WorldTilePosition getLast() const { return last; }
+	void setLast(const WorldTilePosition &pos) { last = pos; }
+
+	WorldTilePosition getOld() const { return old; }
+	void setOld(const WorldTilePosition &pos) { old = pos; }
+
+	WorldTilePosition getTemp() const { return temp; }
+	void setTemp(const WorldTilePosition &pos) { temp = pos; }
 
 	/** @brief Calculates the offset for the walking animation. */
 	DisplacementOf<int8_t> CalculateWalkingOffset(Direction dir, const AnimationInfo &animInfo) const;
