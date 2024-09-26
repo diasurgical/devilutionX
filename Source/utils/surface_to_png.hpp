fc6c66df74f3012b127b534a05f2978974cc3ded
Gleb Mazovetskiy <glex.spb@gmail.com>
2024-09-26 20:52:08 +0100

    Screenshotting: Use `SDL_RWops` throughout
    Turns out `SDL_RWFromFP` isn't implemented in SDL for mingw.

--- Source/utils/surface_to_png.hpp
+++ Source/utils/surface_to_png.hpp
@@ -1,13 +1,19 @@
 #include <cstdio>
 #include <string>
 
+#include <SDL.h>
 #include <expected.hpp>
 
 #include "engine/surface.hpp"
 
 namespace devilution {
 
+/**
+ * @brief Writes the given surface to `dst` as PNG.
+ *
+ * Takes ownership of `dst` and closes it when done.
+ */
 tl::expected<void, std::string>
-WriteSurfaceToFilePng(const Surface &buf, FILE *outStream);
+WriteSurfaceToFilePng(const Surface &buf, SDL_RWops *dst);
 
 } // namespace devilution
