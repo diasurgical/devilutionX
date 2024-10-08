f71c7ff56f451e2fde1a1bd3ab240be8d9a35633
Eric Robinson <ericjrobinson@live.com>
2024-10-07 23:38:20 -0400

    Move Lazarus spawn code

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -544,6 +544,13 @@ void PlaceQuestMonsters()
 		}
 	} else if (setlvlnum == SL_SKELKING) {
 		PlaceUniqueMonst(UniqueMonsterType::SkeletonKing, 0, 0);
+	} else if (setlvlnum == SL_VILEBETRAYER) {
+		AddMonsterType(UniqueMonsterType::Lazarus, PLACE_UNIQUE);
+		AddMonsterType(UniqueMonsterType::RedVex, PLACE_UNIQUE);
+		AddMonsterType(UniqueMonsterType::BlackJade, PLACE_UNIQUE);
+		PlaceUniqueMonst(UniqueMonsterType::Lazarus, 0, 0);
+		PlaceUniqueMonst(UniqueMonsterType::RedVex, 0, 0);
+		PlaceUniqueMonst(UniqueMonsterType::BlackJade, 0, 0);
 	}
 }
 
@@ -3540,15 +3547,6 @@ void SetMapMonsters(const uint16_t *dunData, Point startPosition)
 	if (setlevel)
 		for (int i = 0; i < MAX_PLRS; i++)
 			AddMonster(GolemHoldingCell, Direction::South, 0, false);
-
-	if (setlevel && setlvlnum == SL_VILEBETRAYER) {
-		AddMonsterType(UniqueMonsterType::Lazarus, PLACE_UNIQUE);
-		AddMonsterType(UniqueMonsterType::RedVex, PLACE_UNIQUE);
-		AddMonsterType(UniqueMonsterType::BlackJade, PLACE_UNIQUE);
-		PlaceUniqueMonst(UniqueMonsterType::Lazarus, 0, 0);
-		PlaceUniqueMonst(UniqueMonsterType::RedVex, 0, 0);
-		PlaceUniqueMonst(UniqueMonsterType::BlackJade, 0, 0);
-	}
 
 	int width = SDL_SwapLE16(dunData[0]);
 	int height = SDL_SwapLE16(dunData[1]);
