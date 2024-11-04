635114b2f5e4e10b09603a22da7ccb9d46324635
Gleb Mazovetskiy <glex.spb@gmail.com>
2024-11-04 23:19:15 +0000

    Migrate from SimpleIni to our own implementation
    Our implementation has a more modern interface and only
    supports the features that we care about.
    
    It always outputs `\n` as newlines and does not output BOM.
    
    The modern interface eliminates awkward `c_str()/data()` conversions.
    
    This implementation preserves comments and the file order of sections
    and keys. New keys are written in insertion order.
    
    We now also support modifying and adding default comments,
    which may be a useful thing to do for the especially tricky
    ini options (this PR doesn't add any but adds the ability to do so).
    
    Sadly, this increases the RG99 binary size by 24 KiB.
    I'm guessing this is because the map implementation generates
    quite a bit of code.
    
    Note that while it might seem that using `std::string` for every key and
    value would do a lot of allocations, most of these strings are
    small and thus benefit from Small String Optimization (= no allocations).

---
+++ test/ini_test.cpp
@@ -0,0 +1,86 @@
+#include "utils/ini.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <string_view>
+
+namespace devilution {
+namespace {
+
+using ::testing::ElementsAre;
+using ::testing::Eq;
+using ::testing::Field;
+
+} // namespace
+
+TEST(IniTest, BasicTest)
+{
+	tl::expected<Ini, std::string> result = Ini::parse(R"(
+; Section A comment
+[sectionA]
+key1 = value1
+key2 = value2
+; comment multi 1
+multi = a
+; comment multi 2
+multi = b
+int=-3
+float=2.5
+; bool yes comment
+bool yes=1
+bool no=0
+
+; Section B comment line 1
+; Section B comment line 2
+[sectionB]
+key = value
+)");
+
+	ASSERT_TRUE(result.has_value()) << result.error();
+	EXPECT_EQ(result->getString("sectionA", "key1"), "value1");
+	EXPECT_EQ(result->getString("sectionA", "key2"), "value2");
+	{
+		const std::span<const Ini::Value> multiVals = result->get("sectionA", "multi");
+		std::vector<std::string> multiStrs;
+		for (const Ini::Value &val : multiVals) {
+			multiStrs.push_back(val.value);
+		}
+		EXPECT_THAT(multiStrs, ElementsAre(Eq("a"), Eq("b")));
+	}
+	EXPECT_EQ(result->getInt("sectionA", "int", 0), -3);
+	EXPECT_NEAR(result->getFloat("sectionA", "float", 0.0f), 2.5f, 0.001f);
+	EXPECT_EQ(result->getString("sectionB", "key"), "value");
+
+	result->set("newSection", "newKey", "hello");
+	result->set("sectionA", "key1", "newValue");
+	result->set("sectionA", "int", 1337);
+	result->set("sectionA", "bool yes", false);
+	const std::vector<std::string> newMulti { "x", "y", "z" };
+	result->set("sectionA", "multi", newMulti);
+	result->set("sectionA", "float", 10.5F);
+	EXPECT_EQ(result->serialize(), std::string_view(R"(; Section A comment
+[sectionA]
+key1=newValue
+key2=value2
+; comment multi 1
+multi=x
+; comment multi 2
+multi=y
+multi=z
+int=1337
+float=10.5
+; bool yes comment
+bool yes=0
+bool no=0
+
+; Section B comment line 1
+; Section B comment line 2
+[sectionB]
+key=value
+
+[newSection]
+newKey=hello
+)"));
+}
+} // namespace devilution
