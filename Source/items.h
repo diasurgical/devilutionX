bfd289db88b79ede40db545d57d4c048ee8d1b1d
staphen <staphen@gmail.com>
2024-11-09 11:47:47 -0500

    Clear unique item flags in LoadGameLevel()

--- Source/items.h
+++ Source/items.h
@@ -487,6 +487,7 @@ extern DVL_API_FOR_TEST bool UniqueItemFlags[128];
 uint8_t GetOutlineColor(const Item &item, bool checkReq);
 bool IsItemAvailable(int i);
 bool IsUniqueAvailable(int i);
+void ClearUniqueItemFlags();
 void InitItemGFX();
 void InitItems();
 void CalcPlrItemVals(Player &player, bool Loadgfx);
