99287bb9538bab997c34f62a6da6c3cd0d6a2471
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2024-11-10 18:06:22 +0100

    Improve the validate_translations.py script.
    Without marking the regular expressions as raw strings Python emits the
    following warning:
    
    .../devilutionX/tools/validate_translations.py:24: SyntaxWarning: invalid escape sequence '\d'
      if re.search("^{\d+}$", argument) == None:

--- tools/validate_translations.py
+++ tools/validate_translations.py
@@ -1,27 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from glob import glob
-import polib
 import re
+from glob import glob
+
+import polib
+
 
 def validateEntry(original, translation):
 	if translation == '':
 		return True
 
 	# Find fmt arguments in source message
-	src_arguments = re.findall("{.*?}", original)
-	if len(src_arguments) == 0:
+	src_arguments = re.findall(r"{.*?}", original)
+	if not src_arguments:
 		return True
 
 	# Find fmt arguments in translation
-	translated_arguments = re.findall("{.*?}", translation)
+	translated_arguments = re.findall(r"{.*?}", translation)
 
 	# If paramteres are untyped with order, sort so that they still appear equal if reordered
 	# Note: This does no hadle cases where the translator reordered arguments where not expected
 	# by the source. Or other advanced but valid usages of the fmt syntax
 	isOrdered = True
 	for argument in src_arguments:
-		if re.search("^{\d+}$", argument) == None:
+		if not re.search(r"^{\d+}$", argument):
 			isOrdered = False
 			break
 
@@ -32,7 +34,7 @@ def validateEntry(original, translation):
 	if src_arguments == translated_arguments:
 		return True
 
-	print ("\033[36m" + original + "\033[0m != \033[31m" + translation + "\033[0m")
+	print(f"\033[36m{original}\033[0m != \033[31m{translation}\033[0m")
 
 	return False
 
@@ -40,10 +42,9 @@ def validateEntry(original, translation):
 status = 0
 
 files = glob('Translations/*.po')
-files.sort()
-for path in files:
+for path in sorted(files):
 	po = polib.pofile(path)
-	print ("\033[32mValidating " + po.metadata['Language'] + "\033[0m : " + str(po.percent_translated()) + "% translated")
+	print(f"\033[32mValidating {po.metadata['Language']}\033[0m : {po.percent_translated()}% translated")
 
 	for entry in po:
 		if entry.fuzzy:
