6d15daca484352ea7cd70be05ac75087c3267add
Andrew James <ephphatha@thelettereph.com>
2024-12-03 22:17:22 +1100

    allow moving from stash to inv from left side of wide items

--- Source/controls/plrctrls.cpp
+++ Source/controls/plrctrls.cpp
@@ -1143,7 +1143,6 @@ void StashMove(AxisDirection dir)
 
 	if (dir.x == AxisDirectionX_LEFT) {
 		if (ActiveStashSlot.x > 0) {
-
 			const StashStruct::StashCell itemIdAtActiveStashSlot = Stash.GetItemIdAtPosition(ActiveStashSlot);
 			ActiveStashSlot.x--;
 			if (holdItem.isEmpty() && itemIdAtActiveStashSlot != StashStruct::EmptyCell) {
@@ -1153,8 +1152,11 @@ void StashMove(AxisDirection dir)
 			}
 		}
 	} else if (dir.x == AxisDirectionX_RIGHT) {
-		if (ActiveStashSlot.x < 10 - itemSize.width) {
-
+		// If we're empty-handed and trying to move right while hovering over an item we may not
+		//  have a free stash column to move to. If the item we're hovering over occupies the last
+		//  column then we want to jump to the inventory instead of just moving one column over.
+		Size itemUnderCursorSize = holdItem.isEmpty() ? GetItemSizeOnSlot(ActiveStashSlot) : itemSize;
+		if (ActiveStashSlot.x < 10 - itemUnderCursorSize.width) {
 			const StashStruct::StashCell itemIdAtActiveStashSlot = Stash.GetItemIdAtPosition(ActiveStashSlot);
 			ActiveStashSlot.x++;
 			if (holdItem.isEmpty() && itemIdAtActiveStashSlot != StashStruct::EmptyCell) {
@@ -1172,7 +1174,6 @@ void StashMove(AxisDirection dir)
 	}
 	if (dir.y == AxisDirectionY_UP) {
 		if (ActiveStashSlot.y > 0) {
-
 			const StashStruct::StashCell itemIdAtActiveStashSlot = Stash.GetItemIdAtPosition(ActiveStashSlot);
 			ActiveStashSlot.y--;
 			if (holdItem.isEmpty() && itemIdAtActiveStashSlot != StashStruct::EmptyCell) {
@@ -1183,7 +1184,6 @@ void StashMove(AxisDirection dir)
 		}
 	} else if (dir.y == AxisDirectionY_DOWN) {
 		if (ActiveStashSlot.y < 10 - itemSize.height) {
-
 			const StashStruct::StashCell itemIdAtActiveStashSlot = Stash.GetItemIdAtPosition(ActiveStashSlot);
 			ActiveStashSlot.y++;
 			if (holdItem.isEmpty() && itemIdAtActiveStashSlot != StashStruct::EmptyCell) {
