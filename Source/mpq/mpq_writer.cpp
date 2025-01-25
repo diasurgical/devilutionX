1af8e75191c502770a0578037502d38fe3c6cdc9
pionere <pionere@freemail.hu>
2025-01-25 15:11:19 +0100

    prevent the overwrite of existing mpq files (requires C++17) fixes #7672

--- Source/mpq/mpq_writer.cpp
+++ Source/mpq/mpq_writer.cpp
@@ -93,7 +93,7 @@ MpqWriter::MpqWriter(const char *path)
 	LogVerbose("Opening {}", path);
 	std::string error;
 	bool exists = FileExists(path);
-	const char *mode = "wb";
+	const char *mode = "wbx";
 	if (exists) {
 		mode = "r+b";
 		std::uintmax_t fileSize;
