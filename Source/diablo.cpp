949f5bce60f89acbcf6b20a9542d49bda9402130
obligaron <obligaron@live.com>
2025-02-18 19:49:07 +0100

    Enable CTRL + Mouse scroll wheel for map zoom

--- Source/diablo.cpp
+++ Source/diablo.cpp
@@ -758,6 +758,10 @@ void GameEventHandler(const SDL_Event &event, uint16_t modState)
 				ChatLogScrollUp();
 			} else if (IsStashOpen) {
 				Stash.PreviousPage();
+			} else if (SDL_GetModState() & KMOD_CTRL) {
+				if (AutomapActive) {
+					AutomapZoomIn();
+				}
 			} else {
 				KeymapperPress(MouseScrollUpButton);
 			}
@@ -772,6 +776,10 @@ void GameEventHandler(const SDL_Event &event, uint16_t modState)
 				ChatLogScrollDown();
 			} else if (IsStashOpen) {
 				Stash.NextPage();
+			} else if (SDL_GetModState() & KMOD_CTRL) {
+				if (AutomapActive) {
+					AutomapZoomOut();
+				}
 			} else {
 				KeymapperPress(MouseScrollDownButton);
 			}
